9585cb84a6ffd03bf8ff23f0330cd47cc9e74245
Christopher Ariza <christopher.ariza@gmail.com>
2010-02-02 14:46:08 +0000

    added tests

--- music21/demos/smt2010.py
+++ music21/demos/smt2010.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python
+#-------------------------------------------------------------------------------
+# Name:         icmc2010.py
+# Purpose:      icmc2010.py
+#
+# Authors:      Christopher Ariza
+#               Michael Scott Cuthbert
+#
+# Copyright:    (c) 2009-2010 The music21 Project
+# License:      LGPL
+#-------------------------------------------------------------------------------
+
+
+import unittest, doctest
+from music21 import *
 
 
 
@@ -5,34 +20,48 @@
 # TODO: remove extraneous print statements in examples?
 
 
-def ex01():
+def ex01(show=True, *arguments, **keywords):
     # This example extracts first a part, then a measure from a complete score. Next, pitches are isolated from this score as pitch classes. Finally, consecutive pitches from this measure are extracted, made into a chord, and shown to be a dominant seventh chord. 
     
     from music21 import corpus, chord
-    sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
+
+    if 'op133' in keywords.keys():
+        sStream = keywords['op133']
+    else:
+        sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
+
     v2Part = sStream[1].measures # get all measures from the second violin
-    v2Part[48].show() # render the 48th measure as notation
+    if show:
+        v2Part[48].show() # render the 48th measure as notation
     
     # create a list of pitch classes in this measure
     pcGroup = [n.pitchClass for n in v2Part[48].pitches] 
-    print pcGroup # display the collected pitch classes as a list
+
+    if show:
+        print(pcGroup) # display the collected pitch classes as a list
     # extract from the third pitch until just before the end
     pnGroup = [n.nameWithOctave for n in v2Part[48].pitches[2:-1]] 
     qChord = chord.Chord(pnGroup) # create a chord from these pitches
-    qChord.show() # render this chord as notation
-    print qChord.isDominantSeventh() # find if this chord is a dominant
+    
+    if show:
+        qChord.show() # render this chord as notation
+        print(qChord.isDominantSeventh()) # find if this chord is a dominant
     
 
 
 
-def ex02():
+def ex02(show=True, *arguments, **keywords):
 
     
     # This example searches the second violin part for adjacent non-redundant pitch classes that form dominant seventh chords.
     
     from music21 import corpus, chord, stream
     
-    sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
+    if 'op133' in keywords.keys():
+        sStream = keywords['op133']
+    else:
+        sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
+
     v2Part = sStream[1].measures # get all measures from the first violin
     
     # First, collect all non-redundant adjacent pitch classes, and store these pitch classes in a list. 
@@ -48,39 +77,50 @@ def ex02():
         testChord = chord.Chord(pitches[i:i+4])
         if testChord.isDominantSeventh():
             found.append(testChord)
-    found.show()
+    if show:
+        found.show()
     
    
 
-def ex03():
+def ex03(show=True, *arguments, **keywords):
     
     # This examples graphs the usage of pitch classes in the first and second violin parts. 
     
     from music21 import corpus
     from music21.analysis import correlate
-    sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
+    if 'op133' in keywords.keys():
+        sStream = keywords['op133']
+    else:
+        sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
+
     # Create a graph of pitch class for the first and second part
     for part in [sStream[0], sStream[1]]:
         na = correlate.NoteAnalysis(part.flat)
         fx = lambda n: n.pitchClass # x values will be pitch classes
         fxTick = lambda n: n.name # x ticks will be pitch names
-        na.noteAttributeHistogram(fx, title=part.getInstrument().partName,
+        if show:
+            na.noteAttributeHistogram(fx, title=part.getInstrument().partName,
                                   fxTick=fxTick)
     
 
 
 
-def ex04():
+def ex04(show=True, *arguments, **keywords):
     
     # This example, by graphing pitch class over note offset, shows the usage of pitch classes in the violoncello part over the duration of the composition. While the display is coarse, it is clear that the part gets less chromatic towards the end of the work.
     
     from music21 import corpus
     from music21.analysis import correlate
-    sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
+    if 'op133' in keywords.keys():
+        sStream = keywords['op133']
+    else:
+        sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
+
     na = correlate.NoteAnalysis(sStream[3].flat)
     fy = lambda n: n.pitchClass # y values will be pitch classes
     fx = lambda n: n.offset # x values will be offsets in quarter notes
-    na.noteAttributeScatter(fx, fy, yTicks=correlate.ticksPitchClass())
+    if show:
+        na.noteAttributeScatter(fx, fy, yTicks=correlate.ticksPitchClass())
 
 
 
@@ -89,16 +129,21 @@ def ex04():
 
 #-------------------------------------------------------------------------------
 
-def ex01Alt():
+def ex01Alt(show=True, *arguments, **keywords):
     # measure here is a good test of dynamics positioning:
     from music21 import corpus, chord
-    sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
+    if 'op133' in keywords.keys():
+        sStream = keywords['op133']
+    else:
+        sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
     v2Part = sStream[1].measures # get all measures from the second violin
-    v2Part[45].show() # render the 48th measure as notation
+
+    if show:
+        v2Part[45].show() # render the 48th measure as notation
 
 
 
-def findHighestNotes():
+def findHighestNotes(show=True, *arguments, **keywords):
     import copy
     import music21
     from music21 import corpus, meter, stream
@@ -126,18 +171,23 @@ def findHighestNotes():
                         part.getInstrument().partName[0], m.measureNumber, ts.getBeat(n.offset))
         found.append(highestNote)
 
-    print found.write('musicxml')
+    if show:
+        print (ound.write('musicxml'))
 
 
-from music21 import *
 
-def ex1_revised():
-    beethovenScore = corpus.parseWork('beethoven/opus133.xml') 
+def ex1_revised(show=True, *arguments, **keywords):
+    if 'op133' in keywords.keys():
+        beethovenScore = keywords['op133']
+    else:
+        beethovenScore = corpus.parseWork('opus133.xml') # load a MusicXML file
+
     violin2 = beethovenScore[1]      # most programming languages start counting from 0, 
     #  so part 0 = violin 1, part 1 = violin 2, etc.
     display = stream.Stream() # an empty container for filling with found notes
     for thisMeasure in violin2.measures:
-        notes = thisMeasure.findConsecutiveNotes(skipUnisons = True, skipChords = True,
+        notes = thisMeasure.findConsecutiveNotes(skipUnisons = True, 
+                      skipChords = True,
                        skipOctaves = True, skipRests = True, noNone = True )
         pitches = [n.pitch for n in notes]
         for i in range(len(pitches) - 3):
@@ -158,6 +208,28 @@ def ex1_revised():
                 thisMeasure.setRightBarline("double")
                 display.append(thisMeasure)
     
-    display.write('musicxml')
+    if show:
+        display.write('musicxml')
     
-ex1_revised()
+
+
+class Test(unittest.TestCase):
+
+    def runTest(self):
+        pass
+
+    def testBasic(self):
+        '''Test non-showing functions
+        '''
+        # bethove examples are commented out for time
+        #sStream = corpus.parseWork('opus133.xml') # load a MusicXML file
+        # ex03, ex01, ex02, ex04, ex01Alt, findHighestNotes,ex1_revised
+        for func in [findHighestNotes]:
+            #func(show=False, op133=sStream)
+            func(show=False)
+
+if __name__ == "__main__":
+    import music21
+    music21.mainTest(Test)
+
+
