14385fa4809ea11dadf4465b96e4bef2fb922e37
Christopher Ariza <christopher.ariza@gmail.com>
2010-02-03 17:06:44 +0000

    updated demos

--- music21/demos/mgtaPart1.py
+++ music21/demos/mgtaPart1.py
@@ -16,6 +16,7 @@ import unittest, doctest
 import music21
 from music21 import clef
 from music21 import instrument
+from music21 import converter
 
 #-------------------------------------------------------------------------------
 # CHAPTER 1 
@@ -29,19 +30,36 @@ def ch1_basic_I_A():
     pass
 
 
-def ch1_basic_II_A_1():
+def ch1_basic_II_A_1(show=True, *arguments, **keywords):
     '''
     p3.
     Write letter names and octave designations for the pitches written in the treble and bass clefs below.
 
     (Data in Finale/musicxml format)
     '''
-    exercise = music21.parseData("p3_II_A_1.xml")
-    for thisNote in exercise.notes:
-        thisNote.lyric = thisNote.noteNameWithOctave
-    exercise.show()
+    humdata = '''
+**kern
+1gg#
+1B
+1a##
+1ddd-
+1c#
+1dd
+1G
+1b-
+1ccc
+1f-
+1aa-
+1e--
+*-
+'''
+    exercise = converter.parseData(humdata)
+    #exercise = music21.parseData("ch1_basic_II_A_1.xml")
+    for thisNote in exercise.flat.notes: # have to use flat here
+        thisNote.lyric = thisNote.nameWithOctave
+    if show: exercise.show()
 
-def ch1_II_A_2():
+def ch1_basic_II_A_2():
     '''
     bass clef
     '''
@@ -71,8 +89,9 @@ class Test(unittest.TestCase):
     def runTest(self):
         pass
             
-    def testDummy(self):
-        self.assertEqual(True, True)
+    def testBasic(self):
+        for func in [ch1_basic_II_A_1]:
+            func(show=False, play=False)
 
 
 
