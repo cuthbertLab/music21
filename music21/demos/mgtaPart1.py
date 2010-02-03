059b8df4145d83cddafd19bc2ab3aed8dccae896
Christopher Ariza <christopher.ariza@gmail.com>
2010-02-03 16:22:39 +0000

    updated structure of demos

--- music21/demos/mgtaPart1.py
+++ music21/demos/mgtaPart1.py
@@ -13,40 +13,73 @@
 
 import unittest, doctest
 
-### CHAPTER 1 
-
 import music21
 from music21 import clef
 from music21 import instrument
 
-def ch1():
- def basic():
-  def II():
-   def A():
-    def ex1():
-        '''
-        p3.
-        Write letter names and octave designations for the pitches written in the treble and bass clefs below.
-    
-        (Data in Finale/musicxml format)
-        '''
-        exercise = music21.parseData("p3_II_A_1.xml")
-        for thisNote in exercise.notes:
-            thisNote.lyric = thisNote.noteNameWithOctave
-        exercise.show()
+#-------------------------------------------------------------------------------
+# CHAPTER 1 
 
- def writing():
-  def I():
-   def B():
-    def ex3():
-        '''
-        p. 7.
-        Transcribe Purcell, "Music for a While" for bassoon in tenor clef
-        '''
-        purcellScore = music21.parseData("Music_for_a_while.musicxml")
-        thisClef = purcellScore.getElementsByClass(clef.Clef)[0]
-        thisClef.__class__ = clef.TenorClef
-        purcellScore.insert(0, instrument.Instrument('bassoon'))
-        assert purcellScore.allInRange() is True
-        purcellScore.playMidi()   ## will play on bassoon
-        purcellScore.show() 
+
+def ch1_basic_I_A():
+    '''
+    p2.
+    Two possible letter names for each pitch parked above and below a position    
+    '''
+    pass
+
+
+def ch1_basic_II_A_1():
+    '''
+    p3.
+    Write letter names and octave designations for the pitches written in the treble and bass clefs below.
+
+    (Data in Finale/musicxml format)
+    '''
+    exercise = music21.parseData("p3_II_A_1.xml")
+    for thisNote in exercise.notes:
+        thisNote.lyric = thisNote.noteNameWithOctave
+    exercise.show()
+
+def ch1_II_A_2():
+    '''
+    bass clef
+    '''
+    pass
+
+
+def ch1_writing_I_B_3():
+    '''
+    p. 7.
+    Transcribe Purcell, "Music for a While" for bassoon in tenor clef
+
+    >>> a = True
+    '''
+    purcellScore = music21.parseData("Music_for_a_while.musicxml")
+    thisClef = purcellScore.getElementsByClass(clef.Clef)[0]
+    thisClef.__class__ = clef.TenorClef
+    purcellScore.insert(0, instrument.Instrument('bassoon'))
+    assert purcellScore.allInRange() is True
+    purcellScore.playMidi()   ## will play on bassoon
+    purcellScore.show() 
+
+
+
+#-----------------------------------------------------------------||||||||||||--
+class Test(unittest.TestCase):
+    
+    def runTest(self):
+        pass
+            
+    def testDummy(self):
+        self.assertEqual(True, True)
+
+
+
+        
+
+#-----------------------------------------------------------------||||||||||||--
+if __name__ == "__main__":
+    music21.mainTest(Test)
+
+
