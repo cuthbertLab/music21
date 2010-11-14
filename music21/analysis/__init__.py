62162aa1ddd6f94c5dd90973c9aeecc02837f730
Christopher Ariza <christopher.ariza@gmail.com>
2010-11-14 13:27:43 +0000

    removed extraneous files

--- music21/analysis/__init__.py
+++ music21/analysis/__init__.py
@@ -3,3 +3,12 @@ __ALL__ = ['correlate', 'windowed', 'metrical', 'discrete']
 # this is necessary to get these names available with a 
 # from music21 import * import statement
 from music21.analysis import correlate, windowed, metrical, discrete
+
+
+
+
+
+
+
+
+
--- music21/analysis/discrete.py
+++ music21/analysis/discrete.py
@@ -1109,3 +1109,8 @@ if __name__ == "__main__":
         #t.testKrumhansl()
 
         t.testIntervalDiversity()
+
+
+
+
+
