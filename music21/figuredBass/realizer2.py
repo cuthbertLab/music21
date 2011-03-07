5165690b046eb8917ed5878050c117dc6e59f84a
Jose Cabal-Ugaz <jcabalugaz@gmail.com>
2011-03-07 02:32:58 +0000

    added getRandomSolutions method which was also present in realizer.py

--- music21/figuredBass/realizer2.py
+++ music21/figuredBass/realizer2.py
@@ -1,9 +1,14 @@
 import music21
 import unittest
 import random
+import copy
 
 from music21 import pitch
 from music21 import note
+from music21 import stream
+from music21 import meter
+from music21 import key
+from music21 import chord
 
 from music21.figuredBass import segment
 from music21.figuredBass import realizerScale
@@ -15,13 +20,10 @@ class FiguredBass(object):
         self.timeSig = timeSig
         self.key = key
         self.mode = mode
-        self.scale = realizerScale.FiguredBassScale(key, mode)
-        self.rules = rules.Rules()
-        self.rules.topVoicesWithinOctave = False
         self.maxPitch = pitch.Pitch('B5')
         self.figuredBassList = []
         self.bassNotes = []
-        self.fbInfo = segment.Information(self.scale, voiceList, self.rules)
+        self.fbInfo = segment.Information(realizerScale.FiguredBassScale(key, mode), voiceList, rules.Rules())
         self.allSegments = []
         self.lastSegment = None
         
@@ -31,7 +33,7 @@ class FiguredBass(object):
 
     def solve(self):
         (startBass, startNotation) = self.figuredBassList[0]
-        print("Finding possible starting chords for: " + str((startBass.pitch, startNotation)))
+        print("Finding starting possibilities for: " + str((startBass.pitch, startNotation)))
         a1 = segment.AntecedentSegment(self.fbInfo, startBass, startNotation)
         self.allSegments.append(a1)
         self.lastSegment = a1
@@ -46,47 +48,125 @@ class FiguredBass(object):
         print("Trimming movements...")
         self.lastSegment.trimAllMovements()
         numSolutions = self.lastSegment.getNumSolutions()
-        print("Solving complete. Number of solutions: " + str(numSolutions))
-       
+        if numSolutions <= 200000:
+            numberProgressions = self.calculateAllNumberProgressions()
+            print("Number of solutions, as calculated empirically: " + str(len(numberProgressions)) + ".")
+        print("Solving complete. Number of solutions, as calculated by path counting: " + str(numSolutions) + ".\n")
+                
+    def calculateAllNumberProgressions(self):
+        if len(self.allSegments) <= 1:
+            raise FiguredBassException("One segment or less not enough for progression generator..")
+        currMovements = self.allSegments[0].nextMovements
+        progressions = []
+        for prevIndex in currMovements.keys():
+            allNextIndices = currMovements[prevIndex]
+            for nextIndex in allNextIndices:
+                progressions.append([prevIndex, nextIndex])
+
+        for cSegmentIndex in range(1, len(self.allSegments)-1):
+            currMovements = self.allSegments[cSegmentIndex].nextMovements
+            progLength = len(progressions)
+            for progIndex in range(progLength):
+                prog = progressions.pop(0)
+                prevIndex = prog[-1]
+                for nextIndex in currMovements[prevIndex]:
+                    newProg = copy.copy(prog)
+                    newProg.append(nextIndex)
+                    progressions.append(newProg)
+        
+        return progressions
+
+    def numberToChordProgression(self, numberProgression):
+        chords = []
+        for segmentIndex in range(len(self.allSegments)):
+            elementIndex = numberProgression[segmentIndex]
+            chords.append(self.allSegments[segmentIndex].possibilities[elementIndex])
+            
+        return chords
+   
+    def getRandomChordProgression(self):
+        if len(self.allSegments) <= 1:
+            raise FiguredBassException("One segment or less not enough for progression generator.")
+        numberProgression = []
+        currMovements = self.allSegments[0].nextMovements
+        prevChordIndex = random.sample(currMovements.keys(), 1)[0]
+        numberProgression.append(prevChordIndex)
+
+        for cSegmentIndex in range(0, len(self.allSegments)-1):
+            currMovements = self.allSegments[cSegmentIndex].nextMovements
+            nextChordIndex = random.sample(currMovements[prevChordIndex], 1)[0]
+            numberProgression.append(nextChordIndex)
+            prevChordIndex = nextChordIndex
+
+        chordProgression = self.numberToChordProgression(numberProgression)
+        return chordProgression
+
+    def showRandomSolutions(self, amountToShow):
+        bassLine = stream.Part()
+        rightHand = stream.Part()
+        s = stream.Part()
+        ts = meter.TimeSignature(self.timeSig)
+        numSharps = key.pitchToSharps(self.key, self.mode)
+        ks = key.KeySignature(numSharps)
+        s.insert(0, ts)
+        s.insert(0, ks)
+        
+        for i in range(amountToShow):
+            print("\nProgression #" + str(i + 1))
+            chordProg = self.getRandomChordProgression()
+            for j in range(len(self.bassNotes)):
+                givenChord = chordProg[j]
+                
+                bassNote = copy.deepcopy(self.bassNotes[j])
+                rhPitches = []
+                
+                for k in range(1, len(self.fbInfo.fbVoices)):
+                    v1 = self.fbInfo.fbVoices[k]
+                    rhPitches.append(copy.deepcopy(givenChord[v1.label]))
+                                 
+                rhChord = chord.Chord(rhPitches)
+                rhChord.quarterLength = bassNote.quarterLength
+                
+                bassLine.append(bassNote)
+                rightHand.append(rhChord)
+                
+            
+            rest1 = note.Rest()
+            rest1.quarterLength = ts.totalLength
+            rest2 = note.Rest()
+            rest2.quarterLength = ts.totalLength
+            bassLine.append(rest1)
+            rightHand.append(rest2)
+
+            self.printChordProgression(chordProg)
+        
+        s.insert(0, rightHand)
+        s.insert(0, bassLine)
+        
+        s.show()
+        
+    def printChordProgression(self, chordProgression):
+        linesToPrint = []
+        for v in self.fbInfo.fbVoices:
+            voiceLine = ""
+            for chord in chordProgression:
+                voiceLine += str(chord[v.label]) + "\t"
+            linesToPrint.append(voiceLine)
+        linesToPrint.reverse()
+        for voiceLine in linesToPrint:
+            print voiceLine
+        
 class FiguredBassException(music21.Music21Exception):
     pass
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 class Test(unittest.TestCase):
 
     def runTest(self):
         pass
 
 if __name__ == "__main__":
-    v1 = voice.Voice('Bass', voice.Range('E2', 'E4'))
-    v2 = voice.Voice('Tenor', voice.Range('C3', 'A4'))
-    v3 = voice.Voice('Alto', voice.Range('F3', 'G5'))
-    v4 = voice.Voice('Soprano', voice.Range('C4', 'A5'))
-    
-    voiceList = [v1, v2, v3, v4]
-    voiceList.sort()
-
-    fb = FiguredBass(voiceList, '4/4', 'D', 'minor')
-   
-    n1 = note.Note('D3')
-    n2 = note.Note('A3')
-    n3 = note.Note('B-3')
-    n4 = note.Note('F3')
-    n5 = note.Note('G3')
-    n6 = note.Note('A2')
-    n7 = note.Note('D3')
-    
-    n7.quarterLength = 2.0
-    
-    fb.addElement(n1) #i
-    fb.addElement(n2, '7,5,#3') #V7
-    fb.addElement(n3) #VI
-    fb.addElement(n4, '6') #i6
-    fb.addElement(n5, '6') #ii6
-    fb.addElement(n6, '7,5,#3') #V7
-    fb.addElement(n7) #i
-    
-    fb.solve()
+    pass
     #music21.mainTest(Test)
 
 #------------------------------------------------------------------------------
