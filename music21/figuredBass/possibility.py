f42fe55c4cb052291e1b17fd0b888df333108f4b
Jose Cabal-Ugaz <jcabalugaz@gmail.com>
2011-03-15 06:31:13 +0000

    Eliminated methods which combine rule checking methods, instead choosing to explicitly invoke them in segment.py

--- music21/figuredBass/possibility.py
+++ music21/figuredBass/possibility.py
@@ -28,6 +28,7 @@ class Possibility(dict):
     through the subclassing of the python dictionary. Named Possibility because it is intended to 
     encapsulate a possibility for a figured bass note/figure combination, but can be used in many other ways.
     '''
+    # INITIALIZATION METHODS
     def __init__(self, *args):
         '''
         Creates a Possibility instance, where a voice label (key) corresponds to a music21 Pitch 
@@ -101,48 +102,16 @@ class Possibility(dict):
                 raise PossibilityException("Can't set ->" + str(voiceLabel) + "<-: Can't convert ->" + str(pitchValue) + "<- to a music21 pitch.Pitch instance!")
         dict.__setitem__(self, voiceLabel, newPitchValue)
     
-    # CHORD FORMATION RULES
-    def correctlyFormed(self, pitchNamesToHave = None, fbRules = rules.Rules(), verbose = False):
-        '''
-        Returns True if Possibility (self) is correctly formed. By default, this method checks to
-        see that a Possibility is complete (includes each pitch name provided) and that the top voices
-        are found within an octave, but one can allow incomplete Possibilities and change/eliminate
-        the limit of top voice separation by modifying the appropriate flag(s) in the optional
-        rules.Rules() input.
-        
-        Raises an error if no pitch names are provided to analyze completness.
-        
-        >>> from music21.figuredBass import rules
-        >>> from music21.figuredBass import possibility
-        >>> pitchNames = ['C','E','G']
-        >>> p1 = possibility.Possibility({'S': 'C5', 'A': 'G4', 'T': 'E4', 'B': 'C3'})
-        >>> p1.correctlyFormed(pitchNames)
-        True
-        >>> p1['T'] = 'C3'
-        >>> p1.correctlyFormed(pitchNames)
-        False
-        >>> fbRules = rules.Rules()
-        >>> fbRules.allowIncompletePossibilities = True
-        >>> fbRules.topVoicesMaxIntervalSeparation = None
-        >>> p1.correctlyFormed(None, fbRules)
-        True
-        '''
-        correctlyFormed = True
-        
-        if not fbRules.allowIncompletePossibilities:
-            if pitchNamesToHave is None:
-                raise PossibilityException("Pitch names to analyze completeness not provided.")
-            if self.isIncomplete(pitchNamesToHave, verbose):
-                correctlyFormed = False
-                if not verbose:
-                    return correctlyFormed
-        if not self.topVoicesWithinLimit(fbRules.topVoicesMaxIntervalSeparation, verbose):
-            correctlyFormed = False
-            if not verbose:
-                return correctlyFormed
-        
-        return correctlyFormed
+    def numVoices(self):
+        return len(self.keys())
     
+    #def __str__(self):
+    #    pass
+    
+    #def __repr__(self):
+    #    pass
+    
+    # CHORD FORMATION RULES    
     def isIncomplete(self, pitchNamesToContain, verbose = False):
         '''
         Returns True if a possibility is incomplete, a.k.a. if it doesn't contain at least
@@ -209,55 +178,6 @@ class Possibility(dict):
         return topVoicesWithinLimit
 
     # VOICE LEADING RULES
-    def correctVoiceLeading(self, nextPossibility, fbRules = rules.Rules(), verbose = False):
-        '''
-        Returns True if voice leading rules are followed going from prevPossibility (self) to nextPossibility.
-        By default, this method checks for parallel fifths and octaves, but one can allow either/both intervals 
-        to occur by modifying the appropriate flag(s) in the optional rules.Rules() input.
-        
-        Ignores any voices which aren't shared between the two possibilities, but raises an error if
-        not enough voices are shared.
-        
-        >>> from music21.figuredBass import rules
-        >>> from music21.figuredBass import possibility
-        >>> p1 = possibility.Possibility({'Tenor': 'G3', 'Bass': 'C3', 'Soprano': 'C5'})
-        >>> p2 = possibility.Possibility({'Tenor': 'A3', 'Bass': 'D3'})
-        >>> p1.correctVoiceLeading(p2) # Parallel fifths between bass and tenor
-        False
-        >>> p1['Tenor'] = 'C4'
-        >>> p2['Tenor'] = 'D4'
-        >>> p1.correctVoiceLeading(p2) # Parallel octaves between bass and tenor
-        False
-        >>> fbRules = rules.Rules()
-        >>> fbRules.allowParallelOctaves = True
-        >>> p1.correctVoiceLeading(p2, fbRules) # Parallel octaves between bass and tenor
-        True
-        >>> p1['Tenor'] = 'F3'
-        >>> p2['Tenor'] = 'G3'
-        >>> p1.correctVoiceLeading(p2)
-        True
-        >>> p3 = {'Soprano': 'D5', 'Alto': 'B4'}
-        >>> p1.correctVoiceLeading(p3)
-        Traceback (most recent call last):
-        PossibilityException: Need at least two shared voices to check for parallel fifths.
-        '''
-        hasCorrectVoiceLeading = True
-
-        if not fbRules.allowParallelFifths:
-            hasParallelFifth = self.parallelFifths(nextPossibility, verbose)
-            if hasParallelFifth:
-                hasCorrectVoiceLeading = False
-                if not verbose:
-                    return hasCorrectVoiceLeading
-        if not fbRules.allowParallelOctaves:
-            hasParallelOctave = self.parallelOctaves(nextPossibility, verbose)
-            if hasParallelOctave:
-                hasCorrectVoiceLeading = False
-                if not verbose:
-                    return hasCorrectVoiceLeading
-
-        return hasCorrectVoiceLeading
-
     def parallelFifths(self, nextPossibility, verbose = False):
         '''
         Returns True if there are parallel fifths between prevPossibility (self) and nextPossibility.
@@ -330,46 +250,6 @@ class Possibility(dict):
         return hasParallelOctave
         
     # HIDDEN INTERVAL RULES
-    def noHiddenIntervals(self, nextPossibility, vlTop, vlBottom, fbRules = rules.Rules(), verbose = False):
-        '''
-        Returns True if there are no hidden intervals in going from prevPossibility (self) to nextPossibility.
-        Checks for hidden fifths/octaves, but one can allow either/both to occur by modifying the appropriate
-        flag(s) in the optional rules.Rules() input.
-        
-        Asked to provide the voice labels (keys) of the top and bottom voices. 
-        
-        >>> from music21.figuredBass import possibility
-        >>> from music21.figuredBass import rules
-        >>> p1 = possibility.Possibility({'Soprano': 'E5', 'Bass': 'C3', 'Tenor': 'G4'})
-        >>> p2 = possibility.Possibility({'Soprano': 'A5', 'Bass': 'D3', 'Tenor': 'F4'})
-        >>> p1.noHiddenIntervals(p2, 'Soprano', 'Bass') # Hidden fifth
-        False
-        >>> p1['Soprano'] = 'A5'
-        >>> p2['Soprano'] = 'D6'
-        >>> p1.noHiddenIntervals(p2, 'Soprano', 'Bass') # Hidden octave
-        False
-        >>> fbRules = rules.Rules()
-        >>> fbRules.allowHiddenOctaves = True
-        >>> p1.noHiddenIntervals(p2, 'Soprano', 'Bass', fbRules) # Hidden octave
-        True
-        '''
-        hasNoHiddenIntervals = True
-        
-        if not fbRules.allowHiddenFifths:
-            hasHiddenFifth = self.hiddenFifth(nextPossibility, vlTop, vlBottom, verbose)
-            if hasHiddenFifth:
-                hasNoHiddenIntervals = False
-                if not verbose:
-                    return hasNoHiddenIntervals
-        if not fbRules.allowHiddenOctaves:
-            hasHiddenOctave = self.hiddenOctave(nextPossibility, vlTop, vlBottom, verbose)
-            if hasHiddenOctave:
-                hasNoHiddenIntervals = False
-                if not verbose:
-                    return hasNoHiddenIntervals
-        
-        return hasNoHiddenIntervals
-        
     def hiddenFifth(self, nextPossibility, vlTop, vlBottom, verbose = False):
         '''
         Returns True if there is no hidden fifth in going from prevPossibility (self) to nextPossibility.
@@ -423,56 +303,6 @@ class Possibility(dict):
         return hasHiddenOctave
 
     # TESSITURA RULES    
-    def correctTessitura(self, orderedVoiceList, fbRules = rules.Rules(), verbose = False):
-        '''
-        Returns True if the tessitura of the Possibility (self) is correct. By default, this
-        method checks that pitches in each voice be within range and that there be no voice
-        crossings present, although either/both of these checks can be disabled by modifying
-        the appropriate flag(s) in the optional rules.Rules() input.
-        
-        Takes in a list of Voice instances as input, ordered from low to high. Indicating range 
-        in a Voice is optional, and if no range is provided for a voice, then all the pitches 
-        associated with that voice are presumed to be in range. Voice crossing is determined 
-        using order of voices without preference to range. 
-        
-        >>> from music21.figuredBass import voice
-        >>> from music21.figuredBass import possibility
-        >>> from music21.figuredBass import rules
-        >>> v1 = voice.Voice('B', voice.Range('E2', 'E4'))
-        >>> v2 = voice.Voice('T', voice.Range('C3', 'A4'))
-        >>> v3 = voice.Voice('A', voice.Range('F3', 'G5'))
-        >>> v4 = voice.Voice('S', voice.Range('C4', 'A5'))
-        >>> orderedVoiceList = [v1, v2, v3, v4] #From lowest to highest
-        >>> p1 = possibility.Possibility({'S': 'C5', 'A': 'G4', 'B': 'C4'})
-        >>> p1.correctTessitura(orderedVoiceList)
-        True
-        >>> p2 = possibility.Possibility({'S': 'B5', 'T': 'D4', 'B': 'F4'})
-        >>> p2.correctTessitura(orderedVoiceList) # Voice crossing between Tenor and Bass, Bass pitch too high     
-        False
-        >>> fbRules = rules.Rules()
-        >>> fbRules.allowVoiceCrossing = True
-        >>> p2 = possibility.Possibility({'S': 'B5', 'T': 'D4', 'B': 'E4'}) # Voice crossing between Tenor and Bass
-        >>> p1.correctTessitura(orderedVoiceList, fbRules)
-        True
-        '''
-        hasCorrectTessitura = True
-        orderedVoiceLabels = extractVoiceLabels(orderedVoiceList)
-
-        if fbRules.filterPitchesByRange:
-            pitchesInRange = self.pitchesWithinRange(orderedVoiceList, verbose)
-            if not pitchesInRange:
-                hasCorrectTessitura = False
-            if not verbose:
-                return hasCorrectTessitura
-        if not fbRules.allowVoiceCrossing:
-            hasVoiceCrossing = self.containsVoiceCrossing(orderedVoiceLabels, verbose)
-            if hasVoiceCrossing:
-                hasCorrectTessitura = False
-            if not verbose:
-                return hasCorrectTessitura
-        
-        return hasCorrectTessitura  
-
     def voiceCrossing(self, orderedVoiceLabels, verbose = False):
         '''
         Returns True if Possibility (self) contains a voice crossing.
@@ -581,26 +411,6 @@ class Possibility(dict):
                     return pitchesInRange
 
         return pitchesInRange
-    
-    def correctVoiceLeading2(self, nextPossibility, orderedVoiceList, fbRules = rules.Rules(), verbose = False):
-        '''
-        Returns True if there is correct voice leading tessitura between 
-        '''
-        hasCorrectTessitura = True
-        orderedVoiceLabels = self.extractVoiceLabels(orderedVoiceList)
-
-        leapsWithinLimits = self.voiceLeapsWithinLimits(nextPossibility, orderedVoiceList, verbose)
-        if not leapsWithinLimits:
-            hasCorrectTessitura = False
-        
-        if not fbRules.allowVoiceOverlap:
-            hasVoiceOverlap = self.voiceOverlap(nextPossibility, orderedVoiceLabels, verbose)
-            if hasVoiceOverlap:
-                hasCorrectTessitura = False
-                if not verbose:
-                    return hasCorrectTessitura
-
-        return hasCorrectTessitura
     
     def voiceOverlap(self, nextPossibility, orderedVoiceLabels, verbose = False):
         '''
