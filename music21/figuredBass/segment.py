6eff5ef77f6a908cbbcd8fcf8fcd89d551bf95cc
Jose Cabal-Ugaz <jcabalugaz@gmail.com>
2011-04-04 19:29:07 +0000

    Added methods to resolve dominant seventh and diminished seventh possibilities, and modified correctPossibilities accordingly.

--- music21/figuredBass/segment.py
+++ music21/figuredBass/segment.py
@@ -14,11 +14,17 @@ import copy
 import random
 
 from music21 import note
+from music21 import chord
+from music21 import scale
+from music21 import environment
 
 from music21.figuredBass import rules
 from music21.figuredBass import possibility
 from music21.figuredBass import realizerScale
 from music21.figuredBass import voice
+from music21.figuredBass import resolution2
+
+_MOD = 'segment.py'
 
 class Segment:
     def __init__(self, fbInformation, bassNote, notation = ''):
@@ -31,9 +37,11 @@ class Segment:
         self.pitchNamesInChord = self.fbScale.getPitchNames(self.bassNote.pitch, self.notation)
         self.nextMovements = {}
         self.nextSegment = None
+        self.isDominantSeventh = chord.Chord(self.pitchesAboveBass).isDominantSeventh()
+        self.isDiminishedSeventh = chord.Chord(self.pitchesAboveBass).isDiminishedSeventh()
 
-    def resolve(self):
-        raise SegmentException("Must specifically create PreviousSegment or NextSegment to call this method.")
+    def correctPossibilities(self):
+        raise SegmentException("Must specifically create StartSegment or MiddleSegment to call this method.")
     
     def allPossibilities(self):
         possibilities = []
@@ -58,7 +66,7 @@ class Segment:
         
         return possibilities
     
-    def correctPossibilities(self):
+    def correctSelfContainedPossibilities(self):
         '''
         Default rules:
         (1) No incomplete possibilities
@@ -70,15 +78,19 @@ class Segment:
         
         newPossibilities = []
         for possib in allPossibilities:
+            # No incomplete possibilities
             if not self.fbRules.allowIncompletePossibilities:
                 if possib.isIncomplete(self.pitchNamesInChord):
                     continue
+            # Top voices within interval.Interval
             if not possib.topVoicesWithinLimit(self.fbRules.topVoicesMaxIntervalSeparation):
                 continue
+            # Pitches in each voice within range
             if self.fbRules.filterPitchesByRange:
                 pitchesInRange = possib.pitchesWithinRange(self.fbVoices)
                 if not pitchesInRange:
                     continue
+            # No voice crossing
             if not self.fbRules.allowVoiceCrossing:
                 hasVoiceCrossing = possib.voiceCrossing(possibility.extractVoiceLabels(self.fbVoices))
                 if hasVoiceCrossing:
@@ -138,64 +150,183 @@ class Segment:
             return numSolutions
     
                 
-class PreviousSegment(Segment):
+class StartSegment(Segment):
     def __init__(self, fbInformation, bassNote, notation = ''):
         Segment.__init__(self, fbInformation, bassNote, notation)
-        self.resolve()
+        self.correctPossibilities()
 
-    def resolve(self):
+    def correctPossibilities(self):
         # Imitates _findPossibleStartingChords from realizer.py
-        self.possibilities = self.correctPossibilities()
+        self.possibilities = self.correctSelfContainedPossibilities()
     
-class NextSegment(Segment):
+class MiddleSegment(Segment):
     def __init__(self, fbInformation, prevSegment, bassNote, notation = ''):
         Segment.__init__(self, fbInformation, bassNote, notation)
         self.prevSegment = prevSegment
-        self.resolve()
+        self.correctPossibilities()
     
-    def resolve(self):
-        # Imitates _findNextPossibilities from realizer.py
-        prevPossibilities = self.prevSegment.possibilities
-        nextPossibilities = self.correctPossibilities()
-            
-        prevPossibIndex = 0
-        for prevPossib in prevPossibilities:
-            movements = []
-            nextPossibIndex = 0
-            for nextPossib in nextPossibilities:
-                if self.checkVoiceLeading(prevPossib, nextPossib):
-                    movements.append(nextPossibIndex)
-                nextPossibIndex += 1
-            self.prevSegment.nextMovements[prevPossibIndex] = movements
-            prevPossibIndex += 1
+    def correctPossibilities(self):
+        if self.prevSegment.isDominantSeventh and self.fbRules.resolveDominantSeventhProperly:
+            dominantPossibilities = self.prevSegment.possibilities
+            resolutionPossibilities = []
+            dominantPossibIndex = 0
+            for dominantPossib in dominantPossibilities:
+                movements = []
+                resolutionPossib = self.resolveDominantSeventh(dominantPossib)
+                try:
+                    movements.append(resolutionPossibilities.index(resolutionPossib))
+                except ValueError:
+                    resolutionPossibilities.append(resolutionPossib)
+                    movements.append(len(resolutionPossibilities) - 1)             
+                self.prevSegment.nextMovements[dominantPossibIndex] = movements
+                dominantPossibIndex += 1 
+            self.prevSegment.nextSegment = self
+            self.possibilities = resolutionPossibilities
+        elif self.prevSegment.isDiminishedSeventh and self.fbRules.resolveDiminishedSeventhProperly:
+            diminishedPossibilities = self.prevSegment.possibilities
+            resolutionPossibilities = []
+            diminishedPossibIndex = 0
+            for diminishedPossib in diminishedPossibilities:
+                movements = []
+                resolutionPossib = self.resolveDiminishedSeventh(diminishedPossib)
+                try:
+                    movements.append(resolutionPossibilities.index(resolutionPossib))
+                except ValueError:
+                    resolutionPossibilities.append(resolutionPossib)
+                    movements.append(len(resolutionPossibilities) - 1)             
+                self.prevSegment.nextMovements[diminishedPossibIndex] = movements
+                diminishedPossibIndex += 1
+            self.prevSegment.nextSegment = self
+            self.possibilities = resolutionPossibilities
+        else:
+            prevPossibilities = self.prevSegment.possibilities
+            nextPossibilities = self.correctSelfContainedPossibilities()
+            prevPossibIndex = 0
+            for prevPossib in prevPossibilities:
+                movements = []
+                nextPossibIndex = 0
+                for nextPossib in nextPossibilities:
+                    if self.hasCorrectVoiceLeading(prevPossib, nextPossib):
+                        movements.append(nextPossibIndex)
+                    nextPossibIndex += 1
+                self.prevSegment.nextMovements[prevPossibIndex] = movements
+                prevPossibIndex += 1
+            self.prevSegment.nextSegment = self
+            self.possibilities = nextPossibilities
+      
+    def resolveDominantSeventh(self, dominantPossib):
+        environRules = environment.Environment(_MOD)
+        if not dominantPossib.isDominantSeventh():
+            raise SegmentException("Possibility does not form a correctly spelled dominant seventh chord.")
         
-        self.prevSegment.nextSegment = self
-        self.possibilities = nextPossibilities
+        sc = scale.MajorScale()
+        dominantScale = sc.derive(dominantPossib.chordify().pitches)
+        minorScale = dominantScale.getParallelMinor()       
+        
+        tonic = dominantScale.getTonic()
+        subdominant = dominantScale.pitchFromDegree(4)
+        majSubmediant = dominantScale.pitchFromDegree(6)
+        minSubmediant = minorScale.pitchFromDegree(6)
+        
+        resolutionChord = chord.Chord(self.pitchesAboveBass)
+        
+        if resolutionChord.root().name == tonic.name:
+            if resolutionChord.isMajorTriad():
+                environRules.warn("Dominant seventh resolution: V7->I in " + dominantScale.name)
+                resolutionPossib = resolution2.dominantSeventhToMajorTonic(dominantPossib, self.fbRules.resolveV43toI6)
+            elif resolutionChord.isMinorTriad():
+                environRules.warn("Dominant seventh resolution: V7->i in " + minorScale.name)
+                resolutionPossib = resolution2.dominantSeventhToMinorTonic(dominantPossib, self.fbRules.resolveV43toI6)
+        elif resolutionChord.root().name == majSubmediant.name:
+            if sampleChord.isMinorTriad():
+                environRules.warn("Dominant seventh resolution: V7->vi in " + dominantScale.name)
+                resolutionPossib = resolution2.dominantSeventhToMinorSubmediant(dominantPossib) #Major scale
+        elif resolutionChord.root().name == minSubmediant.name:
+            if resolutionChord.isMajorTriad():
+                environRules.warn("Dominant seventh resolution: V7->VI in " + minorScale.name)
+                resolutionPossib = resolution2.dominantSeventhToMajorSubmediant(dominantPossib) #Minor scale
+        elif resolutionChord.root().name == subdominant.name:
+            if resolutionChord.isMajorTriad():
+                environRules.warn("Dominant seventh resolution: V7->IV in " + dominantScale.name)
+                resolutionPossib = resolution2.dominantSeventhToMajorSubdominant(dominantPossib)
+            elif resolutionChord.isMinorTriad():
+                environRules.warn("Dominant seventh resolution: V7->iv in " + minorScale.name)
+                resolutionPossib = resolution2.dominantSeventhToMinorSubdominant(dominantPossib)
+        else:
+            raise SegmentException("Dominant seventh resolution: No standard resolution available.")
+        
+        if not (resolutionChord.bass() == self.bassNote.pitch):
+            raise SegmentException("Dominant seventh resolution: Bass note resolved improperly in figured bass.")
+            
+        return resolutionPossib
 
-    def checkVoiceLeading(self, prevPossib, nextPossib):
+    def resolveDiminishedSeventh(self, diminishedPossib):
+        environRules = environment.Environment(_MOD)       
+        if not diminishedPossib.isDiminishedSeventh():
+            raise SegmentException("Possibility does not form a correctly spelled diminished seventh chord.")
+          
+        diminishedChord = diminishedPossib.chordify()
+        sc = scale.HarmonicMinorScale()
+        diminishedScale = sc.deriveByDegree(7, diminishedChord.root())
+        minorScale = diminishedScale.getParallelMinor()
+        
+        tonic = diminishedScale.getTonic()
+        subdominant = diminishedScale.pitchFromDegree(4)
+
+        resolutionChord = chord.Chord(self.pitchesAboveBass)
+        
+        if resolutionChord.root().name == tonic.name:
+            if resolutionChord.isMajorTriad():
+                environRules.warn("Diminished seventh resolution: vii7->I in " + diminishedScale.name)
+                resolutionPossib = resolution2.diminishedSeventhToMajorTonic(diminishedPossib, self.fbRules.doubledRootInDim7)
+            elif resolutionChord.isMinorTriad():
+                environRules.warn("Diminished seventh resolution: vii7->i in " + minorScale.name)
+                resolutionPossib = resolution2.diminishedSeventhToMinorTonic(diminishedPossib, self.fbRules.doubledRootInDim7)
+        elif resolutionChord.root().name == subdominant.name:
+             if resolutionChord.isMajorTriad():
+                environRules.warn("Diminished seventh resolution: vii7->IV in " + diminishedScale.name)
+                resolutionPossib = resolution2.diminishedSeventhToMajorSubdominant(diminishedPossib)
+             elif resolutionChord.isMinorTriad():
+                environRules.warn("Diminished seventh resolution: vii7->iv in " + minorScale.name)
+                resolutionPossib = resolution2.diminishedSeventhToMinorSubdominant(diminishedPossib)
+        else:
+            raise SegmentException("Diminished seventh resolution: No standard resolution available.")
+
+        if not (resolutionChord.bass() == self.bassNote.pitch):
+            raise SegmentException("Diminished seventh resolution: Bass note resolved improperly in figured bass.")
+            
+        return resolutionPossib
+
+    def hasCorrectVoiceLeading(self, prevPossib, nextPossib):
         vlTop = self.fbVoices[-1].label
         vlBottom = self.fbVoices[0].label
+        # No hidden fifth
         if not self.fbRules.allowHiddenFifths:
             hasHiddenFifth = prevPossib.hiddenFifth(nextPossib, vlTop, vlBottom)
             if hasHiddenFifth:
                 return False
+        # No hidden octave
         if not self.fbRules.allowHiddenOctaves:
             hasHiddenOctave = prevPossib.hiddenOctave(nextPossib, vlTop, vlBottom)
             if hasHiddenOctave:
                 return False
         
         leapsWithinLimits = prevPossib.voiceLeapsWithinLimits(nextPossib, self.fbVoices)
+        # No voice leaps
         if not leapsWithinLimits:
             return False
+        # No voice overlaps
         if not self.fbRules.allowVoiceOverlap:
             hasVoiceOverlap = prevPossib.voiceOverlap(nextPossib, possibility.extractVoiceLabels(self.fbVoices))
             if hasVoiceOverlap:
                 return False
 
+        # No parallel fifths
         if not self.fbRules.allowParallelFifths:
             hasParallelFifth = prevPossib.parallelFifths(nextPossib)
             if hasParallelFifth:
                 return False
+        # No parallel octaves
         if not self.fbRules.allowParallelOctaves:
             hasParallelOctave = prevPossib.parallelOctaves(nextPossib)
             if hasParallelOctave:
