cdadaf3597477035a721d0600a27cab98e42eda2
Christopher Ariza <christopher.ariza@gmail.com>
2011-05-11 11:44:05 +0000

    added scala package, including testing scl scale library

---
+++ music21/scala/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-------------------------------------------------------------------------------
+# Name:         scala/__init__.py
+# Purpose:      Interface and representation of Scala scale files
+#
+# Authors:      Christopher Ariza
+#
+# Copyright:    (c) 2010 The music21 Project
+# License:      LGPL
+#-------------------------------------------------------------------------------
+
+from music21.scala.base import *
+from music21.scala import base
+
+__doc__ = scala.__doc__
+
+
+
+#------------------------------------------------------------------------------
+# eof
+
