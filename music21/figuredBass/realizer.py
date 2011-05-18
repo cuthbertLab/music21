6baadcf4fa3cc0f9dedc48452c6d57e780f4ca3a
Jose Cabal-Ugaz <jcabalugaz@gmail.com>
2011-05-18 01:26:22 +0000

    Added comments derived from the thesis.

--- music21/figuredBass/realizer.py
+++ music21/figuredBass/realizer.py
@@ -13,6 +13,7 @@ import unittest
 import random
 import copy
 import time
+import datetime
 
 from music21 import pitch
 from music21 import note
@@ -31,10 +32,20 @@ from music21.figuredBass import notation
 MIN_PITCH = pitch.Pitch('C1')
 MAX_PITCH = pitch.Pitch('B5')
 
+'''
+The module realizer.py contains the FiguredBass class. Notes and notations 
+from a figured bass are added to an instance of FiguredBass, after which 
+resolve is called to find every complete resolution to the provided bass line.
+'''
+
 def figuredBassFromStreamPart(streamPart, partList = None, takeFromNotation = False):
     '''
-    takes in a Stream part that has figures in the lyrics and
-    returns a realizer.FiguredBass object
+    Takes a music21.stream Part (or another music21.stream Stream subclass) 
+    and returns a FiguredBass object whose bass notes have Notations taken 
+    from the lyrics in the source stream. This method along with the solve 
+    method provide the easiest way of converting from a notated version of 
+    a figured bass (such as in a MusicXML file) to a realized version of the 
+    same line.
     
     >>> from music21 import *
     >>> s = tinyNotation.TinyNotationStream('C4 D8_6 E8_6 F4 G4_7 c1', '4/4')
@@ -76,6 +87,11 @@ def figuredBassFromStreamPart(streamPart, partList = None, takeFromNotation = Fa
     return fb
 
 def addLyricsToBassNote(bassNote, notationString):
+    '''
+    Takes in a bassNote and a corresponding notationString as arguments. 
+    Adds the parsed notationString as lyrics to the bassNote, which is 
+    useful when displaying the figured bass in external software.
+    '''
     n = notation.Notation(notationString)
     if len(n.figureStrings) == 0:
         return
@@ -92,7 +108,42 @@ def addLyricsToBassNote(bassNote, notationString):
 class FiguredBass(object):
     def __init__(self, partList = None, timeSigString = '4/4', keyString = 'C', modeString = 'major'):
         '''
+        Establishes a framework upon which to append notes by providing 
+        details of the figured bass as arguments. First, one provides a 
+        partList containing a list of Part instances. The next three 
+        arguments are the time signature, key, and mode, all provided 
+        as strings. The strings are converted to appropriate music21 
+        objects before being stored.
         
+        >>> from music21.figuredBass import realizer
+        >>> from music21.figuredBass import part
+        >>> from music21 import note
+        >>> p1 = part.Part(1)
+        >>> p2 = part.Part(2)
+        >>> p3 = part.Part(3)
+        >>> p4 = part.Part(4)
+        >>> partList = [p1, p2, p3, p4]
+        >>> fbLine2 = realizer.FiguredBass(partList, "3/4", "D", "major")
+        
+        Now that a FiguredBass object has been created, we can invoke
+        other methods to append notes and figures of the corresponding 
+        bass line, to realize the line, and then to generate and display
+        complete realizations.
+        
+        >>> bassNote1 = note.Note("D3")
+        >>> bassNote2 = note.Note("E3")
+        >>> bassNote3 = note.Note("F#3")
+        >>> fbLine2.addElement(bassNote1)        # I
+        >>> fbLine2.addElement(bassNote2, "6")   # viio6
+        >>> fbLine2.addElement(bassNote3,  "6")  # I6
+        >>> fbLine2.realize()
+        >>> fbLine2.timeElapsed.seconds
+        7
+        >>> fbLine2.getNumSolutions()
+        171
+        >>> fbLine2.showRandomRealizations(10)
+        >>> allSolsScore = fbLine2.generateAllRealizations()
+        >>> fbLine2.showRandomRealization()
         '''
         if partList is None:
             part1 = part.Part(1,2)
@@ -124,10 +175,14 @@ class FiguredBass(object):
         
         #Contains fb solutions
         self.isRealized = False
-        self.allSegments = []
-        self.lastSegment = None
         
     def addElement(self, bassNote, notationString = ''):
+        '''
+        Takes as arguments a bassNote and a corresponding notationString. 
+        It adds the elements as a tuple to a figuredBassList. Also, calls 
+        addLyricsToBassNote. If no notationString is provided, the default 
+        is an empty string. ("5, 3")
+        '''
         if self.addNotationAsLyrics:
             addLyricsToBassNote(bassNote, notationString)
         self.bassNotes.append(bassNote)
@@ -135,40 +190,50 @@ class FiguredBass(object):
         self.figuredBassList.append((bassNote, notationString))
 
     def realize(self):
-        startTime = time.time()
+        '''
+        Initializes the realization of the inputted bassNote and notationString pairs. 
+        Realization happens in the order in which each pair was provided. A StartSegment 
+        is created for the first bassNote, and a MiddleSegment for each subsequent bassNote. 
+        Then, trimAllMovements is called on the last MiddleSegment. If successful, an 
+        isRealized flag is set to True.
+        '''
+        startTime = time.clock()
         self.isRealized = False
         self.allSegments = []
         self.lastSegment = None
         
         (startBass, startNotationString) = self.figuredBassList[0]
-        #print("Finding starting possibilities for: " + str((startBass.pitch, startNotationString)))
         a1 = segment.StartSegment(self.fbScale, self.fbParts, self.fbRules, startBass, startNotationString)
         self.allSegments.append(a1)
         self.lastSegment = a1
             
         for fbIndex in range(1, len(self.figuredBassList)):
             (nextBass, nextNotationString) = self.figuredBassList[fbIndex]
-            #print("Finding all possibilities for: " + str((nextBass.pitch, nextNotationString)))
             c1 = segment.MiddleSegment(self.fbScale, self.fbParts, self.fbRules, self.lastSegment, nextBass, nextNotationString)
             self.allSegments.append(c1)
             self.lastSegment = c1
        
-        #print("Trimming movements...")
         self.lastSegment.trimAllMovements()
-        numRealizations = self.lastSegment.getNumSolutions()
-        #if numRealizations <= 200000:
-            #numberProgressions = self.getAllIndexProgressions()
-            #print("Number of complete realizations, as calculated empirically: " + str(len(numberProgressions)) + ".")
-        #print("Solving complete. Number of complete realizations, as calculated by path counting: " + str(numRealizations) + ".\n")
-        endTime = time.time()
-        minutesElapsed = int((endTime - startTime) / 60)
-        secondsElapsed = round((endTime - startTime) % 60)
-        #print("Time elapsed: " + str(minutesElapsed) + " minutes " + str(secondsElapsed) + " seconds.")
         self.isRealized = True
-      
+        endTime = time.clock()
+        self.timeElapsed = datetime.timedelta(seconds = endTime - startTime)
+
+    def getNumSolutions(self):
+        return self.lastSegment.getNumSolutions()
+        
     # METHODS FOR GENERATION OF INDEX PROGRESSIONS
     # --------------------------------------------
+    '''
+    There are methods used to retrieve index progressions. An index progression 
+    is a list which represents a valid progression through a sequence of Segment 
+    instances, with an index in the list representing the location of a Possibility 
+    within a corresponding Segment's possibilities. Derived by iteration through 
+    each Segment's nextMovements.
+    '''
     def getAllIndexProgressions(self):
+        '''
+        Returns all the valid index progressions.
+        '''
         if len(self.allSegments) <= 1:
             raise FiguredBassException("One segment or less not enough for progression generator..")
         currMovements = self.allSegments[0].nextMovements
@@ -192,6 +257,9 @@ class FiguredBass(object):
         return progressions
     
     def getRandomIndexProgression(self):
+        '''
+        Returns a random valid index progression.
+        '''
         if len(self.allSegments) <= 1:
             raise FiguredBassException("One segment or less not enough for progression generator.")
         indexProgression = []
@@ -209,7 +277,15 @@ class FiguredBass(object):
         
     # METHODS FOR GENERATION OF POSSIBILITY PROGRESSIONS
     # --------------------------------------------------
+    '''
+    There are subsequent methods for retrieving Possibility progressions. 
+    A Possibility progression is a translation of an index progression 
+    derived by correlating indices with the appropriate Segment's possibilities.
+    '''
     def indexToPossibilityProgression(self, indexProgression):
+        '''
+        Takes as input a valid index progression and returns a valid Possibility progression.
+        '''
         possibilityProgression = []
         
         for segmentIndex in range(len(self.allSegments)):
@@ -219,6 +295,9 @@ class FiguredBass(object):
         return possibilityProgression
    
     def getAllPossibilityProgressions(self):
+        '''
+        Returns all the valid Possibility progressions.
+        '''
         possibilityProgressions = []
         indexProgressions = self.getAllIndexProgressions()
 
@@ -229,12 +308,24 @@ class FiguredBass(object):
         return possibilityProgressions
     
     def getRandomPossibilityProgression(self):
+        '''
+        Returns a random valid Possibility progression.
+        '''
         indexProgression = self.getRandomIndexProgression()
         possibilityProgression = self.indexToPossibilityProgression(indexProgression)
         return possibilityProgression
 
     # METHODS FOR MUSIC21.STREAM SCORE GENERATION FROM POSSILITY PROGRESSIONS
     # -----------------------------------------------------------------------
+    '''
+    The last of the methods compile the results for easy display to the user. 
+    A solution is defined as a valid Possibility progression translated to a 
+    music21.stream Score with two staves. The bottom staff contains the bass line, 
+    while the top staff contains pitches of the upper parts as a music21.chord Chord. 
+    There are methods for generation of solutions. Corresponding methods for display 
+    call show on the results, which are then displayed in default external software, 
+    such as MuseScore or Finale.
+    '''
     def generateRealizationFromPossibilityProgression(self, possibilityProgression):
         '''
         Generates a solution as a stream.Score() given a chord progression
@@ -332,6 +423,10 @@ class FiguredBass(object):
     # METHOD FOR CONSOLE PRINTING OF POSSIBILITY PROGRESSION
     # ------------------------------------------------------
     def printpossibilityProgression(self, possibilityProgression):
+        '''
+        Takes in a Possibility progression as an argument and prints 
+        it to the console. It is useful for debugging.
+        '''
         linesToPrint = []
         for p in self.partList:
             partLine = p.label + ":\n"
