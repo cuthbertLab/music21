a4a52791f686b0bd5062929197b7ce6591c6b594
Jose Cabal-Ugaz <jcabalugaz@gmail.com>
2011-05-29 17:10:30 +0000

    Updated with necessary modules

--- music21/figuredBass/__init__.py
+++ music21/figuredBass/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 
-__all__ = ['realizer', 'realizerScale', 'notation', 'resolution', 'rules', 'examples']
+__all__ = ['realizer', 'realizerScale', 'notation', 'resolution', 'rules', 'examples',
+           'fbPitch', 'possibility', 'segment']
 
 import realizer
 import notation
@@ -9,6 +10,9 @@ import realizerScale
 import resolution
 import rules
 import examples
+import fbPitch
+import possibility
+import segment
 
 
 #------------------------------------------------------------------------------
