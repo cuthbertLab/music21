c1fde778ff810d299beff2e9f3c2b60eef6a3b45
Christopher Ariza <christopher.ariza@gmail.com>
2011-07-13 21:33:06 +0000

    updated test

--- music21/interval.py
+++ music21/interval.py
@@ -2522,7 +2522,7 @@ class Test(unittest.TestCase):
         self.assertEqual(str(i), '<music21.interval.Interval A1>')
 
         i = interval.notesToInterval(note.Note('c4'), note.Note('c~4'))
-        self.assertEqual(str(i), '<music21.interval.Interval A1>')
+        self.assertEqual(str(i), '<music21.interval.Interval A1 (-50c)>')
 
 
 
