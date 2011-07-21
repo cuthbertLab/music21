88807f998f3a0770575956a34355b065b18cb627
Christopher Ariza <christopher.ariza@gmail.com>
2011-07-21 19:12:36 +0000

    updated utf-8 encoding

--- music21/note.py
+++ music21/note.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
 # Name:         note.py
 # Purpose:      music21 classes for representing notes
