cfb26e164678551e431e468a33ed37dcc5acb341
Beth Hadley <bethmhadley@gmail.com>
2011-11-06 14:59:51 +0000

    hack.py: 2nd commit to this hackish file for hack day development of lead sheet chord symbol realizations and checker

--- music21/demos/bhadley/hack.py
+++ music21/demos/bhadley/hack.py
@@ -53,7 +53,7 @@ def getPitches(chordSymbolObject):
     elif kind == 'augmented':
         romanString =  'I+'
     elif kind == 'diminished':
-        romanString =  'io'
+        romanString = 'io'
     elif kind == 'dominant':
         romanString = 'I-7'
     elif kind == 'major-seventh':
@@ -184,7 +184,7 @@ def getInversion(ChordSymbolObject):
 
 def majorOrMinor(xmltypeString):
     kind = xmltypeString
-    
+    tonality = ''
     if kind == 'major':
         tonality =  'major'
     elif kind == 'minor':
@@ -206,10 +206,30 @@ def majorOrMinor(xmltypeString):
     elif kind == 'half-diminished':
         tonality = 'minor'
     elif kind == 'major-minor':
+        tonality = 'minor'
+    elif kind == 'major-sixth':
         tonality = 'major'
-    
+    elif kind == 'minor-sixth':
+        tonality = 'minor'
+    elif kind == 'dominant-ninth':
+        tonality = 'major'
+    elif kind == 'major-ninth':
+        tonality = 'major'
+    elif kind == 'minor-ninth':
+        tonality = 'minor'
+    elif kind == 'dominant-13th':
+        tonality = 'major'
+    elif kind == 'major-13th':
+        tonality = 'major'
+    elif kind == 'minor-13th':
+        tonality = 'minor'
+    elif kind == 'suspended-second':
+        tonality = 'major'
+    elif kind == 'suspended-fourth':
+        tonality = 'major'
+    else:
+        tonality = 'major'
     return tonality
-
 
 def getNotationString(chordSymbolObject):
     notationString = ""
@@ -217,6 +237,7 @@ def getNotationString(chordSymbolObject):
     kind = chordSymbolObject.kind
     root = chordSymbolObject.root
     bass = chordSymbolObject.bass
+    
     if kind == 'major':
         notationString =  ''
     elif kind == 'minor':
@@ -224,51 +245,122 @@ def getNotationString(chordSymbolObject):
     elif kind == 'augmented':
         notationString =  '3,#5'
     elif kind == 'diminished':
-        notationString =  '-'
+        notationString =  '3,-5'
     elif kind == 'dominant':
         notationString = '-7'
     elif kind == 'major-seventh':
         notationString = '7'
     elif kind == 'minor-seventh':
-        notationString = '-7'
+        notationString = '7'
     elif kind == 'diminished-seventh':
-        notationString = ''
+        notationString = '3,-5,-7'
     elif kind == 'augmented-seventh':
-        notationString = ''
+        notationString = '3,#5,-7'
     elif kind == 'half-diminished':
-        notationString = ''
+        notationString = '3,-5,7'
     elif kind == 'major-minor':
+        notationString = '#7'
+    elif kind == 'major-sixth':
+        notationString = '3,5,6'
+    elif kind == 'minor-sixth':
+        notationString = '3,5,6'
+    elif kind == 'dominant-ninth':
+        notationString = '3,5,-7,9'
+    elif kind == 'major-ninth':
+        notationString = '3,5,7,9'
+    elif kind == 'minor-ninth':
+        notationString = '3,5,7,9'
+    elif kind == 'dominant-11':
+        notationString = '3,5,-7,9,11'
+    else:
         notationString = ''
 
     return notationString
 
+'''
+    dominant-11th (dominant-ninth, perfect 11th)
+    major-11th (major-ninth, perfect 11th)
+    minor-11th (minor-ninth, perfect 11th)
+13ths (usually as the basis for alteration):
+    dominant-13th (dominant-11th, major 13th)
+    major-13th (major-11th, major 13th)
+    minor-13th (minor-11th, major 13th)
+Suspended:
+    suspended-second (major second, perfect fifth)
+    suspended-fourth (perfect fourth, perfect fifth)
+Functional sixths:
+    Neapolitan
+    Italian
+    French
+    German
+Other:
+    pedal (pedal-point bass)
+    power (perfect fifth)
+    Tristan'''
     
-def correctChordSymbols(music21piece1, music21piece2):  
+def correctChordSymbols(music21piece1, music21piece2): 
+    numCorrect = 0 
     chords1 = music21piece1.flat.getElementsByClass(music21.chord.Chord)
-    chords2 = music21piece2.flat.getElementsByClass(music21.chord.Chord)
+    totalNumChords = len(chords1)
+    chordLine = music21piece2.getElementsByClass(music21.stream.Part)[1]
+    chords2 = chordLine.flat.getElementsByClass([music21.chord.Chord, music21.note.Note])
+    print 'here'
+    chords2.show('text')
     #1 is correct
     #2 is incorrect
-    
+    isCorrect = False
     for chord1, chord2 in zip(chords1, chords2):
+        print chord2.classes
+        if not('Chord' in chord2.classes):      
+            chord2.lyric = "NOT A CHORD"
+            continue
         newPitches = []
         for x in chord2.pitches:
             newPitches.append(str(x.name))
         for pitch in chord1:
-            if not (pitch.name in newPitches):
-                chord2.lyric = "WRONG"
+            print 'from', pitch.name
+            print 'to', newPitches
+            if pitch.name in newPitches:
+                isCorrect = True
+            else:
+                isCorrect = False
+                break
+        if isCorrect == True:
+            newPitches1 = []
+            for y in chord1.pitches:
+                newPitches1.append(str(y.name))
+            p = chord1.sortDiatonicAscending()
+            o =  chord2.sortDiatonicAscending()
+            print 'this', p.pitches
+            print 'that', o.pitches
+            foolist = []
+            boolist = []
+            for d in p.pitches:
+                foolist.append(str(d.name))
+            for k in o.pitches:
+                boolist.append(str(k.name))
+            print foolist
+            print boolist
+            if foolist != boolist:
+                chord2.lyric = "INVERSION"
+            else:
+                numCorrect = numCorrect + 1
+                chord2.lyric = ":)"
+        if isCorrect == False:
+            chord2.lyric = "PITCHES"
 
+    percentCorrect =  float(numCorrect)/float(totalNumChords) * 100
     s = music21piece2.getElementsByClass(music21.stream.PartStaff)[1]
     s.insert(0, music21.clef.BassClef())
     newScore = music21.stream.Score()
     newScore.append(music21piece2.getElementsByClass(music21.stream.PartStaff)[0])
     newScore.append(s)
    
-    return newScore #student's corrected score
+    return (newScore, percentCorrect) #student's corrected score
     
 
 def realizePitches(harmonyChordSymbolObject):
     x = harmonyChordSymbolObject
-    print x.duration
     #print x.kind, x.root, x.bass, x.inversion
     #print majorOrMinor(x.kind)
     #pitches = getPitches(x) my old way
@@ -318,11 +410,30 @@ def realizePitches(harmonyChordSymbolObject):
 if __name__ == '__main__':     
     #import doctest
     #doctest.testmod()
+    '''WIKIFONIA SPECIFIC
+    nicePiece = music21.converter.parse('C:\\Users\\bhadley\\Dropbox\\UROP\\data\\Databases\\wikifoniawithchords\\wikifonia-1028.mxl')
 
-    nicePiece = music21.converter.parse('C:\Users\sample.xml')
-    incorrectPiece = music21.converter.parse('C:\\Users\\bhadley\\Documents\\hack\incorrect.xml')
+    #nicePiece.show('text')
+    j = music21.harmony.getDuration(nicePiece)
+    onlyChordSymbols = j.flat.getElementsByClass(music21.harmony.ChordSymbol)
+    newStream = music21.stream.Part()
+    newStream.append(music21.clef.BassClef())
+    for chordSymbol in onlyChordSymbols:
+        newStream.append(realizePitches(chordSymbol))
+
+    newStream = newStream.makeMeasures()
+    nicePiece.insert(0,newStream)
+    nicePiece.show()
+    '''#NOTEFLIGHT SPECIFIC
+    nicePiece = music21.converter.parse('C:\\Users\\bhadley\\Documents\\hack\\try11.xml')
+    incorrectPiece = music21.converter.parse('C:\\Users\\bhadley\\Documents\\hack\\try11.xml')
+
+    #incorrectPiece = music21.converter.parse('C:\Users\sample.xml')
     
     sopranoLine = nicePiece.getElementsByClass(music21.stream.PartStaff)[0]
+    chordLine = nicePiece.getElementsByClass(music21.stream.Part)[1]
+    #chordLine.show('text')
+    #bassLine = nicePiece.part(2)
     s = music21.harmony.getDuration(sopranoLine)
     onlyChordSymbols = s.flat.getElementsByClass(music21.harmony.ChordSymbol)
     newStream = music21.stream.PartStaff()
@@ -334,12 +445,13 @@ if __name__ == '__main__':
     
     answerKey.insert(0,newStream)
 
-    correctedAssignment = correctChordSymbols(answerKey, incorrectPiece)
-
+    correctedAssignment, numCorrect = correctChordSymbols(answerKey, incorrectPiece)
     correctedAssignment.show('text')
     answerKey.show('text')
-    answerKey.show()
+
     correctedAssignment.show()
+    answerKey.show()    
+    print numCorrect
     
     
 '''Triads:
