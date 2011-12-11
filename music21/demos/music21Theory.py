9a80f92b07a2400b9bb7a5289b98c5bbc2bb57fb
Beth Hadley <bethmhadley@gmail.com>
2011-12-11 20:11:02 +0000

    Temporary home of music21 theory checker project code for IAP project

---
+++ music21/demos/music21Theory.py
@@ -0,0 +1,22 @@
+'''
+    The temporary home of all music21 theory checking methods that interface with 
+    a museScore plugin to provide feedback to students and teachers.
+    
+    This project is under developmental status.
+    
+'''
+
+
+import music21
+
+
+def checker(s):
+    print s
+    
+    
+
+
+
+
+if __name__ == "__main__":
+    checker('example')
