e5bef13d97798a36aa28864d5b2fa4076bedd609
Jose Cabal-Ugaz <jcabalugaz@gmail.com>
2012-02-03 16:44:11 +0000

    Updated keyboardPartsToBraille and partToBraille, new support for inaccords

--- music21/braille/translate.py
+++ music21/braille/translate.py
@@ -12,6 +12,7 @@
 Objects for exporting music21 data as braille.
 '''
 
+import itertools
 import music21
 import unittest
 
@@ -25,9 +26,6 @@ from music21.braille import segment
 # music21 streams to BrailleText objects.
 
 def measureToBraille(music21Measure, **measureKeywords):
-    '''
-    
-    '''
     if not 'showHeading' in measureKeywords:
         measureKeywords['showHeading'] = False
     if not 'showFirstMeasureNumber' in measureKeywords:
@@ -41,7 +39,7 @@ def partToBraille(music21Part, **partKeywords):
     allSegments = segment.findSegments(music21Part, **partKeywords)
     allTrans = []
     for brailleElementSegment in allSegments:
-        segmentTranscription = segment.transcribeSegment(brailleElementSegment, **partKeywords)
+        segmentTranscription = brailleElementSegment.transcribe()
         allTrans.append(str(segmentTranscription))
     return u"\n".join(allTrans)
 
@@ -51,39 +49,54 @@ def keyboardPartsToBraille(music21PartUpper, music21PartLower, **keywords):
     into braille music bar over bar format.
     '''
     maxLineLength = 40
+    segmentBreaks = None
     if 'maxLineLength' in keywords:
         maxLineLength = keywords['maxLineLength']
+    if 'segmentBreaks' in keywords:
+        segmentBreaks = keywords['segmentBreaks']
 
-    bt = text.BrailleKeyboard(maxLineLength)
-    rhSegment = segment.findSegments(music21PartUpper, showHand = 'right')[0]
-    lhSegment = segment.findSegments(music21PartLower, showHand = 'left')[0]
-    
-    try:
-        brailleHeadingR = segment.extractHeading(rhSegment, maxLineLength)
-        brailleHeadingL = segment.extractHeading(lhSegment, maxLineLength)
-        bt.addElement(heading = brailleHeadingR)
-    except segment.BrailleSegmentException:
-        pass
-
-    allGroupingKeysR = sorted(rhSegment.keys())
-    allGroupingKeysL = sorted(lhSegment.keys())
-
-    bt.highestMeasureNumberLength = len(str(allGroupingKeysR[-1] / 100))
-    
-    rhNoteGroupings = segment.extractNoteGroupings(rhSegment)
-    lhNoteGroupings = segment.extractNoteGroupings(lhSegment)
-    
-    while True:
+    rhSegments = segment.findSegments(music21PartUpper, showHand = 'right',\
+                                      maxLineLength = maxLineLength, segmentBreaks = segmentBreaks)
+    lhSegments = segment.findSegments(music21PartLower, showHand = 'left',\
+                                      maxLineLength = maxLineLength, segmentBreaks = segmentBreaks)
+    allBrailleText = []
+    for (rhSegment, lhSegment) in itertools.izip(rhSegments, lhSegments):
+        bt = text.BrailleKeyboard(maxLineLength)
         try:
-            (gkR, noteGroupingR) = rhNoteGroupings.pop(0)
-            (gkL, noteGroupingL) = lhNoteGroupings.pop(0)
-            rh_braille = segment.transcribeNoteGrouping(noteGroupingR)
-            lh_braille = segment.transcribeNoteGrouping(noteGroupingL)
-            bt.addElement(pair = (basic.numberToBraille(gkR / 100)[1:], rh_braille, lh_braille))
-        except:
-            break
+            brailleHeadingR = rhSegment.extractHeading()
+            brailleHeadingL = lhSegment.extractHeading()
+            bt.addElement(heading = brailleHeadingR)
+            allGroupingKeysR = rhSegment.allGroupingKeys
+            allGroupingKeysL = lhSegment.allGroupingKeys
+        except basic.BrailleBasicException as bbe:
+            if not bbe.args[0] == "No heading can be made.":
+                raise bbe
+            allGroupingKeysR = sorted(rhSegment.keys())
+            allGroupingKeysL = sorted(lhSegment.keys())
 
-    return bt
+        bt.highestMeasureNumberLength = len(str(allGroupingKeysR[-1] / 100))
+        
+        for (gkR, gkL) in itertools.izip(allGroupingKeysR, allGroupingKeysL):
+            if gkR / 100 != gkL / 100 or gkR % 10 < 8 or gkL % 10 < 8:
+                print "Cannot continue keyboard transcription: "
+                print (gkR, gkL)
+                return bt 
+            currentMeasureNumber = basic.numberToBraille(gkR / 100, withNumberSign=False)
+            if gkR % 10 == 8:
+                inaccords = rhSegment[gkR]
+                rh_braille = basic.symbols['full_inaccord'].join([segment.transcribeVoice(vc) for vc in inaccords])
+            else:
+                rh_braille = segment.transcribeNoteGrouping(rhSegment[gkR])
+            if gkL % 10 == 8:
+                inaccords = lhSegment[gkL]
+                lh_braille = basic.symbols['full_inaccord'].join([segment.transcribeVoice(vc) for vc in inaccords])
+            else:
+                lh_braille = segment.transcribeNoteGrouping(lhSegment[gkL])
+            bt.addNoteGroupings(currentMeasureNumber, lh_braille, rh_braille)
+        bt.makeNewLine()
+        allBrailleText.append(bt)
+        
+    return u"\n".join([str(bt) for bt in allBrailleText])
 
 #-------------------------------------------------------------------------------
 
