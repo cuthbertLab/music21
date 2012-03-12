6c80fcb4f18b03d270284d4a16bc727e5ba47d2a
José Cabal-Ugaz@gmail.com <jcabalugaz@gmail.com>
2012-03-12 18:09:30 +0000

    Expanded parsing methods

--- music21/braille/translation/parse.py
+++ music21/braille/translation/parse.py
@@ -1,5 +1,5 @@
-import inspect
 import pprint
+import re
 
 from lxml import etree #need to install lxml
 from music21 import instrument
@@ -8,14 +8,17 @@ from music21.braille.translation import lookup
 #-------------------------------------------------------------------------------
 
 def music():
-    f = open('music.html')
+    """
+    http://www.music.vt.edu/musicdictionary/appendix/translations/Translations.html
+    """
+    f = open('websites/music.html')
     html = etree.HTML(f.read())
     
-    french = {}
-    german = {}
-    italian = {}
-    spanish = {}
-    abbr = {}
+    englishFrench = {}
+    englishGerman = {}
+    englishItalian = {}
+    englishSpanish = {}
+    englishAbbreviations = {}
     
     table = html.iter("tr")
     for row in table:
@@ -29,25 +32,14 @@ def music():
             allSols.append(items)
         for cell in allSols[0]:
             cell = u" ".join(cell.split())
-            french[cell] = [u" ".join(s.split()) for s in allSols[1]]
-            german[cell] = [u" ".join(s.split()) for s in allSols[2]]
-            italian[cell] = [u" ".join(s.split()) for s in allSols[3]]
-            spanish[cell] = [u" ".join(s.split()) for s in allSols[4]]
-            abbr[cell] = [u" ".join(s.split()) for s in allSols[5]]
+            englishFrench[cell] = [u" ".join(s.split()) for s in allSols[1]]
+            englishGerman[cell] = [u" ".join(s.split()) for s in allSols[2]]
+            englishItalian[cell] = [u" ".join(s.split()) for s in allSols[3]]
+            englishSpanish[cell] = [u" ".join(s.split()) for s in allSols[4]]
+            englishAbbreviations[cell] = [u" ".join(s.split()) for s in allSols[5]]
 
-    allClasses = [tup[1] for tup in inspect.getmembers(instrument, inspect.isclass) if issubclass(tup[1], instrument.Instrument)]
-    num = 0
-    for classDef in allClasses:
-        i = classDef()
-        try:
-            val = german[i.bestName().lower()]
-            #print i, val
-            num+=1
-        except:
-            pass
-        
-    allDicts = {"French": french, "German": german, "Italian": italian, "Spanish": spanish, "Abbreviations": abbr}
-    for dict in allDicts.values():
+    allDicts = [englishFrench, englishGerman, englishItalian, englishSpanish, englishAbbreviations]
+    for dict in allDicts:
         del dict[u'']
         for key in dict:
             while(True):
@@ -59,27 +51,30 @@ def music():
             if len(dict[key]) == 0:
                 del dict[key]
 
-    print "french = ", 
-    pprint.pprint(french)
-    print "\ngerman = ", 
-    pprint.pprint(german)
-    print "\nitalian = ", 
-    pprint.pprint(italian)
-    print "\nspanish = ", 
-    pprint.pprint(spanish)
-    print "\nabbreviations = ", 
-    pprint.pprint(abbr)
+    print "englishFrench = ", 
+    pprint.pprint(englishFrench)
+    print "\nenglishGerman = ", 
+    pprint.pprint(englishGerman)
+    print "\nenglishItalian = ", 
+    pprint.pprint(englishItalian)
+    print "\nenglishSpanish = ", 
+    pprint.pprint(englishSpanish)
+    print "\nenglishAbbreviations = ", 
+    pprint.pprint(englishAbbreviations)
 
 #-------------------------------------------------------------------------------
 
 def dolmetsch():
-    f = open('dolmetsch.html')
+    """
+    http://www.dolmetsch.com/musictheory29.htm#translatednames
+    """
+    f = open('websites/dolmetsch.html')
     html = etree.HTML(f.read())
     
-    french = {}
-    german = {}
-    italian = {}
-    spanish = {}
+    englishFrench = {}
+    englishGerman = {}
+    englishItalian = {}
+    englishSpanish = {}
     
     table = html.iter("tr")
     for row in table:
@@ -91,24 +86,13 @@ def dolmetsch():
             allSols.append([unicode(s.encode("latin_1")) for s in items])
         for cell in allSols[0]:
             cell = u" ".join(cell.split("(")[0].split())
-            italian[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[1]]
-            german[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[2]]
-            french[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[3]]
-            spanish[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[4]]
+            englishItalian[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[1]]
+            englishGerman[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[2]]
+            englishFrench[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[3]]
+            englishSpanish[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[4]]
 
-    allClasses = [tup[1] for tup in inspect.getmembers(instrument, inspect.isclass) if issubclass(tup[1], instrument.Instrument)]
-    num = 0
-    for classDef in allClasses:
-        i = classDef()
-        try:
-            val = german[i.bestName().lower()]
-            #print i, val
-            num+=1
-        except:
-            pass
-        
-    allDicts = {"French": french, "German": german, "Italian": italian, "Spanish": spanish}
-    for dict in allDicts.values():
+    allDicts = [englishFrench, englishGerman, englishItalian, englishSpanish]
+    for dict in allDicts:
         try:
             del dict[u'']
         except KeyError:
@@ -123,20 +107,316 @@ def dolmetsch():
             if len(dict[key]) == 0:
                 del dict[key]
 
-    merge(lookup.french, french)
-    merge(lookup.german, german)
-    merge(lookup.italian, italian)
-    merge(lookup.spanish, spanish)
+    merge(lookup.englishFrench, englishFrench)
+    merge(lookup.englishGerman, englishGerman)
+    merge(lookup.englishItalian, englishItalian)
+    merge(lookup.englishSpanish, englishSpanish)
     
-    print "french = ", 
-    pprint.pprint(french)
-    print "\ngerman = ", 
-    pprint.pprint(german)
-    print "\nitalian = ", 
-    pprint.pprint(italian)
-    print "\nspanish = ", 
-    pprint.pprint(spanish)
-        
+    print "englishFrench = ", 
+    pprint.pprint(englishFrench)
+    print "\nenglishGerman = ", 
+    pprint.pprint(englishGerman)
+    print "\nenglishItalian = ", 
+    pprint.pprint(englishItalian)
+    print "\nenglishSpanish = ", 
+    pprint.pprint(englishSpanish)
+
+#-------------------------------------------------------------------------------
+
+def dolmetsch2():
+    """
+    http://www.dolmetsch.com/musictheory29.htm#instrumentabbrev
+    """
+    f = open('websites/dolmetsch2.html')
+    html = etree.HTML(f.read())
+    
+    englishAbbreviations = {}
+    
+    table = html.iter("tr")
+    for row in table:
+        allCols = list(row.iter("td"))
+        allSols = []
+        for col in allCols:
+            items = [td.text.lower() for td in col.iter("td") if td.text is not None]
+            allSols.append([unicode(s.encode("latin_1")) for s in items])
+        for cell in allSols[0]:
+            cell = u" ".join(cell.split("(")[0].split())
+            items = []
+            for a in allSols[1]:
+                items.extend([x.strip() for x in a.split("(")[0].split(",")])
+            englishAbbreviations[cell] = items
+    
+    allDicts = [englishAbbreviations]
+    for dict in allDicts:
+        try:
+            del dict[u'']
+        except KeyError:
+            pass
+        for key in dict:
+            while(True):
+                try:
+                    dict[key].remove(u'')
+                except ValueError:
+                    break
+            while(True):
+                try:
+                    dict[key].remove(u'-')
+                except ValueError:
+                    break
+        for key in dict.keys():
+            if len(dict[key]) == 0:
+                del dict[key]
+
+    merge(lookup.englishAbbreviations, englishAbbreviations)
+    print "englishAbbreviations = ", 
+    pprint.pprint(englishAbbreviations)
+
+#-------------------------------------------------------------------------------
+
+def yale():
+    """
+    http://www.library.yale.edu/cataloging/music/instname.htm#wind
+    """
+    f = open('websites/yale.html')
+    html = etree.HTML(f.read())
+    
+    englishFrench = {}
+    englishGerman = {}
+    englishItalian = {}
+    englishSpanish = {}
+    englishRussian = {}
+    
+    table = html.iter("tr")
+    for row in table:
+        allCols = list(row.iter("td"))
+        allSols = []
+        for col in allCols:
+            items = []
+            for f in col.iter("font"):
+                if f.text is not None:
+                    items.extend(re.split("[,;/]",f.text.lower()))
+            for br in col.iter("br"):
+                if br.tail is not None:
+                    items.extend(re.split("[,;/]",br.tail.lower()))
+            allSols.append(items)
+        for cell in allSols[0]:
+            cell = u" ".join(cell.split("(")[0].split())
+            englishFrench[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[1]]
+            englishGerman[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[2]]
+            englishItalian[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[3]]
+            englishRussian[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[4]]
+            englishSpanish[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[5]]
+
+    allDicts = [englishFrench, englishGerman, englishItalian, englishSpanish, englishRussian]
+    for dict in allDicts:
+        try:
+            del dict[u'']
+        except KeyError:
+            pass
+        for key in dict:
+            while(True):
+                try:
+                    dict[key].remove(u'')
+                except ValueError:
+                    break
+        for key in dict.keys():
+            if len(dict[key]) == 0:
+                del dict[key]
+
+    merge(lookup.englishFrench, englishFrench)
+    merge(lookup.englishGerman, englishGerman)
+    merge(lookup.englishItalian, englishItalian)
+    merge(lookup.englishSpanish, englishSpanish)
+    merge(lookup.englishRussian, englishRussian)
+    
+    print "englishFrench = ", 
+    pprint.pprint(englishFrench)
+    print "\nenglishGerman = ", 
+    pprint.pprint(englishGerman)
+    print "\nenglishItalian = ", 
+    pprint.pprint(englishItalian)
+    print "\nenglishSpanish = ", 
+    pprint.pprint(englishSpanish)
+    print "\nenglishRussian = ", 
+    pprint.pprint(englishRussian)
+
+#-------------------------------------------------------------------------------
+
+def ukItalian():
+    """
+    http://www.musictheory.org.uk/res-musical-terms/italian-musical-terms.php
+    """
+    f = open('websites/ukItalian.html')
+    html = etree.HTML(f.read())
+    
+    englishItalian = {}
+    englishAbbreviations = {}
+
+    table = html.iter("tr")
+    for row in table:
+        allCols = list(row.iter("td"))
+        allSols = []
+        if len(allCols) == 0:
+            continue
+        for td in allCols[3].iter("td"):
+            if td.text is not None:
+                items = re.split("/;", td.text.lower())
+                allSols.append([unicode(s.encode("latin_1")) for s in items])
+        for td in allCols[2].iter("td"):
+            if td.text is not None:
+                items = re.split("/;", td.text.lower()[1:-1])
+                allSols.append([unicode(s.encode("latin_1")) for s in items])
+        for td in allCols[0].iter("td"):
+            if td.text is not None:
+                items = re.split("/;", td.text.lower())
+                allSols.append([unicode(s.encode("latin_1")) for s in items])
+
+        for cell in allSols[0]:
+            cell = u" ".join(cell.split("(")[0].split())
+            englishAbbreviations[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[1]]
+            englishItalian[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[2]]
+
+    allDicts = [englishItalian, englishAbbreviations]
+    for dict in allDicts:
+        try:
+            del dict[u'']
+        except KeyError:
+            pass
+        for key in dict:
+            while(True):
+                try:
+                    dict[key].remove(u'')
+                except ValueError:
+                    break
+        for key in dict.keys():
+            if len(dict[key]) == 0:
+                del dict[key]
+
+    merge(lookup.englishItalian, englishItalian)
+    merge(lookup.englishAbbreviations, englishAbbreviations)
+
+    print "englishItalian = ", 
+    pprint.pprint(englishItalian)
+    print "\nenglishAbbreviations = ", 
+    pprint.pprint(englishAbbreviations)
+
+#-------------------------------------------------------------------------------
+
+def ukGerman():
+    """
+    http://www.musictheory.org.uk/res-musical-terms/german-musical-terms.php
+    """
+    f = open('websites/ukGerman.html')
+    html = etree.HTML(f.read())
+    
+    englishGerman = {}
+    englishAbbreviations = {}
+
+    table = html.iter("tr")
+    for row in table:
+        allCols = list(row.iter("td"))
+        allSols = []
+        if len(allCols) == 0:
+            continue
+        for td in allCols[3].iter("td"):
+            if td.text is not None:
+                items = re.split("/;", td.text.lower())
+                allSols.append([unicode(s.encode("latin_1")) for s in items])
+        for td in allCols[2].iter("td"):
+            if td.text is not None:
+                items = re.split("/;", td.text.lower()[1:-1])
+                allSols.append([unicode(s.encode("latin_1")) for s in items])
+        for td in allCols[0].iter("td"):
+            if td.text is not None:
+                items = re.split("/;", td.text.lower())
+                allSols.append([unicode(s.encode("latin_1")) for s in items])
+
+        for cell in allSols[0]:
+            cell = u" ".join(cell.split("(")[0].split())
+            englishAbbreviations[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[1]]
+            englishGerman[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[2]]
+
+    allDicts = [englishGerman, englishAbbreviations]
+    for dict in allDicts:
+        try:
+            del dict[u'']
+        except KeyError:
+            pass
+        for key in dict:
+            while(True):
+                try:
+                    dict[key].remove(u'')
+                except ValueError:
+                    break
+        for key in dict.keys():
+            if len(dict[key]) == 0:
+                del dict[key]
+
+    merge(lookup.englishGerman, englishGerman)
+    merge(lookup.englishAbbreviations, englishAbbreviations)
+
+    print "englishGerman = ", 
+    pprint.pprint(englishGerman)
+    print "\nenglishAbbreviations = ", 
+    pprint.pprint(englishAbbreviations)
+
+#-------------------------------------------------------------------------------
+
+def ukFrench():
+    """
+    http://www.musictheory.org.uk/res-musical-terms/french-musical-terms.php
+    """
+    f = open('websites/ukFrench.html')
+    html = etree.HTML(f.read())
+    
+    englishFrench = {}
+    englishAbbreviations = {}
+
+    table = html.iter("tr")
+    for row in table:
+        allCols = list(row.iter("td"))
+        allSols = []
+        if len(allCols) == 0:
+            continue
+        for td in allCols[3].iter("td"):
+            if td.text is not None:
+                items = re.split("/;", td.text.lower())
+                allSols.append([unicode(s.encode("latin_1")) for s in items])
+        for td in allCols[2].iter("td"):
+            if td.text is not None:
+                items = re.split("/;", td.text.lower()[1:-1])
+                allSols.append([unicode(s.encode("latin_1")) for s in items])
+        for td in allCols[0].iter("td"):
+            if td.text is not None:
+                items = re.split("/;", td.text.lower())
+                allSols.append([unicode(s.encode("latin_1")) for s in items])
+
+        for cell in allSols[0]:
+            cell = u" ".join(cell.split("(")[0].split())
+            englishAbbreviations[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[1]]
+            englishFrench[cell] = [u" ".join(s.split("(")[0].split()) for s in allSols[2]]
+
+    allDicts = [englishFrench, englishAbbreviations]
+    for dict in allDicts:
+        try:
+            del dict[u'']
+        except KeyError:
+            pass
+        for key in dict:
+            while(True):
+                try:
+                    dict[key].remove(u'')
+                except ValueError:
+                    break
+        for key in dict.keys():
+            if len(dict[key]) == 0:
+                del dict[key]
+
+    merge(lookup.englishFrench, englishFrench)
+
+    print "englishFrench = ", 
+    pprint.pprint(englishFrench)
+
 #-------------------------------------------------------------------------------
 
 def merge(source, target):
@@ -151,8 +431,17 @@ def merge(source, target):
                 if not item in target[srcKey]:
                     target[srcKey].append(item)
 
+#-------------------------------------------------------------------------------
+
+def allToEnglish():
+    allDicts = [lookup.englishFrench,lookup.englishGerman,lookup.englishAbbreviations,
+                lookup.englishItalian,lookup.englishSpanish, lookup.englishRussian]
+    masterDict = {}
+    pass
+
 if __name__ == "__main__":
-    dolmetsch()
+    #allToEnglish()
+    #print lookup.englishFrench["a double sharp"]
 
 #------------------------------------------------------------------------------
 # eof
