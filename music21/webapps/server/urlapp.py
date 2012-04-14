12246ad8e9a27f7752eac99ac7b8eba69ea9c807
Lars Johnson <lrsjohnson@gmail.com>
2012-04-14 05:09:54 +0000

    Added url app that returns xml of manipulated sections of corpus files

--- music21/webapps/server/urlapp.py
+++ music21/webapps/server/urlapp.py
@@ -29,7 +29,77 @@ See docs for music21.webapps for specifications about the JSON string structure
 '''
 
 from music21 import webapps
-import cgitb
-cgitb.enable()
+from music21 import corpus
 
-application = webapps.music21ModWSGIURLApplication
+def music21ModWSGICorpusURLApplication(environ, start_response):
+    '''
+    Application function in proper format for a MOD-WSGI Application:
+    Reads the contents of a post request, and passes the JSON string to
+    webapps.processJSONData for further processing. 
+    Returns a proper HTML response.
+    
+    '''
+    status = '200 OK'
+
+    pathInfo = environ['PATH_INFO'] # Contents of path after mount point of wsgi app but before question mark
+    queryString = environ['QUERY_STRING'] # Contents of URL after question mark    
+    documentRoot = environ['DOCUMENT_ROOT'] # Document root of the server
+    
+    pathParts = pathInfo.split("/")
+    
+    returnType = "text"
+    
+    resultStr = str(pathParts)
+    
+    if len(pathParts) > 1 and pathParts[1] in ['corpusParse','corpusReduce']:
+        try:
+            sc = corpus.parse(pathParts[2]).measures(int(pathParts[3]),int(pathParts[4]))
+            if pathParts[1] == 'corpusReduce':
+                sc = reduction(sc)
+            resultStr = sc.musicxml
+            returnType = "xml"
+            filename = str(pathParts[2])+".m"+str(pathParts[3])+"-"+str(pathParts[4])+"-reduced.xml"
+        except:
+            try:
+                sc = corpus.parse(pathParts[2])
+                if pathParts[1] == 'corpusReduce':
+                    sc = reduction(sc)
+                resultStr = sc.musicxml 
+                returnType = "xml"
+                filename = str(pathParts[2])+"-reduced.xml"
+            except Exception as e:
+                resultStr = "Error parsing file "+str(e) + ''
+
+
+    else:
+        resultStr = "Try calling a url of the form:"
+        resultStr += "\n\n  /music21/urlinterface/corpusParse/<WORKNAME> to download a musicxml file of the specified corpus file."
+        resultStr += "\n\n  /music21/urlinterface/corpusParse/<WORKNAME>/<MEASURE_START>/<MEASURE_END> to download a musicxml file of the specified measures of a corpus file."
+        resultStr += "\n\n  /music21/urlinterface/corpusReduce/<WORKNAME> to download an annotated, chordified reduction of the specified corpus file."
+        resultStr += "\n\n  /music21/urlinterface/corpusReduce/<WORKNAME>/<MEASURE_START>/<MEASURE_END> to download an annotated, chordified reduction of the specified measures of a corpus file."
+
+    if returnType == "xml":
+        response_headers = [('Content-type', 'application/vnd.recordare.musicxml+xml'),('Content-disposition','attachment; filename='+filename),
+            ('Content-Length', str(len(resultStr)))]
+
+        start_response(status, response_headers)
+    
+        return [resultStr]
+        
+    else:
+        response_headers = [('Content-type', 'text/plain'),
+                ('Content-Length', str(len(resultStr)))]
+    
+        start_response(status, response_headers)
+    
+        return [resultStr]
+
+application = music21ModWSGICorpusURLApplication
+
+def reduction(sc):
+    reductionStream = sc.chordify()
+    for c in reductionStream.flat.getElementsByClass('Chord'):
+        c.closedPosition(forceOctave=4, inPlace=True)
+        c.removeRedundantPitches(inPlace=True)
+        c.annotateIntervals()
+    return reductionStream
