214e9323e88f6b048332d8c82091f5d11ba8503a
Lars Johnson <lrsjohnson@gmail.com>
2012-04-15 04:32:49 +0000

    updates to urlapp.py that allows results to be returned in a noteflight embed as an alternative to a downloadable xml file

--- music21/webapps/server/urlapp.py
+++ music21/webapps/server/urlapp.py
@@ -43,13 +43,13 @@ def music21ModWSGICorpusURLApplication(environ, start_response):
 
     pathInfo = environ['PATH_INFO'] # Contents of path after mount point of wsgi app but before question mark
     queryString = environ['QUERY_STRING'] # Contents of URL after question mark    
-    documentRoot = environ['DOCUMENT_ROOT'] # Document root of the server
+    returnType = queryString
     
     pathParts = pathInfo.split("/")
     
-    returnType = "text"
+    error = False
     
-    resultStr = str(pathParts)
+    resultStr = ""
     
     if len(pathParts) > 1 and pathParts[1] in ['corpusParse','corpusReduce']:
         try:
@@ -57,43 +57,78 @@ def music21ModWSGICorpusURLApplication(environ, start_response):
             if pathParts[1] == 'corpusReduce':
                 sc = reduction(sc)
             resultStr = sc.musicxml
-            returnType = "xml"
+            title = pathParts[2]+": Measures "+str(pathParts[3])+"-"+str(pathParts[4])
             filename = str(pathParts[2])+".m"+str(pathParts[3])+"-"+str(pathParts[4])+"-reduced.xml"
         except:
             try:
-                sc = corpus.parse(pathParts[2])
+                sc = corpus.parse(pathParts[2]).measures(int(pathParts[3]),int(pathParts[3]))
                 if pathParts[1] == 'corpusReduce':
                     sc = reduction(sc)
-                resultStr = sc.musicxml 
-                returnType = "xml"
-                filename = str(pathParts[2])+"-reduced.xml"
-            except Exception as e:
-                resultStr = "Error parsing file "+str(e) + ''
-
-
+                resultStr = sc.musicxml
+                title = pathParts[2]+": Measure "+str(pathParts[3])
+                filename = str(pathParts[2])+".m"+str(pathParts[3])+"-reduced.xml"
+            except:
+                try:
+                    sc = corpus.parse(pathParts[2])
+                    if pathParts[1] == 'corpusReduce':
+                        sc = reduction(sc)
+                    resultStr = sc.musicxml 
+                    title = pathParts[2]
+                    filename = str(pathParts[2])+"-reduced.xml"
+                except Exception as e:
+                    resultStr = "Error parsing file "+str(e) + '\n\n'
+                    error = True
     else:
-        resultStr = "Try calling a url of the form:"
-        resultStr += "\n\n  /music21/urlinterface/corpusParse/<WORKNAME> to download a musicxml file of the specified corpus file."
+        error = True
+
+    if error:
+        resultStr += "Try calling a url of the form:"
+        resultStr += "\n  /music21/urlinterface/corpusParse/<WORKNAME> to download a musicxml file of the specified corpus file."
+        resultStr += "\n\n  /music21/urlinterface/corpusParse/<WORKNAME>/<MEASURE_NUM> to download a musicxml file of the specified measure of a corpus file."
         resultStr += "\n\n  /music21/urlinterface/corpusParse/<WORKNAME>/<MEASURE_START>/<MEASURE_END> to download a musicxml file of the specified measures of a corpus file."
         resultStr += "\n\n  /music21/urlinterface/corpusReduce/<WORKNAME> to download an annotated, chordified reduction of the specified corpus file."
+        resultStr += "\n\n  /music21/urlinterface/corpusReduce/<WORKNAME>/<MEASURE_NUM> to download an annotated, chordified reduction of the specified measure of a corpus file."
         resultStr += "\n\n  /music21/urlinterface/corpusReduce/<WORKNAME>/<MEASURE_START>/<MEASURE_END> to download an annotated, chordified reduction of the specified measures of a corpus file."
-
-    if returnType == "xml":
-        response_headers = [('Content-type', 'application/vnd.recordare.musicxml+xml'),('Content-disposition','attachment; filename='+filename),
-            ('Content-Length', str(len(resultStr)))]
-
-        start_response(status, response_headers)
-    
-        return [resultStr]
+        resultStr += "\n\n\nSpecifying Return Format:"
+        resultStr += "\n  The return format will default to a Noteflight embed that will load the resulting score."
+        resultStr += "\n\n  Add ?xmltext to the end of the url to display the xmltext of the score as plain text"
+        resultStr += "\n\n  Add ?xml to the end of the url to download the result as a .xml file"
+        error = True
         
-    else:
         response_headers = [('Content-type', 'text/plain'),
                 ('Content-Length', str(len(resultStr)))]
     
         start_response(status, response_headers)
     
         return [resultStr]
-
+    
+    else:
+        if returnType == "xml":
+            
+            response_headers = [('Content-type', 'application/vnd.recordare.musicxml+xml'),('Content-disposition','attachment; filename='+filename),
+                ('Content-Length', str(len(resultStr)))]
+    
+            start_response(status, response_headers)
+        
+            return [resultStr]
+        
+        elif returnType == "xmltext":
+            
+            response_headers = [('Content-type', 'text/plain'),
+                ('Content-Length', str(len(resultStr)))]
+    
+            start_response(status, response_headers)
+        
+            return [resultStr]
+        
+        else:
+            templateStr = noteflightEmbedTemplate(resultStr,title)
+            response_headers = [('Content-type', 'text/html'),('Content-Length', str(len(templateStr)))]
+    
+            start_response(status, response_headers)
+        
+            return [templateStr]
+            
 application = music21ModWSGICorpusURLApplication
 
 def reduction(sc):
@@ -103,3 +138,46 @@ def reduction(sc):
         c.removeRedundantPitches(inPlace=True)
         c.annotateIntervals()
     return reductionStream
+
+def noteflightEmbedTemplate(musicxml, title):
+    musicxml = musicxml.replace('\n','')
+    musicxml = musicxml.replace('\'','\\\'')
+    htmlData = """
+<html>
+<head>
+<title>Music21 URL App Response</title>
+<script language="javascript" src="http://ciconia.mit.edu/music21/webapps/client/javascript/music21.js"></script>
+<script>
+    // Event handling function
+    function noteflightEventHandler(e)
+    {
+        if(e.type == 'scoreDataLoaded') {
+            m21.noteflight.sendMusicXMLToNoteflightEmbed('nfscore','"""
+    htmlData += musicxml
+    htmlData +="""')
+        }
+    }
+</script>
+<script language="javascript">
+m21 = new Music21interface();
+
+function setup() {
+    m21.noteflight.createNoteflightEmbed('noteflightembed','nfscore','fc79df30896da6aa03f90ff771015913ca6880be',800,450,1.0);
+}
+</script>
+
+</head>
+<body onload="setup()">
+
+<h1>"""
+    htmlData += title
+    htmlData +="""
+</p> 
+<div id="noteflightembed">
+</div>
+
+
+</body>
+</html>
+"""
+    return htmlData
