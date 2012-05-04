964713bf0795d77c475f5389f358260e3fcd9fd5
Michael Scott Cuthbert <cuthbert@mit.edu>
2012-05-04 17:54:57 +0000

    moved Jose's braille translation routines...

--- obsolete/translation/__init__.py
+++ obsolete/translation/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
+'''
+Files to download and create tables for automatic language translation.  Not needed for music21's normal 
+operation, since this is used in other places...
+
+'''
 
 __all__ = ['tlnLookup',
            'tlnParse',
