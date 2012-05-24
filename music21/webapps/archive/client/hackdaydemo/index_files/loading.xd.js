60d2c87d9d0a32a77f2c35b51af93867154605a8
Lars Johnson <lrsjohnson@gmail.com>
2012-05-24 16:27:17 +0000

    Hackday demo is not on ciconia

--- music21/webapps/archive/client/hackdaydemo/index_files/loading.xd.js
+++
@@ -1,4 +0,0 @@
-window[(typeof(djConfig)!="undefined"&&djConfig.scopeMap&&djConfig.scopeMap[0][1])||"dojo"]._xdResourceLoaded(function(dojo, dijit, dojox){
-return {depends: [["provide", "dijit.nls.loading"]],
-defineResource: function(dojo, dijit, dojox){dojo.provide("dijit.nls.loading");dojo._xdLoadFlattenedBundle("dijit", "loading", "", {"loadingState":"Loading...","errorState":"Sorry, an error occurred"});
-}};});
