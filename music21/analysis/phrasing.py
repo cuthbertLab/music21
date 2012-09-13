3f6dd6faee09431205d1649177226ecc8ab8c733
Michael Scott Cuthbert <cuthbert@mit.edu>
2012-09-13 23:22:28 +0000

    shell of a phrasing module for Dylan

---
+++ music21/analysis/phrasing.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:         analysis/phrasing.py
+# Purpose:      
+#
+# Authors:      Dylan Nagler
+#               Michael Scott Cuthbert
+#
+# Copyright:    Copyright © 2012 Michael Scott Cuthbert and the music21 Project
+# License:      LGPL, see license.txt
+#-------------------------------------------------------------------------------
+
+
+# Group elements that are beamed together...
+
+# Group elements within integer .beat property.
+
+
+
+#### Not in here but in stream, but put it here for now:
+#  emptyStream = copyStreamHierarchy(streamIn)
+#  
+
