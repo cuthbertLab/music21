ca7fe37ff84df0fac954bfc445707e19607ebb30
Maura Church <maura.church@gmail.com>
2012-12-04 16:05:09 +0000

    added neoRiemannian to analysis

--- music21/analysis/__init__.py
+++ music21/analysis/__init__.py
@@ -1,10 +1,11 @@
-__ALL__ = ['correlate', 'windowed', 'metrical', 'discrete', 'search', 'patel']
+__ALL__ = ['correlate', 'windowed', 'metrical', 'neoRiemannian', 'discrete', 'search', 'patel']
 
 # this is necessary to get these names available with a 
 # from music21 import * import statement
 from music21.analysis import correlate
 from music21.analysis import discrete
 from music21.analysis import metrical
+from music21.analysis import neoRiemannian
 from music21.analysis import patel
 from music21.analysis import reduction
 from music21.analysis import search
