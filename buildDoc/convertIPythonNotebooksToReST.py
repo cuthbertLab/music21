d17a822eaf07b9433f7b40ac2703f64d5f575059
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-06-14 16:08:02 +0000

    Added copyright notice to buildDoc/convertIPythonNotebooksToReST.py.

--- buildDoc/convertIPythonNotebooksToReST.py
+++ buildDoc/convertIPythonNotebooksToReST.py
@@ -1,4 +1,14 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:         convertIPythonNotebooksToReST.py
+# Purpose:      music21 documentation IPython notebook to ReST converter
+#
+# Authors:      Josiah Wolf Oberholtzer
+#
+# Copyright:    Copyright © 2009-2012 Michael Scott Cuthbert and the music21 Project
+# License:      LGPL, see license.txt
+#-------------------------------------------------------------------------------
 
 import os
 import re
