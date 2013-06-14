84b96b687d824fe43bf7656287d921631de792e2
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-06-14 03:03:26 +0000

    Added explanatory comment to _version.py.

--- music21/_version.py
+++ music21/_version.py
@@ -1,2 +1,21 @@
+'''
+This file contains Music21's version number information.
+
+Why here and not in music21/__init__.py?
+
+Keeping the information here makes it available to Music21's setup.py file
+by simply reading this file in and evaluating its contents.
+
+Importantly, that means that Music21's setup.py *doesn't* need to import any
+part of Music21's code during the installation process.  Not importing any part
+of Music21 makes installation cleaner, and also helps other software - like
+package managers in various Linux distributions - work with Music21 without
+complaint.
+
+When it's time to update Music21's version, just change the numbers in the
+tuple assigned to __version_info__, and the __version__ string will be
+updated along with it.
+'''
+
 __version_info__ = (1, 5, 0)
 __version__ = '.'.join(str(x) for x in __version_info__)
