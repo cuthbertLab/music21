45c0e9ff2bb72ac3978c4a7ee3ec88640fa0b7a5
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-06-17 21:08:21 +0000

    Added stub modules to documentation/:
        - iterators.py, housing tools for finding various types of objects in
          music21's packagesystem
        - extensions.py, housing Sphinx extensions for manipulating docstrings
          according to music21's coding standards
        - writers.py, housing tools for writing ReST documentation to disk

--- music21/documentation/__init__.py
+++ music21/documentation/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import documenters
+from documenters import *
+from iterators import *
+from writers import *
 
---
+++ music21/documentation/iterators.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:         convertIPythonNotebooksToReST.py
+# Purpose:      music21 documentation IPython notebook to ReST converter
+#
+# Authors:      Josiah Wolf Oberholtzer
+#
+# Copyright:    Copyright © 2009-2012 Michael Scott Cuthbert and the music21 Project
+# License:      LGPL, see license.txt
+#-------------------------------------------------------------------------------
+
+
+
