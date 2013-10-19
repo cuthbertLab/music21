09ad4bb2811e93a7fc084fbba43404b018c7d305
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-10-19 01:48:29 +0000

    Removed __slots__ from editorial classes temporarily.

--- music21/editorial.py
+++ music21/editorial.py
@@ -1,22 +1,30 @@
 # -*- coding: utf-8 -*-
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Name:         editorial.py
 # Purpose:      music21 classes for representing notes
 #
 # Authors:      Michael Scott Cuthbert
 #               Christopher Ariza
 #
-# Copyright:    Copyright © 2008-2012 Michael Scott Cuthbert and the music21 Project
+# Copyright:    Copyright © 2008-2012 Michael Scott Cuthbert and the music21
+#               Project
 # License:      LGPL
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
-'''Editorial objects store comments and other meta-data associated with specific :class:`~music21.note.Note` objects or other music21 objects. 
 '''
+Editorial objects store comments and other meta-data associated with specific
+:class:`~music21.note.Note` objects or other music21 objects.
+'''
+
 
 from __future__ import unicode_literals
 
 import unittest
 from music21 import exceptions21
+
+
+#------------------------------------------------------------------------------
+
 
 class EditorialException(exceptions21.Music21Exception):
     pass
@@ -24,45 +32,62 @@ class EditorialException(exceptions21.Music21Exception):
 class CommentException(exceptions21.Music21Exception):
     pass
 
-def getObjectsWithEditorial(listToSearch, editorialStringToFind, 
-    listOfValues=None):
+
+#------------------------------------------------------------------------------
+
+
+def getObjectsWithEditorial(
+    listToSearch, 
+    editorialStringToFind,
+    listOfValues=None,
+    ):
     '''
-    provided a list of objects (typically note objects) to search through, this method returns
-    only those objects that have the editorial attribute defined by the editorialStringToFind.
-    An optional parameter, listOfValues, is a list of all the possible values the given
-    object's editorialString can have.
-    
-    the editorialStringToFind can be any of the pre-defined editorial attributes (such as "ficta" or "harmonicIntervals")
-    but it may also be the dictionary key of editorial notes stored in the miscellaneous (misc) dictionary.
-    For example, "isPassingTone" or "isNeighborTone"
-    
-    
-    >>> n1 = note.Note()
-    >>> n1.editorial.misc['isPassingTone'] = True
-    >>> n2 = note.Note()
-    >>> n2.editorial.comment = 'consider revising'
-    >>> s = stream.Stream()
-    >>> s.repeatAppend(n1, 5)
-    >>> s.repeatAppend(note.Note(), 2)
-    >>> s.repeatAppend(n2, 3)
-    >>> listofNotes = s.getElementsByClass(note.Note)
-    >>> listOfValues = ['consider revising', 'remove']
-    >>> listofNotesWithEditorialisPassingTone = editorial.getObjectsWithEditorial(listofNotes, "isPassingTone")
-    >>> listofNotesWithEditorialComment = editorial.getObjectsWithEditorial(listofNotes, "comment", listOfValues)
-    >>> print len(listofNotesWithEditorialisPassingTone)
-    5
-    >>> print len(listofNotesWithEditorialComment)
-    3
+    Provided a list of objects (typically note objects) to search through, this
+    method returns only those objects that have the editorial attribute defined
+    by the editorialStringToFind.  An optional parameter, listOfValues, is a
+    list of all the possible values the given object's editorialString can
+    have.
+
+    The editorialStringToFind can be any of the pre-defined editorial
+    attributes (such as "ficta" or "harmonicIntervals") but it may also be the
+    dictionary key of editorial notes stored in the miscellaneous (misc)
+    dictionary.  For example, "isPassingTone" or "isNeighborTone"
+
+    ::
+
+        >>> n1 = note.Note()
+        >>> n1.editorial.misc['isPassingTone'] = True
+        >>> n2 = note.Note()
+        >>> n2.editorial.comment = 'consider revising'
+        >>> s = stream.Stream()
+        >>> s.repeatAppend(n1, 5)
+        >>> s.repeatAppend(note.Note(), 2)
+        >>> s.repeatAppend(n2, 3)
+        >>> listofNotes = s.getElementsByClass(note.Note)
+        >>> listOfValues = ['consider revising', 'remove']
+        >>> listofNotesWithEditorialisPassingTone = editorial.getObjectsWithEditorial(
+        ...     listofNotes, "isPassingTone")
+        >>> listofNotesWithEditorialComment = editorial.getObjectsWithEditorial(
+        ...     listofNotes, "comment", listOfValues)
+        >>> print len(listofNotesWithEditorialisPassingTone)
+        5
+
+    ::
+
+        >>> print len(listofNotesWithEditorialComment)
+        3
+
     '''
     listofOBJToReturn = []
     for obj in listToSearch:
         try:
             try:
-                editorialContents = getattr(obj.editorial, editorialStringToFind)
+                editorialContents = getattr(
+                    obj.editorial, editorialStringToFind)
             except:
                 editorialContents = obj.editorial.misc[editorialStringToFind]
-            
-            if listOfValues != None:
+
+            if listOfValues is not None:
                 if editorialContents in listOfValues:
                     listofOBJToReturn.append(obj)
             else:
@@ -70,38 +95,39 @@ def getObjectsWithEditorial(listToSearch, editorialStringToFind,
         except:
             pass
     return listofOBJToReturn
-    
+
+
 class NoteEditorial(object):
-    '''Editorial comments and special effects that can be applied to notes
+    '''
+    Editorial comments and special effects that can be applied to notes
     Standard ones are stored as attributes.  Non-standard/one-off effects are
     stored in the dict called "misc":
-    
-    
-    >>> a = editorial.NoteEditorial()
-    >>> a.color = "blue"  # a standard editorial effect
-    >>> a.misc['backgroundHighlight'] = 'yellow'  # non-standard.
 
-    
-    Every GeneralNote object already has a NoteEditorial object
-    attached to it at object.editorial.  Normally you will just change that
-    object instead.  
-    
-    For instance, take the case where a scribe 
-    wrote F in the score, knowing that a good singer would automatically
-    sing F-sharp instead.  We can store the editorial
-    suggestion to sing F-sharp as a "musica ficta" accidental object:
-    
-    
-    >>> fictaSharp = pitch.Accidental("Sharp")
-    >>> n = note.Note("F")
-    >>> n.editorial.ficta = fictaSharp
-    >>> assert(n.editorial.ficta.alter == 1.0) #_DOCS_HIDE
-    >>> #_DOCS_SHOW n.show('lily.png')  # only Lilypond currently supports musica ficta
-    
-    
+    ::
+
+        >>> a = editorial.NoteEditorial()
+        >>> a.color = "blue"  # a standard editorial effect
+        >>> a.misc['backgroundHighlight'] = 'yellow'  # non-standard.
+
+    Every GeneralNote object already has a NoteEditorial object attached to it
+    at object.editorial.  Normally you will just change that object instead.
+
+    For instance, take the case where a scribe wrote F in the score, knowing
+    that a good singer would automatically sing F-sharp instead.  We can store
+    the editorial suggestion to sing F-sharp as a "musica ficta" accidental
+    object:
+
+    ::
+
+        >>> fictaSharp = pitch.Accidental("Sharp")
+        >>> n = note.Note("F")
+        >>> n.editorial.ficta = fictaSharp
+        >>> assert(n.editorial.ficta.alter == 1.0) #_DOCS_HIDE
+        >>> #_DOCS_SHOW n.show('lily.png')  # only Lilypond currently supports musica ficta
+
     .. image:: images/noteEditorialFictaSharp.*
         :width: 103
-    
+
     '''
 
     _DOC_ATTR = {
@@ -117,21 +143,26 @@ class NoteEditorial(object):
     'melodicIntervalsOverRests': 'same thing but a list',
     'misc': 'A dict to hold anything you might like to store.',
     }
-    __slots__ = ('ficta',
-                 'color',
-                 'misc',
-                 'harmonicInterval',
-                 'harmonicIntervals',
-                 'hidden',
-                 'melodicInterval',
-                 'melodicIntervals',
-                 'melodicIntervalOverRests',
-                 'melodicIntervalsOverRests',
-                 'comment',
-                 )
-    
+
+#    __slots__ = (
+#        'ficta',
+#        'color',
+#        'misc',
+#        'harmonicInterval',
+#        'harmonicIntervals',
+#        'hidden',
+#        'melodicInterval',
+#        'melodicIntervals',
+#        'melodicIntervalOverRests',
+#        'melodicIntervalsOverRests',
+#        'comment',
+#        )
+
+    ### INITIALIZER ###
+
     def __init__(self):
-        self.ficta = None  # Accidental object -- N.B. for PRINTING only not for determining intervals
+        # Accidental object -- N.B. for PRINTING only not for determining intervals
+        self.ficta = None
         self.color = None
         self.misc  = {}
         self.harmonicInterval = None
@@ -143,87 +174,102 @@ class NoteEditorial(object):
         self.melodicIntervalsOverRests = []
         self.comment = Comment()
 
+    ### PUBLIC METHODS ###
+
     def lilyStart(self):
         r'''
-        A method that returns a string containing the 
-        lilypond output that comes before the note.
-        
-        
-        >>> n = note.Note()
-        >>> n.editorial.lilyStart()   
-        u''
-        >>> n.editorial.ficta = pitch.Accidental("Sharp")
-        >>> n.editorial.color = "blue"
-        >>> n.editorial.hidden = True
-        >>> print n.editorial.lilyStart()
-        \ficta \color "blue" \hideNotes
-        
+        A method that returns a string containing the lilypond output that
+        comes before the note.
+
+        ::
+
+            >>> n = note.Note()
+            >>> n.editorial.lilyStart()
+            u''
+
+        ::
+
+            >>> n.editorial.ficta = pitch.Accidental("Sharp")
+            >>> n.editorial.color = "blue"
+            >>> n.editorial.hidden = True
+            >>> print n.editorial.lilyStart()
+            \ficta \color "blue" \hideNotes
+
         '''
-        baseRet = ""
+        result = ""
         if self.ficta is not None:
-            baseRet += self.fictaLilyStart()
+            result += self.fictaLilyStart()
         if self.color:
-            baseRet += self.colorLilyStart()
+            result += self.colorLilyStart()
         if self.hidden is True:
-            baseRet += r"\hideNotes "
-            
-        return baseRet
+            result += r"\hideNotes "
+        return result
 
     def fictaLilyStart(self):
-        r''' 
-        returns \ficta -- called out so it is more easily subclassed
+        r'''
+        Returns \ficta -- called out so it is more easily subclassed
         '''
         return r"\ficta "
 
     def colorLilyStart(self):
         r'''
-        returns \color "theColorName" -- called out so it is more easily subclassed
+        Returns \color "theColorName" -- called out so it is more easily
+        subclassed.
         '''
         return r'\color "' + self.color + '" '
 
     def lilyAttached(self):
-        r'''returns any information that should be attached under the note,
-        currently just returns self.comment.lily or "" '''
-        
+        r'''
+        Returns any information that should be attached under the note,
+        currently just returns self.comment.lily or "".
+        '''
         if self.comment and self.comment.text:
             return unicode(self.comment.lily)
         else:
             return ''
-    
+
     def lilyEnd(self):
         r'''
-        returns a string of editorial lily
-        instructions to come after the note.  Currently it is
-        just info to turn off hidding of notes.
+        Returns a string of editorial lily instructions to come after the note.
+        Currently it is just info to turn off hidding of notes.
         '''
-        
-        baseRet = u""
-        
+        result = u""
         if self.hidden is True:
-            baseRet += u"\\unHideNotes "
-        
-        return baseRet
+            result += u"\\unHideNotes "
+        return result
 
-        
+
 class Comment(object):
     '''
-    an object that adds text above or below a note:
-    
-    
-    >>> n = note.Note()
-    >>> n.editorial.comment.text = "hello"
-    >>> n.editorial.comment.position = "above"
-    >>> n.editorial.comment.lily
-    u'^"hello"'
-    
+    An object that adds text above or below a note:
+
+    ::
+
+        >>> n = note.Note()
+        >>> n.editorial.comment.text = "hello"
+        >>> n.editorial.comment.position = "above"
+        >>> n.editorial.comment.lily
+        u'^"hello"'
+
     '''
-    __slots__ = ('position', 'text')
-    
+
+    ### CLASS VARIABLES ###
+
+#    __slots__ = (
+#        'position',
+#        'text',
+#        )
+
+    ### INITIALIZER ###
+
     def __init__(self):
         self.position = "below"
         self.text = None
-    
-    def _getLily(self):
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def lily(self):
         if self.text is None:
             return ""
         if self.position == 'below':
@@ -231,12 +277,13 @@ class Comment(object):
         elif self.position == 'above':
             return '^"' + self.text + '"'
         else:
-            raise CommentException("Cannot deal with position: " + self.position)
-        
-    lily = property(_getLily)
+            raise CommentException(
+                'Cannot deal with position: ' + self.position)
 
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+
 class Test(unittest.TestCase):
 
     def runTest(self):
@@ -245,7 +292,9 @@ class Test(unittest.TestCase):
     def testCopyAndDeepcopy(self):
         '''Test copying all objects defined in this module
         '''
-        import sys, types, copy
+        import copy
+        import sys
+        import types
         for part in sys.modules[self.__module__].__dict__:
             match = False
             for skip in ['_', '__', 'Test', 'Exception']:
@@ -255,36 +304,27 @@ class Test(unittest.TestCase):
                 continue
             name = getattr(sys.modules[self.__module__], part)
             if callable(name) and not isinstance(name, types.FunctionType):
-                try: # see if obj can be made w/ args
+                try:  # see if obj can be made w/ args
                     obj = name()
                 except TypeError:
                     continue
                 unused_a = copy.copy(obj)
                 unused_b = copy.deepcopy(obj)
 
-
-
     def testBasic(self):
         a = Comment()
         self.assertEqual(a.position, 'below')
 
 
+#------------------------------------------------------------------------------
 
 
-
-#-------------------------------------------------------------------------------
-# define presented order in documentation
-_DOC_ORDER = [NoteEditorial]
-
+_DOC_ORDER = (
+    NoteEditorial,
+    )
 
 if __name__ == "__main__":
     #import doctest
     #doctest.testmod()
     import music21
     music21.mainTest(Test)
-
-
-
-#------------------------------------------------------------------------------
-# eof
-
