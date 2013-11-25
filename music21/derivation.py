51cbcbed4d5f4e54ce0d77ff8d9cffb187eb2e6b
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2013-11-24 20:11:33 -0500

    Derivation is now slotted.

--- music21/derivation.py
+++ music21/derivation.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Name:         derivation.py
 # Purpose:      Class for storing and managing Stream-based derivations
 #
 # Authors:      Christopher Ariza
 #
-# Copyright:    Copyright © 2011-2012 Michael Scott Cuthbert and the music21 Project
+# Copyright:    Copyright © 2011-2012 Michael Scott Cuthbert and the music21
+#               Project
 # License:      LGPL
-#-------------------------------------------------------------------------------
-'''This module defines objects for tracking the derivation of one :class:`~music21.stream.Stream` from another.
+#------------------------------------------------------------------------------
+
+'''
+This module defines objects for tracking the derivation of one
+:class:`~music21.stream.Stream` from another.
 '''
 
 import unittest
 
 from music21 import common
+from music21.base import SlottedObject
 # imported by stream
 
 from music21 import environment
@@ -21,37 +26,95 @@ _MOD = "derivation.py"
 environLocal = environment.Environment(_MOD)
 
 
+class Derivation(SlottedObject):
+    '''
+    A derivation.
 
-class Derivation(object):
+    ::
+
+        >>> import copy
+        >>> s1 = stream.Stream()
+        >>> s2 = stream.Stream()
+        >>> d1 = derivation.Derivation(s1)
+        >>> d1.setAncestor(s2)
+        >>> d1.getAncestor() is s2
+        True
+
+    ::
+
+        >>> d1.getContainer() is s1
+        True
+
+    ::
+
+        >>> d2 = copy.deepcopy(d1)
+        >>> d2.getAncestor() is s2
+        True
+
+    ::
+
+        >>> d1.setMethod('measure')
+        >>> d1.getMethod()
+        'measure'
+
     '''
-    >>> import copy
-      
-    >>> s1 = stream.Stream()
-    >>> s2 = stream.Stream()
-    >>> d1 = derivation.Derivation(s1)
-    >>> d1.setAncestor(s2)
-    >>> d1.getAncestor() is s2
-    True    
-    >>> d1.getContainer() is s1
-    True    
-    >>> d2 = copy.deepcopy(d1)
-    >>> d2.getAncestor() is s2
-    True
-    >>> d1.setMethod('measure')
-    >>> d1.getMethod()
-    'measure'
-    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_ancestor',
+        '_ancestorId',
+        '_container',
+        '_containerId',
+        '_method',
+        )
+
+    ### INITIALIZER ###
+
     def __init__(self, container=None):
         # store a reference to the Stream that contains this derivation
         self._container = None
-        self._containerId = None # store id to optimize w/o unwrapping
+        self._containerId = None  # store id to optimize w/o unwrapping
         self._method = None
         # ancestor should be stored as a weak ref
         self._ancestor = None
-        self._ancestorId = None # store id to optimize w/o unwrapping
-
+        self._ancestorId = None  # store id to optimize w/o unwrapping
         # set container; can handle None
         self.setContainer(container)
+
+    ### SPECIAL METHODS ###
+
+    def __deepcopy__(self, memo=None):
+        '''
+        Manage deepcopying by creating a new reference to the same object. If
+        the ancestor no longer exists, than ancestor is set to None
+        '''
+        new = self.__class__()
+        new.setContainer(self.getContainer())
+        new.setAncestor(self.getAncestor())
+        return new
+
+    ### PUBLIC METHODS ###
+
+    def getAncestor(self):
+        return self._ancestor
+        #return common.unwrapWeakref(self._ancestor)
+
+    def getContainer(self):
+        return common.unwrapWeakref(self._container)
+
+    def getMethod(self):
+        return self._method
+
+    def setAncestor(self, ancestor):
+        # for now, ancestor is not a weak ref
+        if ancestor is None:
+            self._ancestorId = None
+            self._ancestor = None
+        else:
+            self._ancestorId = id(ancestor)
+            self._ancestor = ancestor
+            #self._ancestor = common.wrapWeakref(ancestor)
 
     def setContainer(self, container):
         # container is the Stream that contains
@@ -62,55 +125,31 @@ class Derivation(object):
             self._containerId = id(container)
             self._container = common.wrapWeakref(container)
 
-    def getContainer(self):
-        return common.unwrapWeakref(self._container)
-
-    def setAncestor(self, ancestor):
-        # for now, ancestor is not a weak ref        
-        if ancestor is None:
-            self._ancestorId = None
-            self._ancestor = None
-        else:
-            self._ancestorId = id(ancestor)
-            self._ancestor = ancestor
-            #self._ancestor = common.wrapWeakref(ancestor)
-
-    def getAncestor(self):
-        return self._ancestor
-        #return common.unwrapWeakref(self._ancestor)
-
     def setMethod(self, method):
         '''
-        sets a string identifying how the object was derived
+        Sets a string identifying how the object was derived.
         '''
         self._method = method
 
-    def getMethod(self):
-        return self._method
-
-
-    def __deepcopy__(self, memo=None):
-        '''Manage deepcopying by creating a new reference to the same object. If the ancestor no longer exists, than ancestor is set to None
-        '''
-        new = self.__class__()
-        new.setContainer(self.getContainer())
-        new.setAncestor(self.getAncestor())
-        return new
-
-
     def unwrapWeakref(self):
-        '''Unwrap any and all weakrefs stored.
+        '''
+        Unwrap any and all weakrefs stored.
 
-          
-        >>> s1 = stream.Stream()
-        >>> s2 = stream.Stream()
-        >>> d1 = derivation.Derivation(s1) # sets container
-        >>> d1.setAncestor(s2)
-        >>> common.isWeakref(d1._container)
-        True
-        >>> d1.unwrapWeakref()
-        >>> common.isWeakref(d1._container)
-        False
+        ::
+
+            >>> s1 = stream.Stream()
+            >>> s2 = stream.Stream()
+            >>> d1 = derivation.Derivation(s1) # sets container
+            >>> d1.setAncestor(s2)
+            >>> common.isWeakref(d1._container)
+            True
+
+        ::
+
+            >>> d1.unwrapWeakref()
+            >>> common.isWeakref(d1._container)
+            False
+
         '''
         #environLocal.printDebug(['derivation pre unwrap: self._container', self._container])
         post = common.unwrapWeakref(self._container)
@@ -122,37 +161,26 @@ class Derivation(object):
         '''
         if not common.isWeakref(self._container):
             # update id in case it has changed
-            self._containerId = id(self._container) 
+            self._containerId = id(self._container)
             post = common.wrapWeakref(self._container)
             self._container = post
 
 
+#------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------
+
 class Test(unittest.TestCase):
 
     def runTest(self):
         pass
 
-#-------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
 # define presented order in documentation
+
 _DOC_ORDER = []
-
-
 
 if __name__ == "__main__":
     # sys.arg test options will be used in mainTest()
     import music21
     music21.mainTest(Test)
-
-
-#------------------------------------------------------------------------------
-# eof
-
-
-
-
-
-
-
-
