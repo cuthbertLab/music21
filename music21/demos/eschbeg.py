6ae063fcea5d0694b97e7544c855f8aefe800fc0
Michael Scott Cuthbert <cuthbert@mit.edu>
2014-02-23 21:19:48 -0500

    updated AEsCHBEG demo

--- music21/demos/eschbeg.py
+++ music21/demos/eschbeg.py
@@ -51,19 +51,25 @@ def setupTranspositions():
 
 eschbegTransposed = setupTranspositions()
 
-def generateToneRows(numberToGenerate = 1000):
+def generateToneRows(numberToGenerate = 1000, cardinality=12):
     '''
     generates a list of random 12-tone rows.
 
     >>> from music21.demos import eschbeg
     >>> #_DOCS_SHOW eschbeg.generateToneRows(4)
     ['T310762985E4', '9E036T472158', '5879E3T12064', '417T26E95038']
+
+    generate random 3-note sets:
+
+    >>> #_DOCS_SHOW eschbeg.generateToneRows(4, 3)
+    ['840', 'T61', 'T10', '173']
     '''
-    firstRow = list("0123456789TE")
+    allNotes = "0123456789TE"
+    firstRow = list(allNotes)
     returnRows = []
     for i in range(numberToGenerate):
         random.shuffle(firstRow)
-        returnRows.append(''.join(firstRow))
+        returnRows.append(''.join(firstRow[0:cardinality]))
     return returnRows
 
 def generateRandomRows(numberToGenerate = 1000):
@@ -94,9 +100,9 @@ def priorProbability(rowsToTest = 1000000, enforce12Tone = True):
     '''
     Returns the number of randomly generated
     tone rows (or random 12 pitch collections)
-    that contain the Eschbeg set,
+    that contain the AEsCHBEG set,
     followed by the number that contain a
-    transposition of the Eschbeg set.
+    transposition of the AEsCHBEG set.
     
     Works out to about 12 per million for the former
     and 130 per million for the latter.
@@ -125,7 +131,9 @@ def priorProbability(rowsToTest = 1000000, enforce12Tone = True):
 def findEmbeddedChords(testSet = "0234589", cardinality = 3, skipInverse = False):
     '''
     finds the trichords (or chords of other cardinalities) in the Aschbeg set or 
-    any other set:
+    any other set.  This example shows that every possible trichord appears at least once
+    in the ESCHBEG set and two other trichords (037 = major/minor triad; 048 = augmented triad)
+    appear just as often.   
     
     >>> from music21.demos import eschbeg
     >>> print eschbeg.findEmbeddedChords("0234589")
@@ -148,7 +156,50 @@ def findEmbeddedChords(testSet = "0234589", cardinality = 3, skipInverse = False
     [036]: (258) (903) 
     [037]: (259) (580) (904) 
     [047]: (590) (803) 
+    [048]: (048) (480) (804)
+    
+    
+    This is the all trichord hexachord:
+
+    >>> print eschbeg.findEmbeddedChords("012478", skipInverse=True)
+    [012]: (012) 
+    [013]: (124) 
+    [014]: (014) 
+    [015]: (780) 
+    [016]: (127) (781) 
+    [024]: (024) 
+    [025]: (247) 
+    [026]: (248) 
+    [027]: (027) 
+    [036]: (147) 
+    [037]: (148) 
     [048]: (048) (480) (804) 
+
+    But it does not contain every inversion:
+    
+    >>> print eschbeg.findEmbeddedChords("012478")
+    [012]: (012) 
+    [013]: (124) 
+    [023]: 
+    [014]: (014) 
+    [034]: (478) 
+    [015]: (780) 
+    [045]: (801) 
+    [016]: (127) (781) 
+    [056]: (278) (701) (812) 
+    [024]: (024) 
+    [025]: (247) 
+    [035]: 
+    [026]: (248) 
+    [046]: (802) 
+    [027]: (027) 
+    [057]: (702) 
+    [036]: (147) 
+    [037]: (148) 
+    [047]: (047) 
+    [048]: (048) (480) (804) 
+    
+    
     '''
     eschbegSplit12 = [letterToNumber(x) for x in testSet]
     ret = "" 
@@ -189,15 +240,98 @@ def findEmbeddedChords(testSet = "0234589", cardinality = 3, skipInverse = False
                     if notFound is False:
                         transInverseString = ''.join([str(p) for p in transInverse])
                         ret += "(" + transInverseString + ") "
-    return ret
+    return ret.lstrip()
+
+def uniquenessOfEschbeg(cardinality = 7, searchCardinality = 3, skipInverse = False, showMatching=True):
+    '''
+    the Eschbeg heptachord contains all trichords and their inversions.  How many heptachords do
+    that?
+    
+    Returns a list of heptachords that have all of them.
+    
+    First a baseline:
+    
+    >>> from music21 import *
+    >>> len(chordTables.FORTE[7])
+    39
+    
+    There are 39 chords in that list, but #0 is blank, so that you can reference them by
+    conventional Forte numbers.  Thus there are 38 in actuality.
+    
+    >>> allTrichordAndInversionHeptachords = demos.eschbeg.uniquenessOfEschbeg()
+    >>> len(allTrichordAndInversionHeptachords)
+    16
+
+    So almost half of all heptachords have a complete set of trichords and inversions. How many
+    have all trichords without inversion?
+    
+    >>> len(demos.eschbeg.uniquenessOfEschbeg(skipInverse = True))
+    18
+    
+    So, not too many more.
+    
+    For fun, other questions:
+    
+    Is there an all-tetrachord heptachord?
+    
+    >>> len(demos.eschbeg.uniquenessOfEschbeg(searchCardinality = 4, skipInverse = True))
+    0
+    
+    Nope.  What about octachord?
+    
+    >>> len(demos.eschbeg.uniquenessOfEschbeg(cardinality = 8, searchCardinality = 4, skipInverse = True))
+    2
+    
+    Yep! what are they?
+    
+    >>> demos.eschbeg.uniquenessOfEschbeg(cardinality = 8, searchCardinality = 4, skipInverse = True)
+    ['01234689', '01235679']
+
+    Notice that they're the complement sets of the all-interval tetrachords 0146 and 0137!
+    
+    Is every octachord all-trichord?
+    
+    >>> numOctochords = len(chordTables.FORTE[8]) - 1
+    >>> numOctochords
+    29
+    >>> len(demos.eschbeg.uniquenessOfEschbeg(cardinality = 8, searchCardinality = 3, skipInverse = True))
+    22
+    
+    Nope.  Seven are not.  We can see them by reversing showMatching
+    
+    >>> demos.eschbeg.uniquenessOfEschbeg(cardinality = 8, searchCardinality = 3, skipInverse = True, showMatching = False)
+    ['01234567', '01235678', '01236789', '02345679', '01234679', '0123578T', '0134679T']
+
+    These are the complement sets of 0123 0128 0167 0235 0136 0258 0369
+    
+    '''
+    allHeptachords = music21.chordTables.FORTE[cardinality]
+    allHeptachordList = []
+    for i in range(1, len(allHeptachords)):
+        thisHeptachord = allHeptachords[i][0]
+        thisHeptachordString = ''.join([numberToLetter(p) for p in thisHeptachord])
+        #print thisHeptachordString
+        foundTrichords = findEmbeddedChords(thisHeptachordString, 
+                                            cardinality = searchCardinality, 
+                                            skipInverse = skipInverse)
+        foundTrichordList = foundTrichords.split('\n')
+        noneMissing = True
+        for trichordInfo in foundTrichordList:
+            if '(' not in trichordInfo:
+                noneMissing = False
+                break
+        if noneMissing == showMatching: # default True
+            allHeptachordList.append(thisHeptachordString)
+        
+    return allHeptachordList
 
 if __name__ == "__main__":
     music21.mainTest()
 
-#print findEmbeddedChords(cardinality = 5)
-#p, t = priorProbability(1000000, enforce12Tone = False)
-#print p
-#print t
+    #print findEmbeddedChords(cardinality = 5)
+    #p, t = priorProbability(100000, enforce12Tone = False)
+    #print p
+    #print t
 
 #------------------------------
 #eof
