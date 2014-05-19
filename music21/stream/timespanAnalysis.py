a798e6b82ca22e9f418ef222810add35b9d2bbbb
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-05-19 16:16:23 -0400

    Added new stream/timespanAnalysis.py module.
    The module will house logic related to performing voice-leading analysis with
    timespans.
    
    The module name and location are provisional.

---
+++ music21/stream/timespanAnalysis.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# Name:         timespanAnalysis.py
+# Purpose:      Tools for grouping notes and chords into a searchable tree
+#               organized by start and stop offsets
+#
+# Authors:      Josiah Wolf Oberholtzer
+#
+# Copyright:    Copyright © 2013-14 Michael Scott Cuthbert and the music21
+#               Project
+# License:      LGPL, see license.txt
+#------------------------------------------------------------------------------
+'''
+Tools for performing voice-leading analysis with timespans.
+'''
+
+import unittest
+from music21 import environment
+environLocal = environment.Environment("stream.timespanAnalysis")
+
+
+#------------------------------------------------------------------------------
+
+
+class Test(unittest.TestCase):
+
+    def runTest(self):
+        pass
+
+
+#------------------------------------------------------------------------------
+
+
+_DOC_ORDER = (
+    )
+
+
+#------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    import music21
+    music21.mainTest(Test)
