88542ff362ec4ccdc0f9279ce2e5fbb10fcaad4b
Michael Scott Cuthbert <cuthbert@mit.edu>
2014-05-25 13:16:38 -0400

    Vexflow in IPython Notebook. almost

--- music21/vexflow/toMusic21j.py
+++ music21/vexflow/toMusic21j.py
@@ -14,12 +14,14 @@ Convert a music21 object into JSON and send it to the browser for music21j to us
 '''
 import unittest
 
+from music21.exceptions21 import Music21Exception
 from music21 import freezeThaw
 from music21 import stream
 
 supportedDisplayModes = [
     'html',
     'jsbody',
+    'jsbodyScript',
     'json'
 ]
 
@@ -65,28 +67,29 @@ def fromObject(thisObject, mode='html'):
     return conv.fromObject(thisObject)
 
 class VexflowPickler(object):
-    template = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    templateHtml = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <!-- for MSIE 10 on Windows 8 -->
         <meta http-equiv="X-UA-Compatible" content="requiresActiveX=true"/>
         <title>{title}</title>
-        <script data-main='{m21URI}' src='{requireURI}'></script>
-        <script>
-            require(['music21'], function() {{
-                var pickleIn = {pickleOutput};
-                var jpc = new music21.jsonPickle.Converter();
-                streamObj = jpc.run(pickleIn);
-                {callback}
-            }});
-        </script>
-    
+        {loadM21Template}
+        {jsBodyScript}
     </head>
     <body>
     </body>
     </html>
     '''
+    jsBodyScript = '''<script>\n{jsBody}\n</script>'''
+    jsBody = '''require(['music21'], function() {{
+                var pickleIn = {pickleOutput};
+                var jpc = new music21.jsonPickle.Converter();
+                streamObj = jpc.run(pickleIn);
+                {callback}
+            }});'''
+    loadM21Template = '''<script data-main='{m21URI}' src='{requireURI}'></script>'''
+
     def __init__(self):
         self.defaults = {
             'pickleOutput' : '{"py/object": "hello"}',
@@ -94,10 +97,12 @@ class VexflowPickler(object):
             'requireURI' :'http://web.mit.edu/music21/music21j/ext/require/require.js',
             'callback' :'streamObj.renderOptions.events.resize = "reflow";\n\t\t\tstreamObj.appendNewCanvas();'
         }
-
+        self.mode = 'html'
     
         
-    def fromObject(self, thisObject, mode='html'):
+    def fromObject(self, thisObject, mode=None):
+        if mode is None:
+            mode = self.mode
         if (thisObject.isStream is False):
             retStream = stream.Stream()
             retStream.append(thisObject)
@@ -112,7 +117,112 @@ class VexflowPickler(object):
             allJSONList.append('\'' + jsonString[i:i+chunkSize] + '\'')
         return ' + \n    '.join(allJSONList)
     
-    def fromStream(self,thisStream, mode='html'):
+    def getLoadTemplate(self, urls=None):
+        '''
+        Gets the <script> tag for loading music21 from require.js
+        
+        >>> vfp = vexflow.toMusic21j.VexflowPickler()
+        >>> vfp.getLoadTemplate()
+        "<script data-main='http://web.mit.edu/music21/music21j/src/music21' src='http://web.mit.edu/music21/music21j/ext/require/require.js'></script>"
+
+        >>> d = {'m21URI': 'file:///tmp/music21', 'requireURI': 'http://requirejs.com/require.js'}
+        >>> vfp.getLoadTemplate(d)
+        "<script data-main='file:///tmp/music21' src='http://requirejs.com/require.js'></script>"
+        '''
+        
+        if urls is None:
+            urls = self.defaults
+        loadM21formatted = self.loadM21Template.format(m21URI = urls['m21URI'], 
+                                                       requireURI = urls['requireURI'],)
+        return loadM21formatted
+    
+    def getJSBodyScript(self, dataSplit, defaults = None):
+        '''
+        Get the <script>...</script> tag to render the JSON
+        
+        >>> vfp = vexflow.toMusic21j.VexflowPickler()
+        >>> print(vfp.getJSBodyScript('{"hi": "hello"}'))
+           <script>
+                require(['music21'], function() {
+                    var pickleIn = {"hi": "hello"};
+                    var jpc = new music21.jsonPickle.Converter();
+                    streamObj = jpc.run(pickleIn);
+                    streamObj.renderOptions.events.resize = "reflow";
+                streamObj.appendNewCanvas();
+                });
+            </script>        
+        '''
+        if defaults is None:
+            defaults = self.defaults
+        jsBody = self.getJSBody(dataSplit, defaults)
+        jsBodyScript = self.jsBodyScript.format(jsBody = jsBody)
+        return jsBodyScript;
+
+    def getJSBody(self, dataSplit, defaults = None):
+        '''
+        Get the javascript code without the <script> tags to render the JSON
+        
+        >>> vfp = vexflow.toMusic21j.VexflowPickler()
+        >>> print(vfp.getJSBody('{"hi": "hello"}'))
+                require(['music21'], function() {
+                    var pickleIn = {"hi": "hello"};
+                    var jpc = new music21.jsonPickle.Converter();
+                    streamObj = jpc.run(pickleIn);
+                    streamObj.renderOptions.events.resize = "reflow";
+                streamObj.appendNewCanvas();
+                });
+        '''
+        if defaults is None:
+            d = self.defaults
+        else:
+            d = defaults
+        jsBody = self.jsBody.format(pickleOutput = dataSplit,
+                                    callback = d['callback'])
+        return jsBody;
+    
+    def getHTML(self, dataSplit, title=None, defaults=None):
+        '''
+        Get the complete HTML page to pass to the browser:
+        
+        >>> vfp = vexflow.toMusic21j.VexflowPickler()
+        >>> print(vfp.getHTML('{"hi": "hello"}', 'myPiece'))
+           <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+                <html xmlns="http://www.w3.org/1999/xhtml">
+                <head>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+            <!-- for MSIE 10 on Windows 8 -->
+            <meta http-equiv="X-UA-Compatible" content="requiresActiveX=true"/>
+            <title>myPiece</title>
+            <script data-main='http://web.mit.edu/music21/music21j/src/music21' src='http://web.mit.edu/music21/music21j/ext/require/require.js'></script>
+            <script>
+            require(['music21'], function() {
+                            var pickleIn = {"hi": "hello"};
+                            var jpc = new music21.jsonPickle.Converter();
+                            streamObj = jpc.run(pickleIn);
+                            streamObj.renderOptions.events.resize = "reflow";
+                        streamObj.appendNewCanvas();
+                        });
+            </script>
+            </head>
+            <body>
+            </body>
+            </html>
+        '''
+        if defaults is None:
+            d = self.defaults
+        else:
+            d = defaults
+        loadM21Formatted = self.getLoadTemplate(d)
+        jsBodyScript = self.getJSBodyScript(dataSplit, d)
+        formatted = self.templateHtml.format(title = title, 
+                                                 loadM21Template=loadM21Formatted,
+                                                 jsBodyScript=jsBodyScript)
+        return formatted
+    
+    def fromStream(self, thisStream, mode=None):
+        if mode is None:
+            mode = self.mode
+
         if (thisStream.metadata is not None and thisStream.metadata.title != ""):
             title = thisStream.metadata.title
         else:
@@ -120,10 +230,19 @@ class VexflowPickler(object):
         sf = freezeThaw.StreamFreezer(thisStream)
         data = sf.writeStr(fmt='jsonpickle')
         dataSplit = self.splitLongJSON(data)
-        d = self.defaults
-        formatted = self.template.format(title = title, pickleOutput = dataSplit, 
-                                    m21URI = d['m21URI'], requireURI = d['requireURI'], callback = d['callback'])
-        return formatted
+        if mode == 'json':
+            return dataSplit
+        elif mode == 'jsbody':
+            return self.getJSBody(dataSplit)
+        elif mode == 'jsbodyScript':
+            return self.getJSBodyScript(dataSplit)
+        elif mode == 'html':
+            return self.getHTML(dataSplit, title)
+        else:
+            raise VexflowToM21JException("Cannot deal with mode: %r" % mode)
+
+class VexflowToM21JException(Music21Exception):
+    pass
 
 class Test(unittest.TestCase):
     
