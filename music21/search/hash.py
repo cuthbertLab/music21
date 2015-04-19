c0d6fa4b006966a9e9a43dd2e5cb72ca78f0d17e
Emily <emzhang@mit.edu>
2015-04-19 13:00:37 -0400

    change note name, basic test setup

--- music21/search/hash.py
+++ music21/search/hash.py
@@ -28,7 +28,7 @@ class Hasher(object):
 	def __init__(self, stream):
 		self.stream = stream
 
-	def basicHash(self, stream):
+	def basicHash(self):
 
 		"""
 		proof of concept, likely incomplete feature
@@ -40,10 +40,43 @@ class Hasher(object):
 
 		leaves out rests for now
 		"""
-		note_hash = collections.namedtuple('NoteHash', ['pitch', 'duration', 'beat'])
+		NoteHash = collections.namedtuple('NoteHash', ['pitch', 'duration', 'beat'])
 		note_container = []
 		# TODO: perhaps change container to be a deque? ordered dict?
-		stream_notes = s.getElementsByClass(note.Note)
-		for note in stream_notes:
-			note_container.append(NoteHash(note.pitch.midi, note.duration.quarterLength, note.offset))
+		stream_notes = self.stream.getElementsByClass(note.Note)
+		for n in stream_notes:
+			note_container.append(NoteHash(n.pitch.midi, n.duration.quarterLength, n.offset))
 		return note_container
+
+class Test(unittest.TestCase):
+	def runTest(self):
+		pass
+	def testBasicHash(self):
+		from music21 import *
+		s1 = stream.Stream()
+		note1 = note.Note("C4")
+		note1.duration.type = 'half'
+		note2 = note.Note("F#4")
+		note3 = note.Note("B-2")
+		cMinor = chord.Chord(["C4","G4","E-5"])
+		cMinor.duration.type = 'half'
+		s1.append(note1)
+		s1.append(note2)
+		s1.append(note3)
+		s1.append(cMinor)
+		h = Hasher(s1)
+		print h.basicHash()
+
+
+class TestExternal(unittest.TestCase):
+	def runTest(self):
+		pass
+
+	def testBasicHash(self):
+		from music21 import corpus
+		s1 = corpus.parse('xmlscores/Bologna_596_Nulla_Pitie')
+		print basicHash(s1)
+
+if __name__ == "__main__":
+    import music21
+    music21.mainTest(Test) 
