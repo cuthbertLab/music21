2662c0dcf0dcdfcd59fa85e1583bc2f30f3c5c8d
Emily <emzhang@mit.edu>
2015-04-22 12:37:39 -0400

    started writing generalized form of hash where you can indicate whether certain things ought to be hashed or not

--- music21/search/hash.py
+++ music21/search/hash.py
@@ -28,9 +28,83 @@ NoteHash = collections.namedtuple('NoteHash', ['pitch', 'duration', 'beat'])
 class Hasher(object):
 	def __init__(self, stream):
 		self.stream = stream
+		
+
+		# --- begin general types of things to hash ---
+		self.hashNotes = True # is this even necessary?
+		self.hashRests = True
+		self.hashChords = True
+		# --- end general types of things to hash ---
+
+		# --- begin note properties to hash ---
+		self.hashPitch = True
+		self.hashDuration = True
+		self.roundDuration = True
+		self.hashOffset = True
+		self.roundOffset = True
+		self.outOfKey = False
+		self.isAccidental = False
+		self.isTied = False
+		# --- end note properties to hash ---
+
+	def setSmartDefaults(self):
+		"""
+		maybe multiple of these kind of functions to set defaults that aid certain types of hashes
+		"""
+		pass
+
+	# --- Begin Rounding Helper Functions ---
+	def _getApproxDurOrOffset(self, durOrOffset, granularity=32):
+		return round(durOrOffset*rounding)/rounding
+	
+
+	def _approximatelyEqual(self, a, b, sig_fig = 2):
+		"""
+		use to look at whether beat lengths are close, within a certain range
+		probably can use for other things that are approx. equal
+		"""
+		return (a==b or int(a*10**sig_fig) == int(b*10**sig_fig))
+
+	# --- End Rounding Helper Functions ---
+
+	# --- Begin Hashing Functions --- 
+
+	def generalHash(self, granularity=32):
+		"""
+		hasher with ability to turn certain parts off/on
+		"""
+		note_container = []
+		stream_notes = self.stream.getElementsByClass(note.GeneralNote)
+		for n in stream_notes:
+			if n.isNote and self.hashNotes:
+				if self.hashDuration:
+					if self.roundDuration:
+						note_dur = _getApproxDurOrOffset(n.duration.quarterLength, granularity)
+					else: 
+						note_dur = n.duration.quarterLength
+				else: note_dur = None
+
+				if self.hashOffset:
+					if self.roundOffset:
+						note_offset = _getApproxDurOrOffset(n.offset, granularity)
+					else:
+						note_offset = n.offset
+				else:
+					note_offset = None
+
+				note_container.append(NoteHash(n.pitch.midi, note_dur, note_offset))
+			elif n.isChord and self.hashChords:
+				for p in n.pitches:
+					note_container.append(NoteHash(p.midi, note_dur, note_offset))
+			elif n.isRest and self.hashRests: # n is Rest
+				note_container.append(NoteHash(None, note_dur, note_offset))
+
+			else:
+				raise Exception("Not a note, rest, or chord; shouldn't get here!")
+		return note_container
+			 
 
 	def basicHash(self):
-
 		"""
 		proof of concept, likely incomplete feature
 
@@ -38,8 +112,6 @@ class Hasher(object):
 		pitch: absolute in this sense; maps to midi value between 21 and 108
 		duration: length of note 
 		beat: offset of each note, relative to the beginning of the stream
-
-		leaves out rests for now
 		"""	
 		note_container = []
 		# TODO: perhaps change container to be a deque? ordered dict?
@@ -53,6 +125,7 @@ class Hasher(object):
 			else: # n is Rest
 				note_container.append(NoteHash(None, n.duration.quarterLength, n.offset))
 		return note_container
+	# --- End Hashing Functions ---
 
 class Test(unittest.TestCase):
 	def runTest(self):
