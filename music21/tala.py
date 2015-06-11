6d3751687358b0b8b56d1f29c52249f1f99666b0
swapnilgt <swapnilgupta.iiith@gmail.com>
2015-06-11 16:55:15 +0200

    1) Removing the setters/getters 2) Removing redundant class variables for keeping count. Instead, added attributes for the same.

--- music21/tala.py
+++ music21/tala.py
@@ -6,7 +6,7 @@ from __builtin__ import int
 
 class Matra(base.Music21Object):
     ''' 
-    This class represents the matra in a tala cycle 
+    This class represents the matra in a tala cycle
     '''
     
     khali = 0
@@ -19,21 +19,15 @@ class Matra(base.Music21Object):
     
     def __init__(self, taliKhali = None, bols = None):
         
+        if taliKhali is not None and not isinstance(taliKhali, int):
+            raise TypeError('argument taliKhali should be of type int')
+        elif taliKhali is not None and taliKhali != 1 and taliKhali != 0:
+            raise ValueError('argument taliKhali takes a binary value: 0 or 1')
         # Set the bol when creating a composition.
         self.bols = bols
         
         # This variable takes either of the one the class variables 'tali' or 'khali'
         self.taliKhali = taliKhali
-        
-    def setBol(self, bols):
-        self.bols = bols
-        
-    def setTali(self):
-        self.taliKhali = Matra.tali
-    
-    def setKhali(self):
-        self.taliKhali = Matra.khali
-    
     
 
 class Vibhaga(base.Music21Object):
@@ -41,59 +35,21 @@ class Vibhaga(base.Music21Object):
     This class represents a vibhaga in a tala cycle and other information related to it.
     '''
     
-    def __init__(self, numMatras, startMatraPos, matras = None):
+    def __init__(self, matras = None):
         '''
         This class is used to define a vibhaga in a tala cycle
         '''
-        
-        # Handling exceptions for the numMatras
-        if not isinstance(numMatras, int):
-            raise TypeError('first argument numMatras should be of type int')
-        elif numMatras < 1:
-            raise ValueError('first argument numMatras should be a positive integer')
-        
-        # Handling exceptions for the startMatraPos
-        if not isinstance(startMatraPos, int):
-            raise TypeError('second argument startMatraPos should be of type int')
-        elif startMatraPos < 1:
-            raise ValueError('second argument startMatraPos should be a positive integer')
 
         # Handling exceptions for matra
         if matras is not None and (not isinstance(matras, list) or not all(isinstance(x, Matra)for x in matras)):
-            raise TypeError('argument matra should either be None or a list of Matra')
-        elif matras is not None and len(matras) is not numMatras:
-            raise ValueError('number of elements in list matras should be equal to numMatras')    
+            raise TypeError('argument matra should either be None or a list of Matra')  
         
-        
-        self.numMatras = numMatras
         self.matras = matras
-        self.startMatraPos = startMatraPos
         
         
-    def setMatras(self, matras):
-        if not isinstance(matras, Matra) and not all(isinstance(x, Matra)for x in matras):
-            raise TypeError('argument matra should either be None or a list of Matra')
-        elif len(matras) != self.numMatras:
-            raise ValueError('number of elements in list matras should be equal to numMatras')
-        self.matras = matras
-            
-        
-    def setNumMatras(self, numMatras):
-        # Handling exceptions for the numMatras
-        if not isinstance(numMatras, int):
-            raise TypeError('first argument numMatras should be of type int')
-        elif numMatras < 1:
-            raise ValueError('first argument numMatras should be a positive integer')
-        self.numMatras = numMatras
-        
-        
-    def setStartMatraPos(self, startMatraPos):
-        # Handling exceptions for the startMatraPos
-        if not isinstance(startMatraPos, int):
-            raise TypeError('second argument startMatraPos should be of type int')
-        elif startMatraPos < 0:
-            raise ValueError('second argument startMatraPos should be a positive integer')
-        self.startMatraPos = startMatraPos
+    @property
+    def numMatras(self):
+        return len(self.matras)
         
         
 class Avartan(base.Music21Object):
@@ -105,19 +61,12 @@ class Avartan(base.Music21Object):
         if not isinstance(vibhagas, list) or not all(isinstance(x, Vibhaga)for x in vibhagas):
             raise TypeError('second argument vibhagas should be a list of Vibhaga')
         
-        self.numVibhagas = len(vibhagas)
         self.vibhagas = vibhagas
         
-    def setVibhagas(self, vibhagas):
+    @property
+    def numVibhagas(self):
+        return len(self.vibhagas)
         
-        if not isinstance(vibhagas, list) or not all(isinstance(x, Vibhaga)for x in vibhagas):
-            raise TypeError('second argument vibhagas should be a list of Vibhaga')
-        
-        self.vibhagas = vibhagas
-        self.updateVals()
-        
-    def updateVals(self):
-        self.numVibhagas = len(self.vibhagas)
         
 
 class TeenTalAvartan(Avartan):
@@ -129,24 +78,24 @@ class TeenTalAvartan(Avartan):
         matra2 = Matra()
         matra3 = Matra()
         matra4 = Matra()
-        vibhaga1 = Vibhaga(4, 1, [matra1, matra2, matra3, matra4])
+        vibhaga1 = Vibhaga([matra1, matra2, matra3, matra4])
     
         matra5 = Matra(1)
         matra6 = Matra()
         matra7 = Matra()
         matra8 = Matra()
-        vibhaga2 = Vibhaga(4, 5, [matra5, matra6, matra7, matra8])
+        vibhaga2 = Vibhaga([matra5, matra6, matra7, matra8])
         
         matra9 = Matra(0)
         matra10 = Matra()
         matra11 = Matra()
         matra12 = Matra()
-        vibhaga3 = Vibhaga(4, 9, [matra9, matra10, matra11, matra12])
+        vibhaga3 = Vibhaga([matra9, matra10, matra11, matra12])
         
         matra13 = Matra(1)
         matra14 = Matra()
         matra15 = Matra()
         matra16 = Matra()
-        vibhaga4 = Vibhaga(4, 13,[matra13, matra14, matra15, matra16])
+        vibhaga4 = Vibhaga([matra13, matra14, matra15, matra16])
         
         super(TeenTalAvartan, self).__init__([vibhaga1, vibhaga2, vibhaga3, vibhaga4])
