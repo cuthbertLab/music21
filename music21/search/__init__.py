c1dd0802ff28ebd5eafa4a3e57974000c4356cb1
Michael Scott Cuthbert <cuthbert@mit.edu>
2015-08-12 18:13:57 -0400

    init add...

---
+++ music21/search/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:         search/__init__.py
+# Purpose:      music21 classes for searching within files
+#
+# Authors:      Michael Scott Cuthbert
+#
+# Copyright:    Copyright © 2011-2013 Michael Scott Cuthbert and the music21 Project
+# License:      LGPL or BSD, see license.txt
+#-------------------------------------------------------------------------------
+'''
+Methods and Classes useful in searching within scores.
+
+For searching a group of scores see the search functions within
+:ref:`moduleCorpus` .
+
+'''
+
+
+### __init__.py ######
+__all__ = ['base', 'lyrics', 'segment']
+
+from music21.search import base
+from music21.search import lyrics
+from music21.search import segment
+
+from music21.search.base import *
+
