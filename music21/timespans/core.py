15919691813c7c72e6ee0a547ccf6f0bf1634912
Michael Scott Cuthbert <cuthbert@mit.edu>
2015-10-09 16:44:35 -0400

    moving more things to node...

--- music21/timespans/core.py
+++ music21/timespans/core.py
@@ -11,15 +11,314 @@
 # License:      LGPL or BSD, see license.txt
 #------------------------------------------------------------------------------
 '''
-Tools for grouping notes and chords into a searchable tree
-organized by start and stop offsets.
+These are the lowest level tools for working with self-balancing AVL trees.
 
-This is a lower-level tool that for now at least normal music21
-users won't need to worry about.
+There's an overhead to creating an AVL tree, but for a large score it is
+absolutely balanced by having O(log n) search times.
 '''
-
-from music21.timespans import node as nodeModule
 from music21.exceptions21 import TimespanException
+
+from music21 import common
+
+#------------------------------------------------------------------------------
+class AVLNode(common.SlottedObject):
+    r'''
+    An AVL Tree Node, not specialized in any way, just contains offsets.
+
+    >>> offset = 1.0
+    >>> node = timespans.core.AVLNode(offset)
+    >>> node
+    <Node: Start:1.0 Height:0 L:None R:None>
+    >>> n2 = timespans.core.AVLNode(2.0)
+    >>> node.rightChild = n2
+    >>> node.update()
+    >>> node
+    <Node: Start:1.0 Height:1 L:None R:0>
+    
+    Note that nodes cannot rebalance themselves, that's what a Tree is for.
+    
+
+    Please consult the Wikipedia entry on AVL trees
+    (https://en.wikipedia.org/wiki/AVL_tree) for a very detailed
+    description of how this datastructure works.    
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '__weakref__',
+        'balance',
+        'height',
+        'offset',
+        'payload',
+
+        'leftChild',
+        'rightChild',
+        )
+
+    _DOC_ATTR = {
+    'balance': '''
+        Returns the current state of the difference in heights of the 
+        two subtrees rooted on this node.
+
+        This attribute is used to help balance the AVL tree.
+
+        >>> score = timespans.makeExampleScore()
+        >>> tree = timespans.streamToTimespanTree(score, flatten=True, 
+        ...                    classList=(note.Note, chord.Chord))
+        >>> print(tree.debug())
+        <Node: Start:3.0 Indices:(0:5:6:12) Length:{1}>
+            L: <Node: Start:1.0 Indices:(0:2:3:5) Length:{1}>
+                L: <Node: Start:0.0 Indices:(0:0:2:2) Length:{2}>
+                R: <Node: Start:2.0 Indices:(3:3:5:5) Length:{2}>
+            R: <Node: Start:5.0 Indices:(6:8:9:12) Length:{1}>
+                L: <Node: Start:4.0 Indices:(6:6:8:8) Length:{2}>
+                R: <Node: Start:6.0 Indices:(9:9:11:12) Length:{2}>
+                    R: <Node: Start:7.0 Indices:(11:11:12:12) Length:{1}>
+
+
+        This tree has one more depth on the right than on the left
+
+        >>> tree.rootNode.balance
+        1
+
+
+        The leftChild of the rootNote is perfectly balanced, while the rightChild is off by
+        one (acceptable).
+
+        >>> tree.rootNode.leftChild.balance
+        0
+        >>> tree.rootNode.rightChild.balance
+        1
+
+
+        The rightChild's children are also (acceptably) unbalanced:
+
+        >>> tree.rootNode.rightChild.leftChild.balance
+        0
+        >>> tree.rootNode.rightChild.rightChild.balance
+        1
+        
+        You should never see a balance other than 1, -1, or 0.  If you do then
+        something has gone wrong.
+        ''',
+        
+        
+    'height': r'''
+        The height of the subtree rooted on this node.
+
+        This property is used to help balance the AVL tree.
+
+        >>> score = timespans.makeExampleScore()
+        >>> tree = timespans.streamToTimespanTree(score, flatten=True, 
+        ...              classList=(note.Note, chord.Chord))
+        >>> print(tree.debug())
+        <Node: Start:3.0 Indices:(0:5:6:12) Length:{1}>
+            L: <Node: Start:1.0 Indices:(0:2:3:5) Length:{1}>
+                L: <Node: Start:0.0 Indices:(0:0:2:2) Length:{2}>
+                R: <Node: Start:2.0 Indices:(3:3:5:5) Length:{2}>
+            R: <Node: Start:5.0 Indices:(6:8:9:12) Length:{1}>
+                L: <Node: Start:4.0 Indices:(6:6:8:8) Length:{2}>
+                R: <Node: Start:6.0 Indices:(9:9:11:12) Length:{2}>
+                    R: <Node: Start:7.0 Indices:(11:11:12:12) Length:{1}>
+
+        >>> tree.rootNode.height
+        3
+
+        >>> tree.rootNode.rightChild.height
+        2
+
+        >>> tree.rootNode.rightChild.rightChild.height
+        1
+
+        >>> tree.rootNode.rightChild.rightChild.rightChild.height
+        0
+        
+        Once you hit a height of zero, then the next child on either size should be None
+        
+        >>> print(tree.rootNode.rightChild.rightChild.rightChild.rightChild)
+        None
+        ''',
+        
+    'offset': r'''
+        The offset of this node.
+
+        >>> score = timespans.makeExampleScore()
+        >>> tree = timespans.streamToTimespanTree(score, flatten=True, classList=(note.Note, chord.Chord))
+        >>> print(tree.rootNode.debug())
+        <Node: Start:3.0 Indices:(0:5:6:12) Length:{1}>
+            L: <Node: Start:1.0 Indices:(0:2:3:5) Length:{1}>
+                L: <Node: Start:0.0 Indices:(0:0:2:2) Length:{2}>
+                R: <Node: Start:2.0 Indices:(3:3:5:5) Length:{2}>
+            R: <Node: Start:5.0 Indices:(6:8:9:12) Length:{1}>
+                L: <Node: Start:4.0 Indices:(6:6:8:8) Length:{2}>
+                R: <Node: Start:6.0 Indices:(9:9:11:12) Length:{2}>
+                    R: <Node: Start:7.0 Indices:(11:11:12:12) Length:{1}>
+
+        >>> tree.rootNode.offset
+        3.0
+
+        >>> tree.rootNode.leftChild.offset
+        1.0
+
+        >>> tree.rootNode.rightChild.offset
+        5.0
+        ''',
+    'leftChild': r'''
+        The left child of this node.
+
+        After setting the left child you need to do a node update. with node.update()
+
+        >>> score = timespans.makeExampleScore()
+        >>> tree = timespans.streamToTimespanTree(score, flatten=True, classList=(note.Note, chord.Chord))
+        >>> print(tree.rootNode.debug())
+        <Node: Start:3.0 Indices:(0:5:6:12) Length:{1}>
+            L: <Node: Start:1.0 Indices:(0:2:3:5) Length:{1}>
+                L: <Node: Start:0.0 Indices:(0:0:2:2) Length:{2}>
+                R: <Node: Start:2.0 Indices:(3:3:5:5) Length:{2}>
+            R: <Node: Start:5.0 Indices:(6:8:9:12) Length:{1}>
+                L: <Node: Start:4.0 Indices:(6:6:8:8) Length:{2}>
+                R: <Node: Start:6.0 Indices:(9:9:11:12) Length:{2}>
+                    R: <Node: Start:7.0 Indices:(11:11:12:12) Length:{1}>
+
+        >>> print(tree.rootNode.leftChild.debug())
+        <Node: Start:1.0 Indices:(0:2:3:5) Length:{1}>
+            L: <Node: Start:0.0 Indices:(0:0:2:2) Length:{2}>
+            R: <Node: Start:2.0 Indices:(3:3:5:5) Length:{2}>
+        ''',
+    'rightChild':   r'''
+        The right child of this node.
+
+        After setting the right child you need to do a node update. with node.update()
+
+        >>> score = timespans.makeExampleScore()
+        >>> tree = timespans.streamToTimespanTree(score, flatten=True, classList=(note.Note, chord.Chord))
+        >>> print(tree.rootNode.debug())
+        <Node: Start:3.0 Indices:(0:5:6:12) Length:{1}>
+            L: <Node: Start:1.0 Indices:(0:2:3:5) Length:{1}>
+                L: <Node: Start:0.0 Indices:(0:0:2:2) Length:{2}>
+                R: <Node: Start:2.0 Indices:(3:3:5:5) Length:{2}>
+            R: <Node: Start:5.0 Indices:(6:8:9:12) Length:{1}>
+                L: <Node: Start:4.0 Indices:(6:6:8:8) Length:{2}>
+                R: <Node: Start:6.0 Indices:(9:9:11:12) Length:{2}>
+                    R: <Node: Start:7.0 Indices:(11:11:12:12) Length:{1}>
+
+        >>> print(tree.rootNode.rightChild.debug())
+        <Node: Start:5.0 Indices:(6:8:9:12) Length:{1}>
+            L: <Node: Start:4.0 Indices:(6:6:8:8) Length:{2}>
+            R: <Node: Start:6.0 Indices:(9:9:11:12) Length:{2}>
+                R: <Node: Start:7.0 Indices:(11:11:12:12) Length:{1}>
+
+        >>> print(tree.rootNode.rightChild.rightChild.debug())
+        <Node: Start:6.0 Indices:(9:9:11:12) Length:{2}>
+            R: <Node: Start:7.0 Indices:(11:11:12:12) Length:{1}>
+
+        >>> print(tree.rootNode.rightChild.rightChild.rightChild.debug())
+        <Node: Start:7.0 Indices:(11:11:12:12) Length:{1}>
+        '''
+
+    }
+    
+    ### INITIALIZER ###
+
+    def __init__(self, offset, payload=None):
+        self.balance = 0
+        self.height = 0        
+        self.offset = offset
+        self.payload = payload
+
+        self.leftChild = None
+        self.rightChild = None
+
+
+    ### SPECIAL METHODS ###
+
+    def __repr__(self):
+        lcHeight = None
+        if self.leftChild:
+            lcHeight = self.leftChild.height
+        rcHeight = None
+        if self.rightChild:
+            rcHeight = self.rightChild.height
+            
+        return '<Node: Start:{} Height:{} L:{} R:{}>'.format(
+            self.offset,
+            self.height,
+            lcHeight,
+            rcHeight
+            )
+
+    ### PRIVATE METHODS ###
+
+    def debug(self):
+        '''
+        Get a debug of the Node:
+        
+        >>> score = timespans.makeExampleScore()
+        >>> tree = timespans.streamToTimespanTree(score, flatten=True, classList=(note.Note, chord.Chord))
+        >>> rn = tree.rootNode        
+        >>> print(rn.debug())
+        <Node: Start:3.0 Indices:(0:5:6:12) Length:{1}>
+            L: <Node: Start:1.0 Indices:(0:2:3:5) Length:{1}>
+                L: <Node: Start:0.0 Indices:(0:0:2:2) Length:{2}>
+                R: <Node: Start:2.0 Indices:(3:3:5:5) Length:{2}>
+            R: <Node: Start:5.0 Indices:(6:8:9:12) Length:{1}>
+                L: <Node: Start:4.0 Indices:(6:6:8:8) Length:{2}>
+                R: <Node: Start:6.0 Indices:(9:9:11:12) Length:{2}>
+                    R: <Node: Start:7.0 Indices:(11:11:12:12) Length:{1}>
+        '''
+        return '\n'.join(self._getDebugPieces())
+
+    def _getDebugPieces(self):
+        r'''
+        Return a list of the debugging information of the tree (used for debug):
+        
+        >>> score = timespans.makeExampleScore()
+        >>> tree = timespans.streamToTimespanTree(score, flatten=True, classList=(note.Note, chord.Chord))
+        >>> rn = tree.rootNode
+        >>> rn._getDebugPieces()
+        ['<Node: Start:3.0 Indices:(0:5:6:12) Length:{1}>', 
+        '\tL: <Node: Start:1.0 Indices:(0:2:3:5) Length:{1}>',
+        '\t\tL: <Node: Start:0.0 Indices:(0:0:2:2) Length:{2}>', 
+        '\t\tR: <Node: Start:2.0 Indices:(3:3:5:5) Length:{2}>', 
+        '\tR: <Node: Start:5.0 Indices:(6:8:9:12) Length:{1}>', 
+        '\t\tL: <Node: Start:4.0 Indices:(6:6:8:8) Length:{2}>', 
+        '\t\tR: <Node: Start:6.0 Indices:(9:9:11:12) Length:{2}>', 
+        '\t\t\tR: <Node: Start:7.0 Indices:(11:11:12:12) Length:{1}>']        
+        '''        
+        result = []
+        result.append(repr(self))
+        if self.leftChild:
+            subresult = self.leftChild._getDebugPieces()
+            result.append('\tL: {}'.format(subresult[0]))
+            result.extend('\t' + x for x in subresult[1:])
+        if self.rightChild:
+            subresult = self.rightChild._getDebugPieces()
+            result.append('\tR: {}'.format(subresult[0]))
+            result.extend('\t' + x for x in subresult[1:])
+        return result
+
+    def update(self):
+        '''
+        Updates the height and balance attributes of the nodes.
+        
+        Must be called whenever .leftChild or .rightChild are changed.
+        
+        Does not balance itself! just used for the next balancing operation.
+        
+        Returns None
+        '''        
+        leftHeight = -1
+        rightHeight = -1
+        if self.leftChild is not None:
+            leftHeight = self.leftChild.height
+        if self.rightChild is not None:
+            rightHeight = self.rightChild.height
+        self.height = max(leftHeight, rightHeight) + 1
+        self.balance = rightHeight - leftHeight
+
+
+#----------------------------------------------------------------------------
 
 class AVLTree(object):
     r'''
@@ -31,14 +330,14 @@ class AVLTree(object):
         '__weakref__',
         'rootNode',
         )
-    nodeClass = nodeModule.AVLNode
+    nodeClass = AVLNode
     
     def __init__(self):
         self.rootNode = None
 
     def __iter__(self):
         r'''
-        Iterates through all the nodes in the offset tree.
+        Iterates through all the nodes in the offset tree in left to right order.
 
         >>> tsList = [(0,2), (0,9), (1,1), (2,3), (3,4), (4,9), (5,6), (5,8), (6,8), (7,7)]
         >>> tss = [timespans.Timespan(x, y) for x, y in tsList]
@@ -72,31 +371,25 @@ class AVLTree(object):
         return recurse(self.rootNode)
 
 
-    def _insertNode(self, node, offset):
-        r'''
-        Inserts a node at `offset` in the subtree rooted on `node`.
-
-        Used internally by TimespanTree.
-
-        Returns a node which should be set to the new rootNode
-        
-        Note that if a node already exists at that place, nothing happens.
-        
+    def insertAtOffset(self, offset):
+        '''
+        creates a new node at offset and sets the rootNode
+        appropriately
         
         >>> avl = timespans.core.AVLTree()
-        >>> avl.rootNode = avl._insertNode(avl.rootNode, 20)
+        >>> avl.insertAtOffset(20)
         >>> avl.rootNode
         <Node: Start:20 Height:0 L:None R:None>        
         
-        >>> avl.rootNode = avl._insertNode(avl.rootNode, 10)
+        >>> avl.insertAtOffset(10)
         >>> avl.rootNode
         <Node: Start:20 Height:1 L:0 R:None>
 
-        >>> avl.rootNode = avl._insertNode(avl.rootNode, 5)
+        >>> avl.insertAtOffset(5)
         >>> avl.rootNode
         <Node: Start:10 Height:1 L:0 R:0>
         
-        >>> avl.rootNode = avl._insertNode(avl.rootNode, 30)
+        >>> avl.insertAtOffset(30)
         >>> avl.rootNode
         <Node: Start:10 Height:2 L:0 R:1>
         >>> avl.rootNode.leftChild
@@ -106,20 +399,28 @@ class AVLTree(object):
         
         >>> avl.rootNode.rightChild.rightChild
         <Node: Start:30 Height:0 L:None R:None>
-
         '''
-        if node is None:
-            # if we get to the point where a node does not have a
-            # left or right child, make a new node at this offset...
-            return self.nodeClass(offset)
+        def recurse(node, offset):
+            '''
+            this recursively finds the right place for the new node
+            and either creates a new node (if it is in the right place)
+            or rebalances the nodes above it and tells those nodes how
+            to set their new roots.
+            '''
+            if node is None:
+                # if we get to the point where a node does not have a
+                # left or right child, make a new node at this offset...
+                return self.nodeClass(offset)
+            
+            if offset < node.offset:
+                node.leftChild = recurse(node.leftChild, offset)
+                node.update()
+            elif node.offset < offset:
+                node.rightChild = recurse(node.rightChild, offset)
+                node.update()
+            return self._rebalance(node)            
         
-        if offset < node.offset:
-            node.leftChild = self._insertNode(node.leftChild, offset)
-            node.update()
-        elif node.offset < offset:
-            node.rightChild = self._insertNode(node.rightChild, offset)
-            node.update()
-        return self._rebalance(node)
+        self.rootNode = recurse(self.rootNode, offset)
 
 
     def debug(self):
@@ -225,7 +526,7 @@ class AVLTree(object):
         nextNode.update()
         return nextNode
 
-    def getNodeByOffset(self, offset, node=None):
+    def getNodeByOffset(self, offset):
         r'''
         Searches for a node whose offset is `offset` in the subtree
         rooted on `node`.
@@ -234,14 +535,18 @@ class AVLTree(object):
 
         Returns a Node object or None
         '''
-        if node is not None:
-            if node.offset == offset:
-                return node
-            elif node.leftChild and offset < node.offset:
-                return self.getNodeByOffset(offset, node.leftChild)
-            elif node.rightChild and node.offset < offset:
-                return self.getNodeByOffset(offset, node.rightChild)
-        return None
+        def recurse(offset, node):
+            if node is not None:
+                if node.offset == offset:
+                    return node
+                elif node.leftChild and offset < node.offset:
+                    return recurse(offset, node.leftChild)
+                elif node.rightChild and node.offset < offset:
+                    return recurse(offset, node.rightChild)
+            return None
+    
+        return recurse(offset, self.rootNode)
+
 
     def _getNodeAfter(self, offset):
         r'''
