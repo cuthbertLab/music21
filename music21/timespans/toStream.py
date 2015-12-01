b30ca88a1e0b105dd24dd05081e1783206a10adb
Michael Scott Cuthbert <cuthbert@mit.edu>
2015-12-01 11:47:24 -0500

    fix naming bug

--- music21/timespans/toStream.py
+++ music21/timespans/toStream.py
@@ -105,7 +105,7 @@ def chordified(timespans, templateStream=None):
         return outputStream
 
 
-def toPartwise(timespans, templateStream=None):
+def partwise(timespans, templateStream=None):
     '''
     todo docs
     '''
