1a82f70718f787f05e7c39d6ac7e2bba07e5c12b
Michael Scott Cuthbert <cuthbert@mit.edu>
2016-05-23 17:39:10 -0400

    unused musicxml SoundParser

---
+++ music21/musicxml/xmlSoundParser.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:         musicxml/xmlSoundParser.py
+# Purpose:      Translate the <sound> tag to music21
+#
+# Authors:      Michael Scott Cuthbert
+#
+# Copyright:    Copyright © 2016 Michael Scott Cuthbert and the music21 Project
+# License:      LGPL or BSD, see license.txt
+#-------------------------------------------------------------------------------
+'''
+Methods for converting <sound> tag to the many different music21
+objects that this tag might represent.
+
+Pulled out because xmlToM21 is getting way too big.
+'''
+class SoundTagMixin(object):
+    pass
+
+    def soundTagParser(self, mxSound):
+        '''
+        Returns a list of objects that represent the sound tag.
+        '''
+        soundObjs = []
+        tempoNum = mxSound.get('tempo')
+        dynamicsNum = mxSound.get('dynamics')
+        
+        return soundObjs
