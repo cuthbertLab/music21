b57c6536f6c6262db90a8d382676129bf4381b41
Michael Scott Cuthbert <cuthbert@mit.edu>
2016-08-09 11:18:59 -0400

    Convert all CRLF endings to LF (Thanks Bo-Cheng!)

--- music21/lily/translate.py
+++ music21/lily/translate.py
@@ -5,7 +5,7 @@
 #
 # Authors:      Michael Scott Cuthbert
 #
-# Copyright:    Copyright ?? 2007-2012 Michael Scott Cuthbert and the music21 Project
+# Copyright:    Copyright © 2007-2012 Michael Scott Cuthbert and the music21 Project
 # License:      LGPL or BSD, see license.txt
 #-------------------------------------------------------------------------------
 '''
