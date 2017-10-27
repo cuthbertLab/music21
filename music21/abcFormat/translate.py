62b301ee0d17a99d3fe23c2b4985e7713f1dc509
Alexandre Papadopoulos <32906275+a-papadopoulos@users.noreply.github.com>
2017-10-27 16:36:40 +0200

    Fix bug with b in chord names
    Chord names with a b were not always correctly parsed, so change them to - first.

--- music21/abcFormat/translate.py
+++ music21/abcFormat/translate.py
@@ -285,7 +285,10 @@ def parseTokens(mh, dst, p, useMeasures):
                 cs_name = re.sub('["]','', cs_name).lstrip().rstrip()
                 cs_name = re.sub('[()]', '', cs_name)
                 cs_name = re.sub('[ /].*', '', cs_name)
-                if re.match(r'[A-Ga-g][#-]*', cs_name):
+                cs_name = re.sub('([A-Ga-g])b', r'\1-', cs_name) # Replace b
+                #  with - in malformed chord names
+                if re.match(r'[A-Ga-g][#-]*', cs_name):# Exclude malformed
+                    # chord names
                     cs = harmony.ChordSymbol(cs_name)
                     dst.coreAppend(cs, setActiveSite=False)
                     dst.coreElementsChanged()
