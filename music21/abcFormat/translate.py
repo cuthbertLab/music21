2f774afe9fc426e1ae1c9fd29b0f4f81d7fb2078
Alexandre Papadopoulos <32906275+a-papadopoulos@users.noreply.github.com>
2019-04-08 12:20:48 +0200

    Remove test which was wrong

--- music21/abcFormat/translate.py
+++ music21/abcFormat/translate.py
@@ -1031,20 +1031,6 @@ class Test(unittest.TestCase):
         self.assertEqual(cs.bass(), pitch.Pitch('B-2'))
         self.assertEqual(cs.pitches[0], pitch.Pitch('B-2'))
 
-        # common.cleanedFlatNotation() shouldn't be called by
-        # the following calls, which what is being tested here:
-
-        cs = harmony.ChordSymbol('b-3')
-        self.assertEqual(cs.root(), pitch.Pitch('b-3'))
-        self.assertEqual(cs.pitches[0], pitch.Pitch('B-3'))
-        self.assertEqual(cs.pitches[1], pitch.Pitch('D4'))
-
-        cs = harmony.ChordSymbol('bb3')
-        self.assertEqual(cs.root(), pitch.Pitch('b3'))
-        self.assertEqual(cs.pitches[0], pitch.Pitch('B3'))
-        self.assertEqual(cs.pitches[1], pitch.Pitch('D#4'))
-
-
     def xtestTranslateB(self):
         '''
         Dylan -- this could be too slow to make it a test!
