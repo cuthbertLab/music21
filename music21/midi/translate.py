cuthbertLab/music21

commit bd2299ba4f10010adcc533c4c5350f43b2671710

Enforce nonzero duration on non-grace notes when quantized (#641)
* Enforce nonzero duration on non-grace notes when quantized

* Import zero duration MIDI notes as grace notes

* Removing forceSource=True

If you need to forceSource to make tests pass, before committing final version, remove it and feel free to update the minor version of music21 in __version__.py and single test in base.py to make older sources recompute

Not-really-Co-authored-by: Michael Scott Cuthbert <cuthbert@mit.edu>  JacobTylerWells gets the credit.

music21/midi/translate.py
@@ -352,8 +352,8 @@ def midiEventsToNote(eventList, ticksPerQuarter=None, inputM21=None):
         ticksToDuration(tOff - tOn, ticksPerQuarter, n.duration)
     else:
         # environLocal.printDebug(['cannot translate found midi event with zero duration:', eOn, n])
-        # for now, substitute 0
-        n.quarterLength = 0
+        # for now, substitute grace note
+        n.getGrace(inPlace=True)
 
     return n
 
@@ -521,7 +521,7 @@ def midiEventsToChord(eventList, ticksPerQuarter=None, inputM21=None):
             v = volume.Volume(velocity=eOn.velocity)
             v.velocityIsRelative = False  # velocity is absolute coming from
             volumes.append(v)
-    # assume it is  a flat list
+    # assume it is a flat list
     else:
         onEvents = eventList[:(len(eventList) // 2)]
         offEvents = eventList[(len(eventList) // 2):]
@@ -545,8 +545,8 @@ def midiEventsToChord(eventList, ticksPerQuarter=None, inputM21=None):
     else:
         # environLocal.printDebug(['cannot translate found midi event with zero duration:',
         #                         eventList, c])
-        # for now, substitute 0
-        c.quarterLength = 0
+        # for now, get grace
+        c.getGrace(inPlace=True)
     return c
 
 
@@ -1669,7 +1669,7 @@ def midiTrackToStream(mt,
     # composite = []
     chordSub = None
     i = 0
-    iGathered = []  # store a lost of indexes of gathered values put into chords
+    iGathered = []  # store a list of indexes of gathered values put into chords
     voicesRequired = False
     if len(notes) > 1:
         # environLocal.printDebug(['\n', 'midiTrackToStream(): notes', notes])
@@ -3221,7 +3221,7 @@ class Test(unittest.TestCase):
         '''
         Musescore places zero duration notes in multiple voice scenarios
         to represent double stemmed notes. Avoid false positives for extra voices.
-        # https://github.com/cuthbertLab/music21/issues/600
+        https://github.com/cuthbertLab/music21/issues/600
         '''
         from music21 import converter
 

music21/stream/__init__.py
@@ -8195,6 +8195,10 @@ class Stream(core.StreamCoreMixin, base.Music21Object):
                             ql = 0
                         unused_error, qlNew, signedError = bestMatch(
                             float(ql), quarterLengthDivisors)
+                        # Enforce nonzero duration for non-grace notes
+                        if qlNew == 0 and not e.duration.isGrace:
+                            qlNew = 1 / max(quarterLengthDivisors)
+                            signedError = ql - qlNew
                         e.duration.quarterLength = qlNew
                         if (hasattr(e, 'editorial')
                                 and signedError != 0):

music21/test/testStream.py
@@ -3637,6 +3637,19 @@ class Test(unittest.TestCase):
 
                     [8, 6])  # snap to 0.125 and 0.1666666
 
+    def testQuantizeMinimumDuration(self):
+        '''
+        Notes of nonzero duration should retain a nonzero
+        duration after quantizing.
+        '''
+        from music21 import converter
+
+        dirLib = common.getSourceFilePath() / 'midi' / 'testPrimitive'
+        fp = dirLib / 'test15.mid'  # 3 16ths, 2 32nds
+        s = converter.parse(fp, quarterLengthDivisors=[2])
+        self.assertEqual(s.flat.notes[-1].duration.quarterLength, 0.5)
+        self.assertEqual(s.flat.notes[-1].editorial.quarterLengthQuantizationError, .125 - .5)
+
     def testAnalyze(self):
         from music21 import corpus
 

commit 2523849c97510fba02730ed168e5572b6f6b5e76

Don't assign durations to instruments during MIDI parsing Regression in #641

music21/midi/translate.py
@@ -3216,6 +3216,7 @@ class Test(unittest.TestCase):
         out = midiFileToStream(mf)
         instruments = out.parts[0].getElementsByClass('Instrument')
         self.assertIsInstance(instruments[0], instrument.Oboe)
+        self.assertEqual(instruments[0].quarterLength, 0)
 
     def testImportZeroDurationNote(self):
         '''

music21/stream/__init__.py
@@ -8303,7 +8303,7 @@ class Stream(core.StreamCoreMixin, base.Music21Object):
                         unused_error, qlNew, signedError = bestMatch(
                             float(ql), quarterLengthDivisors)
                         # Enforce nonzero duration for non-grace notes
-                        if qlNew == 0 and not e.duration.isGrace:
+                        if qlNew == 0 and 'GeneralNote' in e.classes and not e.duration.isGrace:
                             qlNew = 1 / max(quarterLengthDivisors)
                             signedError = ql - qlNew
                         e.duration.quarterLength = qlNew