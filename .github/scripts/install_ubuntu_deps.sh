21520887a0d541706c4d68c7cb9e519c37f13ad1
Myke Cuthbert <cuthbert@gmail.com>
2020-12-03 12:15:11 -1000

    more debug

--- .github/scripts/install_ubuntu_deps.sh
+++ .github/scripts/install_ubuntu_deps.sh
@@ -7,7 +7,7 @@ mkdir ~/Desktop
 sudo apt-get install -y libpng-dev
 sudo apt-get install -y python-qt4
 wget -q http://lilypond.org/downloads/binaries/linux-64/lilypond-2.19.83-1.linux-64.sh
-sh lilypond-2.19.83-1.linux-64.sh --batch --prefix /usr/local/bin
+sh lilypond-2.19.83-1.linux-64.sh --batch
 export PATH=/usr/local/bin/lilypond/bin:/usr/local/bin/lilypond:$PATH
 pip3 install -r requirements.txt
 pip3 install coveralls
