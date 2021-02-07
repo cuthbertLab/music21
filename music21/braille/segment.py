05bfb1347d8e4143368c989bed0ab8853fd4c0d7
Jacob Walls <jacobtylerwalls@gmail.com>
2021-02-07 14:43:15 -0500

    Revert "Coveralls says this doesn't run anymore"
    This reverts commit 81c773cf49cf155edd534406d157dcdf00f972b2.

--- music21/braille/segment.py
+++ music21/braille/segment.py
@@ -1670,8 +1670,12 @@ def prepareSlurredNotes(music21Part, **keywords):
     for slur in music21Part.spannerBundle.getByClass(spanner.Slur):
         firstNote = slur[0]
         lastNote = slur[1]
-        beginIndex = allNotes.index(firstNote)
-        endIndex = allNotes.index(lastNote)
+
+        try:
+            beginIndex = allNotes.index(firstNote)
+            endIndex = allNotes.index(lastNote)
+        except exceptions21.StreamException:
+            continue
 
         delta = abs(endIndex - beginIndex) + 1
 
