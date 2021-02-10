8fa53bbb55a73a4d8cf7b6c42350b06b0ecd59ea
Sven <svenh91@gmail.com>
2021-02-10 11:11:18 +0000

    fix flake issues

--- music21/osmd/__init__.py
+++ music21/osmd/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Name:         osmd/__init__.py
 # Purpose:      music21 classes for displaying music21 scores in IPython notebooks
 #
@@ -7,11 +7,11 @@
 #
 # Copyright:    Copyright © 2012-14 Michael Scott Cuthbert and the music21 Project
 # License:      LGPL or BSD, see license.txt
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 __all__ = ['ConverterOpenSheetMusicDisplay']
 
 from music21.osmd.osmd import ConverterOpenSheetMusicDisplay
+from music21.converter import registerSubconverter
 
-from music21.converter import registerSubconverter 
 registerSubconverter(ConverterOpenSheetMusicDisplay)
