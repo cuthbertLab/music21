52caa22c560b813409697f40a8d40f2d503a2d8b
Jacob Walls <jacobtylerwalls@gmail.com>
2021-03-29 11:00:06 -0400

    remove verbose=True

--- music21/converter/subConverters.py
+++ music21/converter/subConverters.py
@@ -1546,6 +1546,6 @@ if __name__ == '__main__':
     import music21
     # import sys
     # sys.argv.append('SimpleTextShow')
-    music21.mainTest(Test, verbose=True)
+    music21.mainTest(Test)
     # run command below to test commands that open musescore, etc.
     # music21.mainTest(TestExternal)
