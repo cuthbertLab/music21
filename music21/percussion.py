1033f55876fe74c7eecd2e00e93a9eb40230a62b
Myke Cuthbert <cuthbert@gmail.com>
2021-04-28 08:37:50 -1000

    initial work on unpitched

---
+++ music21/percussion.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+# Name:         note.py
+# Purpose:      music21 classes for representing notes
+#
+# Authors:      Michael Scott Cuthbert
+#               Christopher Ariza
+#
+# Copyright:    Copyright © 2006-2019 Michael Scott Cuthbert and the music21 Project
+# License:      BSD, see license.txt
+# ------------------------------------------------------------------------------
