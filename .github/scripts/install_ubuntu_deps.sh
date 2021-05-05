cuthbertLab/music21

commit 0c47a64df49330e8fc798742cad88850a00b3447

remove test that rebuilds core corpus

music21/metadata/bundles.py
@@ -153,13 +153,9 @@ class MetadataBundle(object):
         >>> coreBundle
         <music21.metadata.bundles.MetadataBundle 'core': {14958 entries}>
 
-    ::
-
         >>> searchResults = coreBundle.search('bach', field='composer')
         >>> searchResults
         <music21.metadata.bundles.MetadataBundle {21 entries}>
-
-    ::
 
         >>> resultsEntries = searchResults.search('3/4')
         >>> resultsEntries
@@ -191,16 +187,12 @@ class MetadataBundle(object):
         >>> localBundle = metadata.MetadataBundle.fromLocalCorpus()
         >>> virtualBundle = metadata.MetadataBundle.fromVirtualCorpus()
 
-    ::
-
         >>> coreCorpus = corpus.CoreCorpus()
         >>> coreBundle = metadata.MetadataBundle(coreCorpus)
         >>> localCorpus = corpus.LocalCorpus()
         >>> localBundle = metadata.MetadataBundle(localCorpus)
         >>> virtualCorpus = corpus.VirtualCorpus()
         >>> virtualBundle = metadata.MetadataBundle(virtualCorpus)
-
-    ::
 
         >>> coreBundle = metadata.MetadataBundle('core')
         >>> localBundle = metadata.MetadataBundle('local')
@@ -229,15 +221,9 @@ class MetadataBundle(object):
         ...     )
         >>> bachBundle
         <music21.metadata.bundles.MetadataBundle {21 entries}>
-
-    ::
-
         >>> tripleMeterBundle = coreBundle.search('3/4')
         >>> tripleMeterBundle
         <music21.metadata.bundles.MetadataBundle {2012 entries}>
-
-    ::
-
         >>> bachBundle.intersection(tripleMeterBundle)
         <music21.metadata.bundles.MetadataBundle {4 entries}>
 
@@ -250,16 +236,10 @@ class MetadataBundle(object):
         >>> paths = corpus.CoreCorpus().getMonteverdiMadrigals()[:4]
         >>> failedPaths = anonymousBundle.addFromPaths(
         ...     paths, useMultiprocessing=False)
-
-    ::
-
         >>> failedPaths
         []
-
-    ::
-
         >>> anonymousBundle
-        <music21.metadata.bundles.MetadataBundle {4 entries}>
+        <music21.metadata.bundles.MetadataBundle {4 entries}>        
 
     '''
 
@@ -284,24 +264,15 @@ class MetadataBundle(object):
 
             >>> from music21 import metadata
             >>> coreBundle = metadata.MetadataBundle.fromCoreCorpus()
-
-        ::
-
             >>> bachBundle = coreBundle.search(
             ...     'bach',
             ...     field='composer',
             ...     )
             >>> bachBundle
             <music21.metadata.bundles.MetadataBundle {21 entries}>
-
-        ::
-
             >>> tripleMeterBundle = coreBundle.search('3/4')
             >>> tripleMeterBundle
             <music21.metadata.bundles.MetadataBundle {2012 entries}>
-
-        ::
-
             >>> bachBundle & tripleMeterBundle
             <music21.metadata.bundles.MetadataBundle {4 entries}>
 
@@ -331,17 +302,11 @@ class MetadataBundle(object):
             ...     )
             >>> bachBundle == beethovenBundle
             False
-
-        ::
-
             >>> bachBundle == coreBundle.search(
             ...     'bach',
             ...     field='composer',
             ...     )
             True
-
-        ::
-
             >>> bachBundle == 'foo'
             False
 
@@ -368,48 +333,22 @@ class MetadataBundle(object):
             ...     'beethoven',
             ...     field='composer',
             ...     )
-
-        ::
-
             >>> bachBundle >= bachBundle
             True
-
-        ::
-
             >>> bachBundle >= beethovenBundle
             False
-
-        ::
-
             >>> bachBundle >= coreBundle
             False
-
-        ::
-
             >>> beethovenBundle >= bachBundle
             False
-
-        ::
-
             >>> beethovenBundle >= beethovenBundle
             True
-
-        ::
-
             >>> beethovenBundle >= coreBundle
             False
-
-        ::
-
             >>> coreBundle >= bachBundle
             True
-
-        ::
             >>> coreBundle >= beethovenBundle
             True
-
-        ::
-
             >>> coreBundle >= coreBundle
             True
 
@@ -437,48 +376,22 @@ class MetadataBundle(object):
             ...     'beethoven',
             ...     field='composer',
             ...     )
-
-        ::
-
             >>> bachBundle > bachBundle
             False
-
-        ::
-
             >>> bachBundle > beethovenBundle
             False
-
-        ::
-
             >>> bachBundle > coreBundle
             False
-
-        ::
-
             >>> beethovenBundle > bachBundle
             False
-
-        ::
-
             >>> beethovenBundle > beethovenBundle
             False
-
-        ::
-
             >>> beethovenBundle > coreBundle
             False
-
-        ::
-
             >>> coreBundle > bachBundle
             True
-
-        ::
             >>> coreBundle > beethovenBundle
             True
-
-        ::
-
             >>> coreBundle > coreBundle
             False
 
@@ -505,47 +418,22 @@ class MetadataBundle(object):
             ...     field='composer',
             ...     )
 
-        ::
-
             >>> bachBundle <= bachBundle
             True
-
-        ::
-
             >>> bachBundle <= beethovenBundle
             False
-
-        ::
-
             >>> bachBundle <= coreBundle
             True
-
-        ::
-
             >>> beethovenBundle <= bachBundle
             False
-
-        ::
-
             >>> beethovenBundle <= beethovenBundle
             True
-
-        ::
-
             >>> beethovenBundle <= coreBundle
             True
-
-        ::
-
             >>> coreBundle <= bachBundle
             False
-
-        ::
             >>> coreBundle <= beethovenBundle
             False
-
-        ::
-
             >>> coreBundle <= coreBundle
             True
 
@@ -572,48 +460,22 @@ class MetadataBundle(object):
             ...     'beethoven',
             ...     field='composer',
             ...     )
-
-        ::
-
             >>> bachBundle < bachBundle
             False
-
-        ::
-
             >>> bachBundle < beethovenBundle
             False
-
-        ::
-
             >>> bachBundle < coreBundle
             True
-
-        ::
-
             >>> beethovenBundle < bachBundle
             False
-
-        ::
-
             >>> beethovenBundle < beethovenBundle
             False
-
-        ::
-
             >>> beethovenBundle < coreBundle
             True
-
-        ::
-
             >>> coreBundle < bachBundle
             False
-
-        ::
             >>> coreBundle < beethovenBundle
             False
-
-        ::
-
             >>> coreBundle < coreBundle
             False
 
@@ -632,27 +494,18 @@ class MetadataBundle(object):
 
             >>> from music21 import metadata
             >>> coreBundle = metadata.MetadataBundle.fromCoreCorpus()
-
-        ::
-
             >>> bachBundle = coreBundle.search(
             ...     'bach',
             ...     field='composer',
             ...     )
             >>> bachBundle
             <music21.metadata.bundles.MetadataBundle {21 entries}>
-
-        ::
-
             >>> beethovenBundle = coreBundle.search(
             ...     'beethoven',
             ...     field='composer',
             ...     )
             >>> beethovenBundle
             <music21.metadata.bundles.MetadataBundle {16 entries}>
-
-        ::
-
             >>> bachBundle | beethovenBundle
             <music21.metadata.bundles.MetadataBundle {37 entries}>
 
@@ -684,24 +537,15 @@ class MetadataBundle(object):
 
             >>> from music21 import metadata
             >>> coreBundle = metadata.MetadataBundle.fromCoreCorpus()
-
-        ::
-
             >>> bachBundle = coreBundle.search(
             ...     'bach',
             ...     field='composer',
             ...     )
             >>> bachBundle
             <music21.metadata.bundles.MetadataBundle {21 entries}>
-
-        ::
-
             >>> tripleMeterBundle = coreBundle.search('3/4')
             >>> tripleMeterBundle
             <music21.metadata.bundles.MetadataBundle {2012 entries}>
-
-        ::
-
             >>> bachBundle - tripleMeterBundle
             <music21.metadata.bundles.MetadataBundle {17 entries}>
 
@@ -720,9 +564,6 @@ class MetadataBundle(object):
 
             >>> from music21 import metadata
             >>> coreBundle = metadata.MetadataBundle.fromCoreCorpus()
-
-        ::
-
             >>> bachBundle = coreBundle.search(
             ...     'bach',
             ...     field='composer',
@@ -730,14 +571,9 @@ class MetadataBundle(object):
             >>> bachBundle
             <music21.metadata.bundles.MetadataBundle {21 entries}>
 
-        ::
-
             >>> tripleMeterBundle = coreBundle.search('3/4')
             >>> tripleMeterBundle
             <music21.metadata.bundles.MetadataBundle {2012 entries}>
-
-        ::
-
             >>> bachBundle ^ tripleMeterBundle
             <music21.metadata.bundles.MetadataBundle {2025 entries}>
 
@@ -787,9 +623,6 @@ class MetadataBundle(object):
             >>> coreBundle = metadata.MetadataBundle.fromCoreCorpus()
             >>> coreBundle
             <music21.metadata.bundles.MetadataBundle 'core': {14958 entries}>
-
-        ::
-
             >>> coreBundle.corpus
             <music21.corpus.corpora.CoreCorpus>
 
@@ -840,9 +673,6 @@ class MetadataBundle(object):
             >>> from music21 import metadata
             >>> metadata.MetadataBundle().name is None
             True
-
-        ::
-
             >>> metadata.MetadataBundle.fromCoreCorpus().name == 'core'
             True
 
@@ -857,6 +687,7 @@ class MetadataBundle(object):
         paths,
         useCorpus=False,
         useMultiprocessing=True,
+        storeOnDisk=True,
         ):
         '''
         Parse and store metadata from numerous files.
@@ -875,11 +706,9 @@ class MetadataBundle(object):
             ...     corpus.getWorkList('bwv66.6'),
             ...     useCorpus=True,
             ...     useMultiprocessing=False,
+            ...     storeOnDisk=False, #_DOCS_HIDE
             ...     )
             []
-
-        ::
-
             >>> len(metadataBundle._metadataEntries)
             1
 
@@ -933,10 +762,11 @@ class MetadataBundle(object):
             accumulatedErrors.extend(result['errors'])
             for metadataEntry in result['metadataEntries']:
                 self._metadataEntries[metadataEntry.corpusPath] = metadataEntry
-            if (currentIteration % 50) == 0:
+            if (currentIteration % 50) and (storeOnDisk is True) == 0:
                 self.write()
         self.validate()
-        self.write()
+        if storeOnDisk is True:
+            self.write()
         return accumulatedErrors
 
     def clear(self):
@@ -1410,14 +1240,14 @@ class MetadataBundle(object):
             >>> from music21 import corpus, metadata
             >>> workList = corpus.getWorkList('ciconia')
             >>> metadataBundle = metadata.MetadataBundle()
-            >>> metadataBundle.addFromPaths(
+            >>> failedPaths = metadataBundle.addFromPaths(
             ...     workList,
             ...     useCorpus=True,
             ...     useMultiprocessing=False,
+            ...     storeOnDisk=False, #_DOCS_HIDE
             ...     )
+            >>> failedPaths
             []
-
-        ::
 
             >>> searchResult = metadataBundle.search(
             ...     'cicon',
@@ -1425,19 +1255,10 @@ class MetadataBundle(object):
             ...     )
             >>> searchResult
             <music21.metadata.bundles.MetadataBundle {1 entry}>
-
-        ::
-
             >>> len(searchResult)
             1
-
-        ::
-
             >>> searchResult[0]
             <music21.metadata.bundles.MetadataEntry: ciconia_quod_jactatur_xml>
-
-        ::
-
             >>> searchResult = metadataBundle.search(
             ...     'cicon',
             ...     field='composer',
@@ -1445,8 +1266,6 @@ class MetadataBundle(object):
             ...     )
             >>> len(searchResult) # no files in this format
             0
-
-        ::
 
             >>> searchResult = metadataBundle.search(
             ...     'cicon',
@@ -1492,24 +1311,15 @@ class MetadataBundle(object):
 
             >>> from music21 import metadata
             >>> coreBundle = metadata.MetadataBundle.fromCoreCorpus()
-
-        ::
-
             >>> bachBundle = coreBundle.search(
             ...     'bach',
             ...     field='composer',
             ...     )
             >>> bachBundle
             <music21.metadata.bundles.MetadataBundle {21 entries}>
-
-        ::
-
             >>> tripleMeterBundle = coreBundle.search('3/4')
             >>> tripleMeterBundle
             <music21.metadata.bundles.MetadataBundle {2012 entries}>
-
-        ::
-
             >>> bachBundle.symmetric_difference(tripleMeterBundle)
             <music21.metadata.bundles.MetadataBundle {2025 entries}>
 
@@ -1528,26 +1338,18 @@ class MetadataBundle(object):
 
             >>> from music21 import metadata
             >>> coreBundle = metadata.MetadataBundle.fromCoreCorpus()
-
-        ::
-
             >>> bachBundle = coreBundle.search(
             ...     'bach',
             ...     field='composer',
             ...     )
             >>> bachBundle
             <music21.metadata.bundles.MetadataBundle {21 entries}>
-
-        ::
-
             >>> beethovenBundle = coreBundle.search(
             ...     'beethoven',
             ...     field='composer',
             ...     )
             >>> beethovenBundle
             <music21.metadata.bundles.MetadataBundle {16 entries}>
-
-        ::
 
             >>> bachBundle.union(beethovenBundle)
             <music21.metadata.bundles.MetadataBundle {37 entries}>
@@ -1614,16 +1416,11 @@ class MetadataBundle(object):
             >>> bachBundle.filePath is None
             True
 
-        ::
-
             >>> import os
             >>> import tempfile
             >>> tempFilePath = tempfile.mkstemp()[1]
             >>> bachBundle.write(filePath=tempFilePath)
             <music21.metadata.bundles.MetadataBundle {21 entries}>
-
-        ::
-
             >>> os.remove(tempFilePath)
 
         '''

music21/stream/__init__.py
@@ -11187,7 +11187,7 @@ class Measure(Stream):
         >>> m.barDuration.fullName
         'Dotted Half'
         >>> m.duration.fullName
-        'Whole tied to Quarter (5.0 total QL)'
+        'Whole tied to Quarter (5 total QL)'
         ''')
 
     #---------------------------------------------------------------------------

music21/layout.py
@@ -976,7 +976,7 @@ class LayoutScore(stream.Opus):
 
         >>> staffLayout031 = ls.pages[0].systems[3].staves[1].staffLayout
         >>> staffLayout031
-        <music21.layout.StaffLayout distance None, staffNumber None, staffSize 80.0, staffLines 5.0>
+        <music21.layout.StaffLayout distance None, staffNumber None, staffSize 80.0, staffLines 5>
         >>> staffLayout031.hidden
         True
 

music21/metadata/caching.py
@@ -46,13 +46,7 @@ def cacheMetadata(
     '''
     Cache metadata from corpuses in `corpusNames` as local cache files:
 
-    ::
-
-        >>> from music21 import metadata
-        >>> metadata.cacheMetadata(
-        ...     corpusNames='core',
-        ...     useMultiprocessing=False,
-        ...     )
+    Call as ``metadata.cacheMetadata()``
 
     '''
     from music21 import corpus

commit 7fec3f5b9df49c11d5a2eac4de95497236805c0d

More miscellany (#993) (JTW)
* Use pathlib in Environment.__setitem__

* Module names

* Fix compileall command

* Typo in ch. 53

* Update test coverage

* Fix bug with intermittent metadata writing
Regression in 0c47a64df49330e8fc798742cad88850a00b3447.

* Enable providing path to `saveFile` arg in `runTranscribe()`
Paths are truthy, so we executed `if saveFile` and ignored the input.

* Better failure in audioSearch module if no pyaudio
ImportError is better than NoneType has no attribute...

* Remove unused EnumerationException

* Prevent crash in exporting MIDI from having MetronomeMark without a number.

* Reduce indentation in midiTrackToStream()

* Don't export invalid StringIndications
Number must be > 0

* Remove check for hasattr 'software'

* Remove moot TODO
Has been working since at least 5.1.0

* Add VS Code config files to .gitignore

* Avoid some calls to hyphenToCamelCase()

* Restore `- minPitch` for future safety

* Clarify return of None in new doctest

* README typo

* Correct name is `rebuildMetadataCache()`

* Revert "Reduce indentation in midiTrackToStream()"

This reverts commit 6ff0377bad2e7afed4fe566f17224329c680b54d.

* Revert "Use pathlib in Environment.__setitem__"

This reverts commit 3ae828951db47ff09ec75c6ea3533b991e7ff518.

* Spelling

.github/scripts/install_ubuntu_deps.sh
@@ -5,7 +5,7 @@ set -e  # error if anything returns non-zero exit code
 mkdir ~/Desktop
 
 sudo apt-get install -y libpng-dev
-# sudo apt-get install -y python-qt4
+# sudo apt-get install -y python3-pyqt5
 wget -q https://lilypond.org/download/binaries/linux-64/lilypond-2.22.0-1.linux-64.sh
 sh lilypond-2.22.0-1.linux-64.sh --batch
 export PATH=/home/runner/bin:$PATH
@@ -15,5 +15,4 @@ pip3 install scipy
 pip3 install python-Levenshtein
 pip3 install setuptools
 pip3 install coverage
-cd music21
-python -m compileall music21
+python3 -m compileall music21

.gitignore
@@ -60,6 +60,9 @@ nosetests.xml
 .pydevproject
 /.settings
 
+# VS Code
+/.vscode
+
 # remove all from autogenerated except conf.py
 documentation/autogenerated/**/*
 documentation/autogenerated/**/*.png

documentation/source/developerReference/testing.ipynb
@@ -210,7 +210,7 @@
     "\n",
     "    https://coveralls.io/github/cuthbertLab/music21\n",
     "\n",
-    "While 100% code coverage is impossible, increasing the coverage percentage with each new contribution is essential.  We're at 91% code coverage and get the green badge of pride on our README.md file, and we like that!\n",
+    "While 100% code coverage is impossible, increasing the coverage percentage with each new contribution is essential.  We're at 92% code coverage and get the green badge of pride on our README.md file, and we like that!\n",
     "\n",
     "If something should be impossible to trigger, but the code is being maintained for some reason, you can add \"`# pragma: no cover`\" to the line or to the fork, but this is generally a no-no."
    ]

documentation/source/usersGuide/usersGuide_32_articulations.ipynb
@@ -471,7 +471,7 @@
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "Again, unfortuantely, some MusicXML readers do not respect this value.\n",
+    "Again, unfortunately, some MusicXML readers do not respect this value.\n",
     "\n",
     "MusicXML distinguishes articulations from technical indicates, but in `music21` these are all in the `articulations` module and stored in the `.articulations` list.  They do, however, have `TechnicalIndication` in their classes: "
    ]

documentation/source/usersGuide/usersGuide_53_advancedCorpus.ipynb
@@ -437,7 +437,7 @@
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "Consult the API for class:`~music21.metadata.bundles.MetadataBundle` for a more\n",
+    "Consult the API for :class:`~music21.metadata.bundles.MetadataBundle` for a more\n",
     "in depth look at how this works."
    ]
   },
@@ -693,7 +693,7 @@
    "metadata": {},
    "source": [
     "You can delete, rebuild and save a metadata bundle in one go with the\n",
-    "``rebuild()`` method:"
+    "``rebuildMetadataCache()`` method:"
    ]
   },
   {
@@ -703,7 +703,7 @@
    "outputs": [],
    "source": [
     "localBundle = corpus.corpora.LocalCorpus().metadataBundle\n",
-    "#_DOCS_SHOW localBundle.rebuild()"
+    "#_DOCS_SHOW localBundle.rebuildMetadataCache()"
    ]
   },
   {

music21/analysis/discrete.py
@@ -983,7 +983,7 @@ class Ambitus(DiscreteAnalysis):
         else:  # create minPitch maxPitch
             maxPitch = numColors
 
-        valueRange = maxPitch
+        valueRange = maxPitch - minPitch
         if valueRange == 0:
             valueRange = 1  # avoid float division by zero
         step = 0

music21/audioSearch/recording.py
@@ -53,14 +53,9 @@ def samplesFromRecording(seconds=10.0, storeFile=True,
 
     Returns a list of samples.
     '''
-    try:
-        # noinspection PyPackageRequirements
-        import pyaudio  # @UnresolvedImport
-        recordFormatDefault = pyaudio.paInt16
-    except (ImportError, SystemExit):
-        pyaudio = None
-        environLocal.warn("No Pyaudio found. Recording will probably not work.")
-        recordFormatDefault = 8  # pyaudio.paInt16
+    # noinspection PyPackageRequirements
+    import pyaudio  # @UnresolvedImport  # pylint: disable=import-error
+    recordFormatDefault = pyaudio.paInt16
 
     if recordFormat is None:
         recordFormat = recordFormatDefault

music21/audioSearch/transcriber.py
@@ -48,13 +48,12 @@ def runTranscribe(show=True, plot=True, useMic=True,
     if useScale is None:
         useScale = scale.ChromaticScale('C4')
     # beginning - recording or not
-    if saveFile is not False:
-        if saveFile:
-            waveFilename = environLocal.getRootTempDir() / 'ex.wav'
-        else:
-            waveFilename = saveFile
-    else:
+    if saveFile is True:
+        waveFilename = environLocal.getRootTempDir() / 'ex.wav'
+    elif saveFile is False:
         waveFilename = False
+    else:
+        waveFilename = saveFile
 
     # the rest of the score
     if useMic is True:

music21/metadata/bundles.py
@@ -815,7 +815,7 @@ class MetadataBundle(prebase.ProtoM21Object):
             accumulatedErrors.extend(result['errors'])
             for metadataEntry in result['metadataEntries']:
                 self._metadataEntries[metadataEntry.corpusPath] = metadataEntry
-            if (currentIteration % 50) and (storeOnDisk is True) == 0:
+            if (currentIteration % 50 == 0) and storeOnDisk is True:
                 self.write()
         self.validate()
         if storeOnDisk is True:

music21/midi/__init__.py
@@ -29,7 +29,6 @@ __all__ = [
     'DeltaTime',
     'MetaEvents', 'ChannelVoiceMessages', 'ChannelModeMessages',
     'SysExEvents',
-    'EnumerationException',
 ]
 
 import io
@@ -59,10 +58,6 @@ environLocal = environment.Environment(_MOD)
 # good midi reference:
 # http://www.sonicspot.com/guide/midifiles.html
 # ------------------------------------------------------------------------------
-class EnumerationException(exceptions21.Music21Exception):
-    pass
-
-
 class MidiException(exceptions21.Music21Exception):
     pass
 

music21/midi/translate.py
@@ -1064,8 +1064,16 @@ def tempoToMidiEvents(tempoIndication, includeDeltaTime=True):
 
     >>> midi.translate.midiEventsToTempo(events)
     <music21.tempo.MetronomeMark maestoso Quarter=90.0>
+
+    `None` is returned if the MetronomeMark lacks a number, which can
+    happen with metric modulation marks.
+
+    >>> midi.translate.tempoToMidiEvents(tempo.MetronomeMark(number=None)) is None
+    True
     '''
     from music21 import midi as midiModule
+    if tempoIndication.number is None:
+        return
     mt = None  # use a midi track set to None
     eventList = []
     if includeDeltaTime:

music21/musicxml/m21ToXml.py
@@ -861,7 +861,7 @@ class XMLExporterBase:
         '''
         Sets mxObject['color'] to a normalized version of m21Object.style.color
         '''
-        self.setStyleAttributes(mxObject, m21Object, 'color')
+        self.setStyleAttributes(mxObject, m21Object, 'color', 'color')
         if 'color' in mxObject.attrib:  # set
             mxObject.attrib['color'] = normalizeColor(mxObject.attrib['color'])
 
@@ -2239,8 +2239,7 @@ class ScoreExporter(XMLExporterBase, PartStaffExporterMixin):
         mxEncodingDate.text = str(datetime.date.today())  # right format...
         # TODO: encoder
 
-        if self.scoreMetadata is not None and hasattr(self.scoreMetadata, 'software'):
-            # TODO: Remove hasattr after all caches are cleared after v5 release.
+        if self.scoreMetadata is not None:
             for software in self.scoreMetadata.software:
                 mxSoftware = SubElement(mxEncoding, 'software')
                 mxSoftware.text = software
@@ -2796,8 +2795,6 @@ class MeasureExporter(XMLExporterBase):
         Note that if the .id is high enough to be an id(x) memory location, then a small
         voice number is used instead.
         '''
-        # TODO: fix mid-measure clef change with voices and part-staff in Schoenberg op. 19 no 2
-        # staff 2, m. 6... -- placed at the beginning of the measure not at the appropriate place.
         root = self.xmlRoot
         divisions = self.currentDivisions
         self.offsetInMeasure = 0.0
@@ -4058,6 +4055,8 @@ class MeasureExporter(XMLExporterBase):
 
         for artObj in applicableArticulations:
             if 'Pizzicato' in artObj.classes:
+                continue
+            if 'StringIndication' in artObj.classes and artObj.number < 1:
                 continue
             if 'TechnicalIndication' in artObj.classes:
                 if mxTechnicalMark is None:
@@ -5500,7 +5499,7 @@ class MeasureExporter(XMLExporterBase):
 
         # not to be done: repeater (deprecated)
         self.setColor(mxBeam, beamObject)
-        self.setStyleAttributes(mxBeam, beamObject, 'fan')
+        self.setStyleAttributes(mxBeam, beamObject, 'fan', 'fan')
 
         return mxBeam
 
@@ -6508,6 +6507,20 @@ class Test(unittest.TestCase):
         self.assertEqual(len(tree.findall('.//rest')), 1)
         rest = tree.find('.//rest')
         self.assertEqual(rest.get('measure'), 'yes')
+
+    def testArticulationSpecialCases(self):
+        from music21 import articulations
+
+        n = note.Note()
+        a = articulations.StringIndication()
+        n.articulations.append(a)
+
+        # Legal values for StringIndication begin at 1
+        self.assertEqual(a.number, 0)
+        # Use GEX to go through wellformed object conversion
+        gex = GeneralObjectExporter(n)
+        tree = ET.fromstring(gex.parse().decode('utf-8'))
+        self.assertIsNone(tree.find('.//string'))
 
     def testMeasurePadding(self):
         from music21 import converter

music21/musicxml/xmlToM21.py
@@ -474,7 +474,7 @@ class XMLParserBase:
         '''
         Sets m21Object.style.color to be the same as color...
         '''
-        self.setStyleAttributes(mxObject, m21Object, 'color')
+        self.setStyleAttributes(mxObject, m21Object, 'color', 'color')
 
     def setFont(self, mxObject, m21Object):
         '''
@@ -2804,7 +2804,7 @@ class MeasureParser(XMLParserBase):
         # TODO: get number to preserve
         # not to-do: repeater; is deprecated.
         self.setColor(mxBeam, beamOut)
-        self.setStyleAttributes(mxBeam, beamOut, 'fan')
+        self.setStyleAttributes(mxBeam, beamOut, 'fan', 'fan')
 
         try:
             mxType = mxBeam.text.strip()

music21/stream/base.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
-# Name:         stream/__init__.py
+# Name:         stream/base.py
 # Purpose:      base classes for dealing with groups of positioned objects
 #
 # Authors:      Michael Scott Cuthbert
@@ -7719,7 +7719,7 @@ class Stream(core.StreamCoreMixin, base.Music21Object):
         if s1.containerInHierarchy(n) is called, it will return m1,
         the Measure that contains the note.
 
-        Unless `setActiveSite` is False, n's activeStie will be set to m1, and
+        Unless `setActiveSite` is False, n's activeSite will be set to m1, and
         its `.offset` will be the offset in `m1`.
 
         >>> s1 = stream.Score(id='s1')

music21/stream/core.py
@@ -669,7 +669,7 @@ class StreamCoreMixin:
         that spannerBundle are added.  This can be useful, for instance, in restoring
         spanners from an excerpt that might already have spanners removed.  In
         Jacob Tyler Walls's brilliant phrasing, it prevents regrowing zombie spanners
-        the you thought you had killed.
+        that you thought you had killed.
 
         Here we will constrain only to spanners also present in another Stream:
 

music21/stream/filters.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
-# Name:         stream/filter.py
+# Name:         stream/filters.py
 # Purpose:      classes for filtering iterators of  streams...
 #
 # Authors:      Michael Scott Cuthbert

music21/stream/iterator.py
@@ -637,7 +637,7 @@ class StreamIterator(prebase.ProtoM21Object):
          <music21.note.Note E>, <music21.note.Note F>, <music21.note.Note G>,
          <music21.note.Note A>]
 
-        If restoreActiveSties is False then the elements will not have
+        If restoreActiveSites is False then the elements will not have
         their activeSites changed (callers should use it when they do not plan to actually
         expose the elements to users, such as in `__len__`).
 

music21/vexflow/toMusic21j.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
-# Name:         vexflow/toM21p.py
+# Name:         vexflow/toM21j.py
 # Purpose:      music21 classes for converting music21 objects to music21j
 #
 # Authors:      Michael Scott Cuthbert