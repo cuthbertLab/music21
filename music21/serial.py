3721f46c09f74e6ace52737e43f964546f42f07a
Jacob Walls <jacobtylerwalls@gmail.com>
2021-05-22 15:58:11 -0400

    Improve changed notice

--- music21/serial.py
+++ music21/serial.py
@@ -1074,8 +1074,8 @@ class TwelveToneRow(ToneRow):
         >>> moses.areCombinatorial('R', 6, 'RI', 4)
         False
 
-        Changed in v.7 -- `convention` is no longer necessary and defaults None;
-        to be removed in v.8.
+        Changed in v.7 -- `convention` is no longer necessary and no longer used.
+        Renamed to `unused_convention` and defaults None; to be removed in v.8.
         '''
         if self.isTwelveToneRow() is False:
             raise SerialException('Combinatoriality applies only to twelve-tone rows.')
