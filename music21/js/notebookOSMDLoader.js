212b98e3af1cee64293e723bb1fbf25c2bfa9eba
Jacob Walls <jacobtylerwalls@gmail.com>
2021-09-11 17:04:09 -0400

    Comment out console logging

--- music21/js/notebookOSMDLoader.js
+++ music21/js/notebookOSMDLoader.js
@@ -1,19 +1,19 @@
-console.log("loadOSMD()");
+// console.log("loadOSMD()");
 function loadOSMD() { 
     return new Promise(function(resolve, reject){
 
         if (window.opensheetmusicdisplay) {
-            console.log("already loaded")
+            // console.log("already loaded")
             return resolve(window.opensheetmusicdisplay)
         }
-        console.log("loading osmd for the first time")
+        // console.log("loading osmd for the first time")
         // OSMD script has a 'define' call which conflicts with requirejs
         var _define = window.define // save the define object 
         window.define = undefined // now the loaded script will ignore requirejs
         var s = document.createElement( 'script' );
         function oncompleted(){
             window.define = _define
-            console.log("loaded OSMD for the first time",opensheetmusicdisplay)
+            // console.log("loaded OSMD for the first time",opensheetmusicdisplay)
             resolve(opensheetmusicdisplay);
         };
         
@@ -34,16 +34,16 @@ function loadOSMD() {
     }) 
 }
 loadOSMD().then((OSMD)=>{
-    console.log("loaded OSMD")
+    // console.log("loaded OSMD")
     var div_id = "{{DIV_ID}}";
-        console.log(div_id)
+        // console.log(div_id)
     document.querySelector('#'+div_id).innerHTML = "";
     window.openSheetMusicDisplay = new OSMD.OpenSheetMusicDisplay(div_id);
     openSheetMusicDisplay
         .load("{{data}}") // this is replaced by the xml generated in python
         .then(
           function() {
-            console.log("rendering data")
+            // console.log("rendering data")
             openSheetMusicDisplay.render();
             // we could also remove this script tag to free up memory (would limit debugging though)
             // var me = document.currentScript;
