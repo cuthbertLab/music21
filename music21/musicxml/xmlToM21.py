8cb2387f77724b6862e4daec41b75eaf4928c480
Jacob Walls <jacobtylerwalls@gmail.com>
2021-10-11 22:46:41 -0400

    preserve same instance

--- music21/musicxml/xmlToM21.py
+++ music21/musicxml/xmlToM21.py
@@ -3940,12 +3940,12 @@ class MeasureParser(XMLParserBase):
             self.setColor(mxObj, slur)
 
         for mxObj in mxNotations.findall('technical/hammer-on'):
-            self.xmlOneSpanner(mxObj, n, articulations.HammerOn)
-            n.articulations.append(articulations.HammerOn())
+            ho = self.xmlOneSpanner(mxObj, n, articulations.HammerOn)
+            n.articulations.append(ho)
 
         for mxObj in mxNotations.findall('technical/pull-off'):
-            self.xmlOneSpanner(mxObj, n, articulations.PullOff)
-            n.articulations.append(articulations.PullOff())
+            po = self.xmlOneSpanner(mxObj, n, articulations.PullOff)
+            n.articulations.append(po)
 
         for tagSearch in ('glissando', 'slide'):
             for mxObj in mxNotations.findall(tagSearch):
