8f3f1c3358b57f4eebc737524bdacc8d970c1a12
Jacob Walls <jacobtylerwalls@gmail.com>
2022-01-19 09:16:32 -0500

    Add deprecation warning for TimeSignature.loadRatio()

--- music21/meter/base.py
+++ music21/meter/base.py
@@ -237,10 +237,9 @@ def bestTimeSignature(meas: 'music21.stream.Stream') -> 'music21.meter.TimeSigna
         else:
             return ts2
 
-    # environLocal.printDebug(['n/d', numerator, denominator])
     else:
         ts = TimeSignature()
-        ts.loadRatio(numerator, denominator)
+        ts.load(f'{numerator}/{denominator}')
         return ts
 
 
@@ -562,6 +561,7 @@ class TimeSignature(base.Music21Object):
             except MeterException:
                 environLocal.printDebug(['cannot set default accents for:', self])
 
+    @common.deprecated('v7', 'v8', 'call .ratioString or .load()')
     def loadRatio(self, numerator, denominator, divisions=None):
         '''
         Change the numerator and denominator, like ratioString, but with
