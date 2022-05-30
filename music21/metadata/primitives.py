e9e80a3aec638ea22b5d8e19fe8e3034ba96f745
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2022-05-29 17:00:48 -0700

    More coverage (and a bugfix or two) in metadata/primitives.py.

--- music21/metadata/primitives.py
+++ music21/metadata/primitives.py
@@ -114,6 +114,9 @@ class Date(prebase.ProtoM21Object):
                 setattr(self, attr, keywords[attr])
 
     # SPECIAL METHODS #
+    def __eq__(self, other) -> bool:
+        return str(self) == str(other)
+
     def __str__(self):
         r'''
         Return a string representation, including error if defined.
@@ -446,6 +449,23 @@ class DateSingle(prebase.ProtoM21Object):
 
     # SPECIAL METHODS #
 
+    def __eq__(self, other) -> bool:
+        '''
+        >>> dd = metadata.DateSingle('1805/3/12', 'uncertain')
+        >>> dd2 = metadata.DateSingle('1805/3/12', 'uncertain')
+        >>> str(dd)
+        '1805/03/12'
+        >>> dd == dd2
+        True
+        >>> dd2.relevance='certain'
+        >>> dd == dd2
+        False
+        '''
+        return (type(self) is type(other)
+                    and self._data == other._data
+                    and self._dataError == other._dataError
+                    and self.relevance == other.relevance)
+
     def _reprInternal(self) -> str:
         return str(self)
 
@@ -767,6 +787,23 @@ class Text(prebase.ProtoM21Object):
         return str(self)
 
     def __eq__(self, other) -> bool:
+        '''
+        >>> t1 = metadata.Text('some text')
+        >>> t2 = metadata.Text('some text')
+        >>> t1 == t2
+        True
+        >>> t2 = metadata.Text('some text', language='en')
+        >>> t1 == t2
+        False
+        >>> t2 = metadata.Text('some text', isTranslated=True)
+        >>> t1 == t2
+        False
+        >>> t2 = metadata.Text('some text', encodingScheme='scheme42')
+        >>> t1 == t2
+        False
+        >>> t1 == 3
+        False
+        '''
         if type(other) is not type(self):
             return False
         if self._data != other._data:
@@ -834,10 +871,36 @@ class Copyright(Text):
     'fictitious'
     >>> str(copyleft)
     'Copyright 1969 Cuthbert'
+    >>> copyleft2 = metadata.Copyright('Copyright 1969 Cuthbert',
+    ...                 role='fictitious')
+    >>> copyleft == copyleft2
+    True
+    >>> copyleft2 = metadata.Copyright('Copyright © 1969 Cuthbert',
+    ...                 role='fictitious')
+    >>> copyleft == copyleft2
+    False
+    >>> copyleft2 = metadata.Copyright('Copyright 1969 Cuthbert', language='en',
+    ...                 role='fictitious')
+    >>> copyleft == copyleft2
+    False
+    >>> copyleft2 = metadata.Copyright('Copyright 1969 Cuthbert', isTranslated=True,
+    ...                 role='fictitious')
+    >>> copyleft == copyleft2
+    False
+    >>> copyleft2 = metadata.Copyright('Copyright 1969 Cuthbert',
+    ...                 role='facetious')
+    >>> copyleft == copyleft2
+    False
+    >>> copyleft == 3
+    False
     '''
 
-    def __init__(self, data='', language=None, *, role=None):
-        super().__init__(data, language)
+    def __init__(self,
+                 data: t.Union[str, 'Text'] = '',
+                 language: t.Optional[str] = None,
+                 isTranslated: t.Optional[bool] = None,   # True, False, or None (unknown)
+                 *, role=None):
+        super().__init__(data, language, isTranslated)
         self.role = role
 
     def __eq__(self, other) -> bool:
@@ -848,8 +911,6 @@ class Copyright(Text):
         if self.language != other.language:
             return False
         if self.isTranslated != other.isTranslated:
-            return False
-        if self.encodingScheme != other.encodingScheme:
             return False
         if self.role != other.role:
             return False
@@ -955,6 +1016,41 @@ class Contributor(prebase.ProtoM21Object):
         return self.name
 
     def __eq__(self, other) -> bool:
+        '''
+        >>> c1 = metadata.Contributor(role='composer', name='The Composer',
+        ...         birth='1923', death='2013')
+        >>> c2 = metadata.Contributor(role='composer', name='The Composer',
+        ...         birth='1923', death='2013')
+        >>> c1 == c2
+        True
+        >>> c2 = metadata.Contributor(role='lyricist', name='The Composer',
+        ...         birth='1923', death='2013')
+        >>> c1 == c2
+        False
+        >>> c2 = metadata.Contributor(role='composer', name='A Composer',
+        ...         birth='1923', death='2013')
+        >>> c1 == c2
+        False
+        >>> c2 = metadata.Contributor(role='composer', names=['A Composer',
+        ...         'The Composer'], birth='1923', death='2013')
+        >>> c1 == c2
+        False
+        >>> c2 = metadata.Contributor(role='composer', name='The Composer',
+        ...         birth='1924', death='2013')
+        >>> c1 == c2
+        False
+        >>> c2 = metadata.Contributor(role='composer', name='The Composer',
+        ...         birth='1923', death='2012')
+        >>> c1 == c2
+        False
+        >>> c2.death = '2012'
+        >>> c1 == c2
+        False
+        >>> c1 == 3
+        False
+        '''
+        if type(other) is not type(self):
+            return False
         if self._role != other._role:
             return False
         if len(self._names) != len(other._names):
