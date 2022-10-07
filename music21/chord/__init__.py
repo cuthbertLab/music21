c5f6be0484dec746c1540f7f61237c21bbe473e4
Mark Gotham <mrhgotham@gmail.com>
2022-10-07 09:02:42 +0100

    isConsonant line break fix

--- music21/chord/__init__.py
+++ music21/chord/__init__.py
@@ -2661,11 +2661,14 @@ class Chord(ChordBase):
     def isConsonant(self):
         # noinspection PyShadowingNames
         '''
-        returns True if the chord is
-             one pitch
-             two pitches: uses :meth:`~music21.interval.Interval.isConsonant()` , which
-             checks if interval is a major or minor third or sixth or perfect fifth.
-             three pitches: if chord is a major or minor triad not in second inversion.
+        Returns True if the chord is:
+        
+        * one pitch (always consonant)
+        
+        * two pitches: uses :meth:`~music21.interval.Interval.isConsonant()` , which
+             checks if the interval is a major or minor third or sixth or perfect fifth.
+        
+        * three pitches: if chord is a major or minor triad not in second inversion.
 
         These rules define all common-practice consonances
         (and earlier back to about 1300 all imperfect consonances)
