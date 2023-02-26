afcde732f1bc94fd67b1250328db0d826679b5b4
Moritz Heffter <heffter@mh-freiburg.de>
2023-02-26 18:43:49 +0100

    added meterSig to mei import

--- music21/musicxml/m21ToXml.py
+++ music21/musicxml/m21ToXml.py
@@ -362,6 +362,7 @@ class GeneralObjectExporter:
         ('DiatonicScale', 'fromDiatonicScale'),
         ('Scale', 'fromScale'),
         ('Music21Object', 'fromMusic21Object'),
+        ('FiguredBassIndication', 'fromFiguredBassIndication'),
     ])
 
     def __init__(self, obj: prebase.ProtoM21Object | None = None):
@@ -514,6 +515,9 @@ class GeneralObjectExporter:
                 + f'{self.generalObj} to a complete musicXML document; put it in a Stream first!'
             )
         return outObj
+
+    def fromFiguredBassIndication(self):
+        print('was here!')
 
     def fromScore(self, sc):
         '''
