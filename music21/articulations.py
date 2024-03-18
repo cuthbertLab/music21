3cf71e0c154d80284eee1304b02be806f646cea8
Alexandre D'Hooge <alexandre@petrucci>
2024-03-18 12:30:32 +0100

    alphabetical imports

--- music21/articulations.py
+++ music21/articulations.py
@@ -83,13 +83,9 @@ from music21 import base
 from music21 import common
 from music21.common.classTools import tempAttribute
 from music21 import environment
-from music21 import style
-from music21 import interval
 from music21 import spanner
-from music21 import interval
+from music21 import style
 
-if t.TYPE_CHECKING:
-    from music21 import interval
 
 
 environLocal = environment.Environment('articulations')
