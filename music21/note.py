150a9ce9be8e499d9d7dfd84be25e99dada66176
Tim Beyer <35711942+TimFelixBeyer@users.noreply.github.com>
2024-03-26 01:00:36 +0100

    Add 'fa up' notehead type

--- music21/note.py
+++ music21/note.py
@@ -59,6 +59,7 @@ noteheadTypeNames = (
     'diamond',
     'do',
     'fa',
+    'fa up',
     'inverted triangle',
     'la',
     'left triangle',
@@ -1132,7 +1133,7 @@ class NotRest(GeneralNote):
 
         >>> note.noteheadTypeNames
         ('arrow down', 'arrow up', 'back slashed', 'circle dot', 'circle-x', 'circled', 'cluster',
-         'cross', 'diamond', 'do', 'fa', 'inverted triangle', 'la', 'left triangle',
+         'cross', 'diamond', 'do', 'fa', 'fa up', 'inverted triangle', 'la', 'left triangle',
          'mi', 'none', 'normal', 'other', 're', 'rectangle', 'slash', 'slashed', 'so',
          'square', 'ti', 'triangle', 'x')
         >>> n = note.Note()
