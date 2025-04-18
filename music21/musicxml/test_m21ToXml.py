8cf3ffdfa854779e09f2ddd0967c0af57fc679b2
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-04-18 12:33:07 -0700

    Add a test for (MusicXML) import/export/re-import of spanners with offsets, making sure that (1) the spanners in the two imported scores have the same offsets in the score, and (2) that there are no overlapping GeneralNotes in the streams containing either end of each spanner.

--- music21/musicxml/test_m21ToXml.py
+++ music21/musicxml/test_m21ToXml.py
@@ -7,7 +7,7 @@ import io
 import re
 import unittest
 from xml.etree.ElementTree import (
-    ElementTree, fromstring as et_fromstring
+    ElementTree, fromstring as et_fromstring, tostring as et_tostring
 )
 
 from music21 import articulations
@@ -766,6 +766,59 @@ class Test(unittest.TestCase):
             with self.subTest(pedal_index=startIdx + i):
                 for k in expectedResults2[i]:
                     self.assertEqual(mxPedal.get(k, ''), expectedResults2[i][k])
+
+    def testSpannersWithOffsets(self):
+        def gnfilter(overlaps):
+            removeKeys = []
+            for key, elList in overlaps.items():
+                gnCount = 0
+                for el in elList:
+                    if isinstance(el, note.GeneralNote):
+                        gnCount += 1
+                if gnCount < 2:
+                    removeKeys.append(key)
+            for key in removeKeys:
+                del overlaps[key]
+            return overlaps
+
+        def check(s1, s2, classType):
+            s1Spanners = list(s1[classType])
+            s2Spanners = list(s2[classType])
+            for s1sp, s2sp in zip(s1Spanners, s2Spanners):
+                # check that the spanners start and stop at exactly the same score offset
+                s1StartOffset = s1sp.getFirst().getOffsetInHierarchy(s1)
+                s2StartOffset = s2sp.getFirst().getOffsetInHierarchy(s2)
+                self.assertEqual(s1StartOffset, s2StartOffset)
+                s1EndOffset = s1sp.getLast().getOffsetInHierarchy(s1)
+                s2EndOffset = s2sp.getLast().getOffsetInHierarchy(s2)
+                self.assertEqual(s1EndOffset, s2EndOffset)
+
+                # check that there are no overlapping GeneralNotes in those measures
+                s1StartVoice = s1.containerInHierarchy(s1sp.getFirst())
+                s1EndVoice = s1.containerInHierarchy(s1sp.getLast())
+                s1StartVoiceOverlaps = s1StartVoice.getOverlaps()
+                s1EndVoiceOverlaps = s1EndVoice.getOverlaps()
+                self.assertEqual(gnfilter(s1StartVoiceOverlaps), {})
+                self.assertEqual(gnfilter(s1EndVoiceOverlaps), {})
+
+                s2StartVoice = s2.containerInHierarchy(s2sp.getFirst())
+                s2EndVoice = s2.containerInHierarchy(s2sp.getLast())
+                s2StartVoiceOverlaps = s2StartVoice.getOverlaps()
+                s2EndVoiceOverlaps = s2EndVoice.getOverlaps()
+                self.assertEqual(gnfilter(s2StartVoiceOverlaps), {})
+                self.assertEqual(gnfilter(s2EndVoiceOverlaps), {})
+
+        s1 = converter.parse(testPrimitive.directions31a)
+        x = self.getET(s1)
+        xmlStr = et_tostring(x)
+        s2 = converter.parseData(xmlStr, format='musicxml')
+        check(s1, s2, dynamics.DynamicWedge)
+
+        s1 = converter.parse(testPrimitive.octaveShifts33d)
+        x = self.getET(s1)
+        xmlStr = et_tostring(x)
+        s2 = converter.parseData(xmlStr, format='musicxml')
+        check(s1, s2, spanner.Ottava)
 
     def testArpeggios(self):
         expectedResults = (
