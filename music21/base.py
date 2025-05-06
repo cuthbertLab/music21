15bbe7d1e8dcf0c79377a2da4ad12c189ec53a61
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-05-06 14:59:55 -0700

    Bump version number in hopes that's why tests are failing.

--- music21/base.py
+++ music21/base.py
@@ -27,7 +27,7 @@ available after importing `music21`.
 <class 'music21.base.Music21Object'>
 
 >>> music21.VERSION_STR
-'9.6.0b10'
+'9.6.0b12'
 
 Alternatively, after doing a complete import, these classes are available
 under the module "base":
