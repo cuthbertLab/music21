243f3b6c498df552e7abf881210a1afaca6b2541
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2025-05-14 11:49:02 +0200

    import OffsetQL for pylint

--- music21/articulations.py
+++ music21/articulations.py
@@ -81,6 +81,7 @@ import unittest
 from music21 import base
 from music21 import common
 from music21.common.classTools import tempAttribute
+from music21.common.types import OffsetQL
 from music21 import environment
 from music21 import interval
 from music21 import spanner
