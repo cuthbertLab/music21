1784fb72c76fbfe010dec26050c62e9944faca4a
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-07-26 11:58:16 -0700

    makeRests now makes rests that can be exported to MusicXML (non-complex duration).

--- music21/base.py
+++ music21/base.py
@@ -27,7 +27,7 @@ available after importing `music21`.
 <class 'music21.base.Music21Object'>
 
 >>> music21.VERSION_STR
-'9.7.1'
+'9.7.2a6'
 
 Alternatively, after doing a complete import, these classes are available
 under the module "base":
