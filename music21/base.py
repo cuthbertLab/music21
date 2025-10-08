a5579d21c53dbd0439f0324fe37650dd878ba2cb
Michael Scott Asato Cuthbert <michael.asato.cuthbert@gmail.com>
2025-10-07 14:25:01 -1000

    sortTuple gets typing
    cannot use the new class definition directly because of the magic not working with anything that includes self.__class__(*...) -- but we can subclass a typed base class.
    
    a,b,c,d,e,f = ZeroSortTupleHigh
    reveal_type((a, b, c, d, e, f))
    tuple[builtins.int, builtins.float | fractions.Fraction, builtins.int, builtins.int, builtins.int, builtins.int]
    
    reveal_type(ZeroSortTupleHigh)
    tuple[builtins.int, builtins.float | fractions.Fraction, builtins.int, builtins.int, builtins.int, builtins.int, fallback=music21.sorting.SortTuple]
    
    update Version because removal of .flat invalidated caches

--- music21/base.py
+++ music21/base.py
@@ -27,7 +27,7 @@ available after importing `music21`.
 <class 'music21.base.Music21Object'>
 
 >>> music21.VERSION_STR
-'10.0.1b1'
+'10.0.1b2'
 
 Alternatively, after doing a complete import, these classes are available
 under the module "base":
