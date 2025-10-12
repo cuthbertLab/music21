1166346dab464cd55f43e0a5285250776c9af2c2
Michael Scott Asato Cuthbert <michael.asato.cuthbert@gmail.com>
2025-10-11 21:50:32 -1000

    nope -- avoid

--- music21/tree/core2.py
+++ music21/tree/core2.py
@@ -1,7 +1,7 @@
 
 import typing as t
 
-from sortedcontainers import SortedList
+from sortedcontainers import SortedKeyList
 
 from music21 import prebase
 from music21.sorting import SortTuple
@@ -24,6 +24,47 @@ class HasKey(t.Protocol[KeyT]):
 NodeT = t.TypeVar('NodeT', bound=HasKey)
 
 
+# only about 8% speedup even to 100000 entries -- not worth maintaining the added complexity
+
+# class SortedAppendKeyList(SortedKeyList[NodeT, KeyT], t.Generic[NodeT, KeyT]):
+#     def append(self, value: NodeT) -> None:  # type: ignore[attr-defined]
+#         '''
+#         Append value to the sorted-key list
+#         if its key is >= the current maximum; otherwise,
+#         raise ValueError.
+#
+#         Works in O(1) time up to LOAD FACTOR and then O(log(n/LOAD FACTOR))
+#         after that -- essentially free.
+#         '''
+#         # pylint: disable=protected-access
+#         _lists = self._lists  # noqa
+#         _keys = self._keys  # noqa
+#         _maxes = self._maxes  # noqa
+#         key = self._key(value)  # noqa
+#
+#         # First element: initialize structures
+#         if not _maxes:
+#             _lists.append([value])
+#             _keys.append([key])
+#             _maxes.append(key)
+#             self._len += 1  # noqa
+#             return
+#
+#         last = len(_maxes) - 1
+#
+#         # Enforce monotonicity
+#         if key < _maxes[last]:
+#             raise ValueError(
+#                 f'Append key {key!r} is less than current maximum {_maxes[last]!r}'
+#             )
+#
+#         # Fast path: append to last sublist
+#         _lists[last].append(value)
+#         _keys[last].append(key)
+#         _maxes[last] = key
+#         self._expand(last)  # noqa
+#         self._len += 1  # noqa
+
 
 class ElementNode(t.NamedTuple):
     position: SortTuple
@@ -34,7 +75,7 @@ class OffsetNode(t.NamedTuple):
     payload: Music21Object
 
 
-class CoreTree(SortedList[NodeT], t.Generic[NodeT], prebase.ProtoM21Object):
+class CoreTree(SortedKeyList[NodeT, KeyT], t.Generic[NodeT, KeyT], prebase.ProtoM21Object):
     def getNodeBefore(self, key: KeyT) -> NodeT | None:
         '''
         Return the node immediately before the given key.
